1fbb8ff21008199c85ee90048d506912fca4e0bb
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2014-11-20 23:46:27 -0800

    fix small typo bug

--- src/vegalite.js
+++ src/vegalite.js
@@ -472,14 +472,14 @@ function point_props(e) {
   // size
   if (e.has(SIZE)) {
     p.size = {scale: SIZE, field: e.field(SIZE)};
-  } else if (!e.has(X)) {
+  } else if (!e.has(SIZE)) {
     p.size = {value: e.config("pointSize")};
   }
   
   // shape
   if (e.has(SHAPE)) {
     p.shape = {scale: SHAPE, field: e.field(SHAPE)};
-  } else if (!e.has(X)) {
+  } else if (!e.has(SHAPE)) {
     p.shape = {value: e.config("pointShape")};
   }
 
