ad6df54663b9a12d9ec9a1e71da992ebc0c90b6e
Dominik Moritz <domoritz@gmail.com>
2014-12-16 16:13:11 -0800

    Fix parsing of shorthand and fix scaling of count field

--- src/vegalite.js
+++ src/vegalite.js
@@ -321,7 +321,7 @@ vl.Encoding = (function() {
       // check time fn
       for(var i in vl.timeFuncs){
         var f = vl.timeFuncs[i];
-        if(o.name.indexOf(f+"_") == 0){
+        if(o.name && o.name.indexOf(f+"_") == 0){
           o.name = o.name.substr(o.length+1);
           o.fn = f;
           break;
@@ -1037,7 +1037,7 @@ function template(encoding, size, stats) {
       data.format.parse[field.name] = "date";
     }else if(field.type == Q){
       data.format.parse = data.format.parse || {};
-      data.format.parse[field.name] = "number";
+      data.format.parse[field.aggr === "count" ? "count" : field.name] = "number";
     }
   });
 
