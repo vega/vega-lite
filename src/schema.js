7544967dab672906f2003504e7f102bbc7260c11
Dominik Moritz <domoritz@gmail.com>
2015-01-19 14:38:16 -0800

    Schema stuff in src

--- src/schema.js
+++ src/schema.js
@@ -1,5 +1,6 @@
 // Defining Vegalite Encoding's schema
-var schema = module.exports = {};
+var schema = module.exports = {},
+  util = require('./util');
 
 schema.util = require('./schemautil');
 
@@ -44,5 +45,167 @@ schema.field = {
     name: {
       type: "string"
     }
+  }
+};
+
+var typicalField = _.chain(schema.field).cloneDeep().merge({
+  type: "object",
+  properties: {
+    type: {
+      type: "string",
+      enum: ["O", "Q", "T"]
+    },
+    bin: {
+      type: "boolean",
+      supportedTypes: {"Q": true, "O": true}
+    },
+    aggr: schema.aggr,
+    fn: schema.fn,
+    scale: {
+      type: "object",
+      properties: {
+        type: schema.scale_type,
+        reverse: { type: "boolean", default: false },
+        zero: {
+          type: "boolean",
+          description: "Include zero",
+          default: false,
+          supportedTypes: {"Q": true}
+        },
+        nice: {
+          type: "string",
+          enum: ["second", "minute", "hour", "day", "week", "month", "year"],
+          supportedTypes: {"T": true}
+        }
+      }
+    }
+  }
+}); // no value()
+
+var onlyOrdinalField = _.chain(schema.field).cloneDeep().merge({
+  type: "object",
+  properties: {
+    type: {
+      type: "string",
+      enum: ["O"]
+    },
+    bin: {
+      type: "boolean",
+      supportedTypes: {"O": true}
+    },
+    aggr: {
+      type: "string",
+      enum: ["count"],
+      supportedTypes: {"O": true}
+    }
+  }
+});  // no value()
+
+var axisMixin = {
+  type: "object",
+  properties: {
+    axis: {
+      type: "object",
+      properties: {
+        grid: { type: "boolean", default: false },
+        title: { type: "boolean", default: true }
+      }
+    }
+  }
+}
+
+var legendMixin = {
+  type: "object",
+  properties: {
+    legend: { type: "boolean", default: true }
+  }
+}
+
+var textMixin = {
+  type: "object",
+  properties: {
+    text: {
+      type: "object",
+      properties: {
+        weight: {
+          type: "string",
+          enum: ["normal", "bold"],
+          default: "normal",
+          supportedTypes: {"T": true}
+        },
+        size: {
+          type: "integer",
+          default: 10,
+          minimum: 0,
+          supportedTypes: {"T": true}
+        },
+        font: {
+          type: "string",
+          default: "Halvetica Neue",
+          supportedTypes: {"T": true}
+        }
+      }
+    }
+  }
+}
+
+var x = typicalField.cloneDeep().merge(axisMixin).value();
+var y = _.cloneDeep(x);
+
+var row = onlyOrdinalField.cloneDeep().value();
+var col = _.cloneDeep(row);
+
+var size = typicalField.cloneDeep().merge(legendMixin).value();
+var color = typicalField.cloneDeep().merge(legendMixin).value();
+var alpha = typicalField.cloneDeep().value();
+var shape = onlyOrdinalField.cloneDeep().merge(legendMixin).value();
+
+var text = typicalField.cloneDeep().merge(textMixin).value();
+
+var cfg = {
+  type: "object",
+  properties: {
+    dataFormatType: {
+      type: "string",
+      enum: ["json", "csv"]
+    },
+    useVegaServer: {
+      type: "boolean",
+      default: false
+    },
+    dataUrl: {
+      type: "string"
+    },
+    vegaServerTable: {
+      type: "string"
+    },
+    vegaServerUrl: {
+      type: "string",
+      default: "http://localhost:3001"
+    }
+  }
+}
+
+schema.spec = {
+  $schema: "http://json-schema.org/draft-04/schema#",
+  type: "object",
+  required: ["marktype", "enc", "cfg"],
+  properties: {
+    marktype: schema.marktype,
+    enc: {
+      type: "object",
+      properties: {
+        x: x,
+        y: y,
+        row: row,
+        col: col,
+        size: size,
+        color: color,
+        alpha: alpha,
+        shape: shape,
+        text: text
+      }
+    },
+    cfg: cfg
   }
 };
