652d606062e131201c2a3db11bd37abf706e34ae
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-24 19:42:41 -0800

    split layout.js

--- src/compile/compile.js
+++ src/compile/compile.js
@@ -1,10 +1,10 @@
 var globals = require('../globals'),
   util = require('../util');
 
-
 module.exports = compile;
 
 var template  = compile.template    = require('./template'),
+  layout      = compile.layout      = require('./layout'),
   axis        = compile.axis        = require('./axis'),
   group       = compile.group       = require('./group'),
   legends     = compile.legends     = require('./legends'),
@@ -19,7 +19,7 @@ var template  = compile.template    = require('./template'),
 
 
 function compile (encoding, stats) {
-  var size = setSize(encoding, stats),
+  var size = layout.setSize(encoding, stats),
     cellWidth = size.cellWidth,
     cellHeight = size.cellHeight;
 
@@ -78,66 +78,4 @@ function compile (encoding, stats) {
   }
   return spec;
 }
-
-function getCardinality(encoding, encType, stats){
-  var field = encoding.fieldName(encType);
-  if (encoding.bin(encType)) {
-    var bins = util.getbins(stats[field]);
-    return (bins.stop - bins.start) / bins.step;
-  }
-  return stats[field].cardinality;
-}
-
-function setSize(encoding, stats) {
-  var hasRow = encoding.has(ROW),
-      hasCol = encoding.has(COL),
-      hasX = encoding.has(X),
-      hasY = encoding.has(Y);
-
-  // HACK to set chart size
-  // NOTE: this fails for plots driven by derived values (e.g., aggregates)
-  // One solution is to update Vega to support auto-sizing
-  // In the meantime, auto-padding (mostly) does the trick
-  //
-  var colCardinality = hasCol ? getCardinality(encoding, COL, stats) : 1,
-    rowCardinality = hasRow ? getCardinality(encoding, ROW, stats) : 1;
-
-  var cellWidth = hasX ?
-      +encoding.config("cellWidth") || encoding.config("width") * 1.0 / colCardinality :
-      encoding.marktype() === "text" ?
-        +encoding.config("textCellWidth") :
-        +encoding.config("bandSize"),
-    cellHeight = hasY ?
-      +encoding.config("cellHeight") || encoding.config("height") * 1.0 / rowCardinality :
-      +encoding.config("bandSize"),
-    cellPadding = encoding.config("cellPadding"),
-    bandPadding = encoding.config("bandPadding"),
-    width = encoding.config("_minWidth"),
-    height = encoding.config("_minHeight");
-
-  if (hasX && (encoding.isType(X, O) || encoding.bin(X))) { //ordinal field will override parent
-    // bands within cell use rangePoints()
-    var xCardinality = getCardinality(encoding, X, stats);
-    cellWidth = (xCardinality + bandPadding) * +encoding.config("bandSize");
-  }
-  // Cell bands use rangeBands(). There are n-1 padding.  Outerpadding = 0 for cells
-  width = cellWidth * ((1 + cellPadding) * (colCardinality-1) + 1);
-
-  if (hasY && (encoding.isType(Y, O) || encoding.bin(Y))) {
-    // bands within cell use rangePoint()
-    var yCardinality = getCardinality(encoding, Y, stats);
-    cellHeight = (yCardinality + bandPadding) * +encoding.config("bandSize");
-  }
-  // Cell bands use rangeBands(). There are n-1 padding.  Outerpadding = 0 for cells
-  height = cellHeight * ((1 + cellPadding) * (rowCardinality-1) + 1);
-
-  return {
-    cellWidth: cellWidth,
-    cellHeight: cellHeight,
-    width: width,
-    height:height
-  };
-}
-
-
 
