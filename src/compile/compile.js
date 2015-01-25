cc4d3b5986c3fe6672a90c7bb6dd4790244dff5c
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-24 19:16:53 -0800

    split stacking.js

--- src/compile/compile.js
+++ src/compile/compile.js
@@ -9,7 +9,9 @@ var axis      = compile.axis        = require('./axis'),
   marks       = compile.marks       = require('./marks'),
   scale       = compile.scale       = require('./scale'),
   time        = compile.time        = require('./time'),
-  aggregates  = compile.aggregates  = require('.aggregates');
+  aggregates  = compile.aggregates  = require('./aggregates'),
+  binning     = compile.binning     = require('./binning'),
+  stacking    = compile.stacking    = require('./stacking');
 
 
 function compile (encoding, stats) {
@@ -262,54 +264,6 @@ function subfacet(group, mdef, details, stack, encoding) {
     trans.unshift({type: "sort", by: encoding.field(COLOR)});
   }
 }
-
-function stacking(spec, encoding, mdef, facets) {
-  if (!marks[encoding.marktype()].stack) return false;
-  if (!encoding.has(COLOR)) return false;
-
-  var dim = X, val = Y, idx = 1;
-  if (encoding.isType(X,Q|T) && !encoding.isType(Y,Q|T) && encoding.has(Y)) {
-    dim = Y;
-    val = X;
-    idx = 0;
-  }
-
-  // add transform to compute sums for scale
-  var stacked = {
-    name: STACKED,
-    source: TABLE,
-    transform: [{
-      type: "aggregate",
-      groupby: [encoding.field(dim)].concat(facets), // dim and other facets
-      fields: [{op: "sum", field: encoding.field(val)}] // TODO check if field with aggr is correct?
-    }]
-  };
-
-  if(facets && facets.length > 0){
-    stacked.transform.push({ //calculate max for each facet
-      type: "aggregate",
-      groupby: facets,
-      fields: [{op: "max", field: "data.sum_" + encoding.field(val, true)}]
-    });
-  }
-
-  spec.data.push(stacked);
-
-  // add stack transform to mark
-  mdef.from.transform = [{
-    type: "stack",
-    point: encoding.field(dim),
-    height: encoding.field(val),
-    output: {y1: val, y0: val+"2"}
-  }];
-
-  // TODO: This is super hack-ish -- consolidate into modular mark properties?
-  mdef.properties.update[val] = mdef.properties.enter[val] = {scale: val, field: val};
-  mdef.properties.update[val+"2"] = mdef.properties.enter[val+"2"] = {scale: val, field: val+"2"};
-
-  return val; //return stack encoding
-}
-
 
 function markdef(mark, encoding, opt) {
   var p = mark.prop(encoding, opt)
---
+++ src/compile/stacking.js
@@ -0,0 +1,51 @@
+var globals = require('../globals'),
+  util = require('../util');
+
+module.exports = stacking;
+
+function stacking(spec, encoding, mdef, facets) {
+  if (!marks[encoding.marktype()].stack) return false;
+  if (!encoding.has(COLOR)) return false;
+
+  var dim = X, val = Y, idx = 1;
+  if (encoding.isType(X,Q|T) && !encoding.isType(Y,Q|T) && encoding.has(Y)) {
+    dim = Y;
+    val = X;
+    idx = 0;
+  }
+
+  // add transform to compute sums for scale
+  var stacked = {
+    name: STACKED,
+    source: TABLE,
+    transform: [{
+      type: "aggregate",
+      groupby: [encoding.field(dim)].concat(facets), // dim and other facets
+      fields: [{op: "sum", field: encoding.field(val)}] // TODO check if field with aggr is correct?
+    }]
+  };
+
+  if(facets && facets.length > 0){
+    stacked.transform.push({ //calculate max for each facet
+      type: "aggregate",
+      groupby: facets,
+      fields: [{op: "max", field: "data.sum_" + encoding.field(val, true)}]
+    });
+  }
+
+  spec.data.push(stacked);
+
+  // add stack transform to mark
+  mdef.from.transform = [{
+    type: "stack",
+    point: encoding.field(dim),
+    height: encoding.field(val),
+    output: {y1: val, y0: val+"2"}
+  }];
+
+  // TODO: This is super hack-ish -- consolidate into modular mark properties?
+  mdef.properties.update[val] = mdef.properties.enter[val] = {scale: val, field: val};
+  mdef.properties.update[val+"2"] = mdef.properties.enter[val+"2"] = {scale: val, field: val+"2"};
+
+  return val; //return stack encoding
+}
