e641c0937fef20efc237db8e6598a48c90905916
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-24 19:27:45 -0800

    split faceting.js

--- src/compile/compile.js
+++ src/compile/compile.js
@@ -11,6 +11,7 @@ var axis      = compile.axis        = require('./axis'),
   time        = compile.time        = require('./time'),
   aggregates  = compile.aggregates  = require('./aggregates'),
   binning     = compile.binning     = require('./binning'),
+  faceting    = compile.faceting    = require('./faceting'),
   stacking    = compile.stacking    = require('./stacking');
 
 
@@ -65,7 +66,7 @@ function compile (encoding, stats) {
 
   // Small Multiples
   if (hasRow || hasCol) {
-    spec = facet(group, encoding, cellHeight, cellWidth, spec, mdef, stack, stats);
+    spec = faceting(group, encoding, cellHeight, cellWidth, spec, mdef, stack, stats);
   } else {
     group.scales = scale.defs(scale.names(mdef.properties.update), encoding,
       {stack: stack, stats: stats});
@@ -135,118 +136,7 @@ function setSize(encoding, stats) {
   };
 }
 
-function facet(group, encoding, cellHeight, cellWidth, spec, mdef, stack, stats) {
-    var enter = group.properties.enter;
-    var facetKeys = [], cellAxes = [];
 
-    var hasRow = encoding.has(ROW), hasCol = encoding.has(COL);
-
-    var xAxisMargin = encoding.has(Y) ? encoding.config("xAxisMargin") : undefined;
-
-    enter.fill = {value: encoding.config("cellBackgroundColor")};
-
-    //move "from" to cell level and add facet transform
-    group.from = {data: group.marks[0].from.data};
-
-    if (group.marks[0].from.transform) {
-      delete group.marks[0].from.data; //need to keep transform for subfacetting case
-    } else {
-      delete group.marks[0].from;
-    }
-    if (hasRow) {
-      if (!encoding.isType(ROW, O)) {
-        util.error("Row encoding should be ordinal.");
-      }
-      enter.y = {scale: ROW, field: "keys." + facetKeys.length};
-      enter.height = {"value": cellHeight}; // HACK
-
-      facetKeys.push(encoding.field(ROW));
-
-      var from;
-      if (hasCol) {
-        from = util.duplicate(group.from);
-        from.transform = from.transform || [];
-        from.transform.unshift({type: "facet", keys: [encoding.field(COL)]});
-      }
-
-      var axesGrp = groupdef("x-axes", {
-          axes: encoding.has(X) ?  axis.defs(["x"], encoding) : undefined,
-          x: hasCol ? {scale: COL, field: "keys.0", offset: xAxisMargin} : {value: xAxisMargin},
-          width: hasCol && {"value": cellWidth}, //HACK?
-          from: from
-        });
-
-      spec.marks.push(axesGrp);
-      (spec.axes = spec.axes || []);
-      spec.axes.push.apply(spec.axes, axis.defs(["row"], encoding));
-    } else { // doesn't have row
-      if(encoding.has(X)){
-        //keep x axis in the cell
-        cellAxes.push.apply(cellAxes, axis.defs(["x"], encoding));
-      }
-    }
-
-    if (hasCol) {
-      if (!encoding.isType(COL, O)) {
-        util.error("Col encoding should be ordinal.");
-      }
-      enter.x = {scale: COL, field: "keys." + facetKeys.length};
-      enter.width = {"value": cellWidth}; // HACK
-
-      facetKeys.push(encoding.field(COL));
-
-      var from;
-      if (hasRow) {
-        from = util.duplicate(group.from);
-        from.transform = from.transform || [];
-        from.transform.unshift({type: "facet", keys: [encoding.field(ROW)]});
-      }
-
-      var axesGrp = groupdef("y-axes", {
-        axes: encoding.has(Y) ? axis.defs(["y"], encoding) : undefined,
-        y: hasRow && {scale: ROW, field: "keys.0"},
-        x: hasRow && {value: xAxisMargin},
-        height: hasRow && {"value": cellHeight}, //HACK?
-        from: from
-      });
-
-      spec.marks.push(axesGrp);
-      (spec.axes = spec.axes || [])
-      spec.axes.push.apply(spec.axes, axis.defs(["col"], encoding, {
-        xAxisMargin: xAxisMargin
-      }));
-    } else { // doesn't have col
-      if(encoding.has(Y)){
-        cellAxes.push.apply(cellAxes, axis.defs(["y"], encoding));
-      }
-    }
-
-    if(hasRow){
-      if(enter.x) enter.x.offset= xAxisMargin;
-      else enter.x = {value: xAxisMargin};
-    }
-    if(hasCol){
-      //TODO fill here..
-    }
-
-    // assuming equal cellWidth here
-    // TODO: support heterogenous cellWidth (maybe by using multiple scales?)
-    spec.scales = (spec.scales ||[]).concat(scale.defs(
-      scale.names(enter).concat(scale.names(mdef.properties.update)),
-      encoding,
-      {cellWidth: cellWidth, cellHeight: cellHeight, stack: stack, facet:true, stats: stats}
-    )); // row/col scales + cell scales
-
-    if (cellAxes.length > 0) {
-      group.axes = cellAxes;
-    }
-
-    // add facet transform
-    var trans = (group.from.transform || (group.from.transform = []));
-    trans.unshift({type: "facet", keys: facetKeys});
-
-  return spec;
-  }
 
 function subfacet(group, mdef, details, stack, encoding) {
   var m = group.marks,
