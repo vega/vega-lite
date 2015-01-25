1f419188daff12213f4fcfa9f311fd7cc8ece3fe
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-24 19:12:42 -0800

    split aggregates.js

---
+++ src/compile/aggregates.js
@@ -0,0 +1,57 @@
+var globals = require('../globals'),
+  util = require('../util');
+
+module.exports = aggregates;
+
+function aggregates (spec, encoding, opt) {
+  opt = opt || {};
+  var dims = {}, meas = {}, detail = {}, facets={};
+  encoding.forEach(function(encType, field) {
+    if (field.aggr) {
+      if(field.aggr==="count"){
+        meas['count'] = {op:"count", field:"*"};
+      }else{
+        meas[field.aggr+"|"+field.name] = {
+          op:field.aggr,
+          field:"data."+field.name
+        };
+      }
+    } else {
+      dims[field.name] = encoding.field(encType);
+      if (encType==ROW || encType == COL){
+        facets[field.name] = dims[field.name];
+      }else if (encType !== X && encType !== Y) {
+        detail[field.name] = dims[field.name];
+      }
+    }
+  });
+  dims = util.vals(dims);
+  meas = util.vals(meas);
+
+  if (meas.length > 0 && !opt.preaggregatedData) {
+    if (!spec.transform) spec.transform = [];
+    spec.transform.push({
+      type: "aggregate",
+      groupby: dims,
+      fields: meas
+    });
+
+    if (encoding.marktype() === TEXT) {
+      meas.forEach( function (m) {
+        var fieldName = m.field.substr(5), //remove "data."
+          field = "data." + (m.op ? m.op + "_" : "") + fieldName;
+        spec.transform.push({
+          type: "formula",
+          field: field,
+          expr: "d3.format('.2f')(d."+field+")"
+        });
+      });
+    }
+  }
+  return {
+    details: util.vals(detail),
+    dims: dims,
+    facets: util.vals(facets),
+    aggregated: meas.length > 0
+  };
+}
--- src/compile/compile.js
+++ src/compile/compile.js
@@ -4,11 +4,12 @@ var globals = require('../globals'),
 
 module.exports = compile;
 
-var axis  = compile.axis    = require('./axis'),
-  legends = compile.legends = require('./legends'),
-  marks   = compile.marks   = require('./marks'),
-  scale   = compile.scale   = require('./scale'),
-  time    = compile.time    = require('./time');
+var axis      = compile.axis        = require('./axis'),
+  legends     = compile.legends     = require('./legends'),
+  marks       = compile.marks       = require('./marks'),
+  scale       = compile.scale       = require('./scale'),
+  time        = compile.time        = require('./time'),
+  aggregates  = compile.aggregates  = require('.aggregates');
 
 
 function compile (encoding, stats) {
@@ -284,58 +285,7 @@ function binning(spec, encoding, opt) {
   return bins;
 }
 
-function aggregates(spec, encoding, opt) {
-  opt = opt || {};
-  var dims = {}, meas = {}, detail = {}, facets={};
-  encoding.forEach(function(encType, field) {
-    if (field.aggr) {
-      if(field.aggr==="count"){
-        meas["count"] = {op:"count", field:"*"};
-      }else{
-        meas[field.aggr+"|"+field.name] = {
-          op:field.aggr,
-          field:"data."+field.name
-        };
-      }
-    } else {
-      dims[field.name] = encoding.field(encType);
-      if (encType==ROW || encType == COL){
-        facets[field.name] = dims[field.name];
-      }else if (encType !== X && encType !== Y) {
-        detail[field.name] = dims[field.name];
-      }
-    }
-  });
-  dims = util.vals(dims);
-  meas = util.vals(meas);
 
-  if (meas.length > 0 && !opt.preaggregatedData) {
-    if (!spec.transform) spec.transform = [];
-    spec.transform.push({
-      type: "aggregate",
-      groupby: dims,
-      fields: meas
-    });
-
-    if (encoding.marktype() === TEXT) {
-      meas.forEach( function (m) {
-        var fieldName = m.field.substr(5), //remove "data."
-          field = "data." + (m.op ? m.op + "_" : "") + fieldName;
-        spec.transform.push({
-          type: "formula",
-          field: field,
-          expr: "d3.format('.2f')(d."+field+")"
-        });
-      });
-    }
-  }
-  return {
-    details: util.vals(detail),
-    dims: dims,
-    facets: util.vals(facets),
-    aggregated: meas.length > 0
-  }
-}
 
 function stacking(spec, encoding, mdef, facets) {
   if (!marks[encoding.marktype()].stack) return false;
