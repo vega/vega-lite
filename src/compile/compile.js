ca12aa511f373d7f6f62657a8d274376c8043255
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-24 19:14:45 -0800

    split binning.js

--- src/compile/compile.js
+++ src/compile/compile.js
@@ -263,30 +263,6 @@ function subfacet(group, mdef, details, stack, encoding) {
   }
 }
 
-function binning(spec, encoding, opt) {
-  opt = opt || {};
-  var bins = {};
-  encoding.forEach(function(vv, d) {
-    if (d.bin) bins[d.name] = d.name;
-  });
-  bins = util.keys(bins);
-
-  if (bins.length === 0 || opt.preaggregatedData) return false;
-
-  if (!spec.transform) spec.transform = [];
-  bins.forEach(function(d) {
-    spec.transform.push({
-      type: "bin",
-      field: "data." + d,
-      output: "data.bin_" + d,
-      maxbins: MAX_BINS
-    });
-  });
-  return bins;
-}
-
-
-
 function stacking(spec, encoding, mdef, facets) {
   if (!marks[encoding.marktype()].stack) return false;
   if (!encoding.has(COLOR)) return false;
