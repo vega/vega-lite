04b11f906c9cd4e42b05f8e4f916d5d63f540dc3
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-01-29 19:27:41 -0800

    add `vl.field` as  field utility (similar to vl.data) + update npm package's main to be vl.js

--- src/vl.js
+++ src/vl.js
@@ -7,6 +7,8 @@ var vl = util.merge(consts, util);
 vl.Encoding = require('./Encoding');
 vl.compile = require('./compile/compile');
 vl.data = require('./data');
+vl.field = require('./field');
 vl.schema = require('./schema/schema');
+
 
 module.exports = vl;
