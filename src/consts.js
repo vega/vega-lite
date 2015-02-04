7d8e9e797cbf2698b0ff38dc04df62e6cc057e5a
Dominik Moritz <domoritz@gmail.com>
2015-02-03 20:52:24 -0800

    Move all cfg defaults to schema

--- src/consts.js
+++ src/consts.js
@@ -9,23 +9,3 @@ consts.dataTypes = {'O': O, 'Q': Q, 'T': T};
 consts.dataTypeNames = ['O', 'Q', 'T'].reduce(function(r, x) {
   r[consts.dataTypes[x]] = x; return r;
 },{});
-
-consts.DEFAULTS = {
-  // single plot
-  singleHeight: 200, // will be overwritten by bandWidth * (cardinality + padding)
-  singleWidth: 200, // will be overwritten by bandWidth * (cardinality + padding)
-  largeBandSize: 21,
-
-  smallBandSize: 13, //small multiples or single plot with high cardinality
-
-  // small multiples
-  cellPadding: 0.1,
-  cellBackgroundColor: '#fdfdfd',
-  textCellWidth: 90,
-
-  // marks
-  strokeWidth: 2,
-
-  // scales
-  timeScaleLabelLength: 3
-};
