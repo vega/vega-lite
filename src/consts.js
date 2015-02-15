710c0cc625615a2059f508196a67bef4b4d23611
Dominik Moritz <domoritz@gmail.com>
2015-02-15 11:00:13 -0800

    style

--- src/consts.js
+++ src/consts.js
@@ -7,7 +7,8 @@ consts.encodingTypes = [X, Y, ROW, COL, SIZE, SHAPE, COLOR, ALPHA, TEXT];
 consts.dataTypes = {'O': O, 'Q': Q, 'T': T};
 
 consts.dataTypeNames = ['O', 'Q', 'T'].reduce(function(r, x) {
-  r[consts.dataTypes[x]] = x; return r;
+  r[consts.dataTypes[x]] = x;
+  return r;
 },{});
 
 consts.shorthand = {
