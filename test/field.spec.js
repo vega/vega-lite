e801f7d395091361a366d6292af10ac1e83fe24f
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-02-28 23:15:37 -0800

    add test for vl.field.cardinality

---
+++ test/field.spec.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+
+var vlfield = require('../src/field');
+
+
+describe('vl.field.cardinality()', function () {
+  describe('for Q', function () {
+    it('should return cardinality', function() {
+      var field = {name:2, type:'Q'};
+      var stats = {2:{cardinality: 10, min:0, max:150}};
+      var cardinality = vlfield.cardinality(field, stats);
+      expect(cardinality).to.equal(10);
+    });
+  });
+
+  describe('for B(Q)', function(){
+    it('should return cardinality', function() {
+      var field = {name:2, type:'Q', bin: {maxbins: 15}};
+      var stats = {2:{cardinality: 10, min:0, max:150}};
+      var cardinality = vlfield.cardinality(field, stats);
+      expect(cardinality).to.equal(10);
+    });
+  });
+});
