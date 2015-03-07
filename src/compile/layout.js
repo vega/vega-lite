b930030da937d367d68f249bf4d25a791baff467
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-03-07 13:11:28 -0800

    add comment

--- src/compile/layout.js
+++ src/compile/layout.js
@@ -28,6 +28,7 @@ function box(encoding, stats) {
       hasY = encoding.has(Y),
       marktype = encoding.marktype();
 
+  // FIXME/HACK we need to take filter into account
   var xCardinality = hasX && encoding.isDimension(X) ? encoding.cardinality(X, stats) : 1,
     yCardinality = hasY && encoding.isDimension(Y) ? encoding.cardinality(Y, stats) : 1;
 
