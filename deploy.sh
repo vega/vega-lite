77a4ffd96b8552c2974db621e2f5d0f289e72aaa
Dominik Moritz <domoritz@gmail.com>
2015-04-21 15:02:45 -0700

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 gitsha=$(git rev-parse HEAD)
+rm vegalite* -f
+rm spec.json
+
+git checkout gh-pages
+git merge master
+
 git add vegalite* -f
 git add spec.json -f
 git commit -m "release $gitsha"
+
+git checkout master
