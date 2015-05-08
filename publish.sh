2bdb1cf79b6e061da7c3f6c78e1eee1c26b5598d
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-05-08 09:11:27 -0700

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,25 @@
+# update on npm
 npm publish
 
+# read version
+gitsha=$(git rev-parse HEAD)
+version=$(npm view vegalite -j | jq .version)
+
+rm vegalite* -f
+rm spec.json
+
+# update github pages
+git checkout gh-pages
+git merge master
+
+gulp build
+git add vegalite* -f
+git add spec.json -f
+git commit -m "release $version $gitsha"
+git push
+
+# and tag gh-pages, so our release contains compiled file
 git tag "v$version"
 git push --tags
 
-./deploy.sh
+git checkout master
