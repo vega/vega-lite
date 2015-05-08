a6e807da776b90a87c33b06c29e92e848a3f1e57
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-05-07 22:19:25 -0700

    remove getSchema & update test

--- test/data.spec.js
+++ test/data.spec.js
@@ -8,24 +8,6 @@ describe('getUrl', function() {
   //TODO write tests for getUrl
 });
 
-describe('getSchema', function() {
-  it('should return correct schema', function() {
-    var d = [
-      {a: '', b: null, d: undefined},
-      {a: 1, b: 's', d: 'Jan 11, 2014'}
-    ];
-    var s = data.getSchema(d, vlfield.order.name);
-
-    assert.equal(s.length, 3);
-    assert.equal(s[0].name, 'a');
-    assert.equal(s[0].type, 'Q');
-    assert.equal(s[1].name, 'b');
-    assert.equal(s[1].type, 'O');
-    assert.equal(s[2].name, 'd');
-    assert.equal(s[2].type, 'T');
-  });
-});
-
 describe('getStats', function() {
   //TODO write more tests
 });
