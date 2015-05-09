c13588431390b5f15c39c1821d1d670c026cfdd3
Dominik Moritz <domoritz@gmail.com>
2015-05-08 23:53:29 -0700

    Fix wrong data format

--- editor/editor.js
+++ editor/editor.js
@@ -200,7 +200,6 @@ vled.init = function() {
     vled.format();
   } else {
     document.getElementById("vlspec").value = JSON.stringify({
-      data: 'data/barley.json',
       marktype: "point",
       enc: {
         x: {type: "Q",name: "yield",aggr: "avg"},
