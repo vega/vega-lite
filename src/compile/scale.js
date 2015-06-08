47d52075889845c17079fce66beb7660f615d7a9
Dominik Moritz <domoritz@gmail.com>
2015-06-08 13:35:43 -0700

    Fix color palette

--- src/compile/scale.js
+++ src/compile/scale.js
@@ -179,7 +179,7 @@ scale.color = function(s, encoding, stats) {
       if (type === N) {
         // use categorical color scale
         if (cardinality <= 10) {
-          range = 'category10';
+          range = 'category10k';
         } else {
           range = 'category20';
         }
