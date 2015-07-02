47a0788e66bc33b1a689d4c4968f314c501585c4
Vivek Ratnavel S <vivekratnavel90@gmail.com>
2015-07-02 15:12:06 -0700

    Added unit tests for point and line

--- test/compiler/marks.spec.js
+++ test/compiler/marks.spec.js
@@ -71,5 +71,140 @@ describe('compile.marks', function() {
     });
   });
 
+  describe('point', function() {
+    describe('1D, horizontal', function() {
+      var f = fixtures.points['1d_hor'],
+          e = Encoding.fromSpec(f),
+          def = marks.point.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should end on y axis', function() {
+        expect(def.y).to.eql({value: e.bandSize(Y, mockLayout.y.useSmallBand) / 2});
+      });
+      it('should scale on x', function() {
+        expect(def.x).to.eql({scale: X, field: "data.year"});
+      });
+    });
+
+    describe('1D, vertical', function() {
+      var f = fixtures.points['1d_ver'],
+          e = Encoding.fromSpec(f),
+          def = marks.point.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should end on x axis', function() {
+        expect(def.x).to.eql({value: e.bandSize(X, mockLayout.x.useSmallBand) / 2});
+      });
+      it('should scale on y', function() {
+        expect(def.y).to.eql({scale: Y, field: "data.year"});
+      });
+    });
+
+    describe('2D, x and y', function() {
+      var f = fixtures.points['x_and_y'],
+          e = Encoding.fromSpec(f),
+          def = marks.point.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should scale on x', function() {
+        expect(def.x).to.eql({scale: X, field: "data.year"});
+      });
+      it('should scale on y', function(){
+        expect(def.y).to.eql({scale: Y, field: "data.yield"});
+      });
+    });
+
+    describe('3D', function() {
+      describe('size', function () {
+        var f = fixtures.points.size,
+            e = Encoding.fromSpec(f),
+            def = marks.point.prop(e, mockLayout, {});
+        console.log("def: ");
+        console.log(def);
+        it('should have scale for size', function () {
+          expect(def.size).to.eql({scale: SIZE, field: "data.count"});
+        });
+      });
+
+      describe('color', function () {
+        var f = fixtures.points.stroke,
+            e = Encoding.fromSpec(f),
+            def = marks.point.prop(e, mockLayout, {});
+        console.log("def: ");
+        console.log(def);
+        it('should have scale for color', function () {
+          expect(def.stroke).to.eql({scale: COLOR, field: "data.yield"});
+        });
+      });
+
+      describe('shape', function () {
+        var f = fixtures.points.shape,
+            e = Encoding.fromSpec(f),
+            def = marks.point.prop(e, mockLayout, {});
+        console.log("def: ");
+        console.log(def);
+        it('should have scale for shape', function () {
+          expect(def.shape).to.eql({scale: SHAPE, field: "data.bin_yield"});
+        });
+      });
+    });
+  });
+
+  describe('line', function() {
+    describe('1D, horizontal', function() {
+      var f = fixtures.lines['1d_hor'],
+          e = Encoding.fromSpec(f),
+          def = marks.line.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should end on y axis', function() {
+        expect(def.y).to.eql({group: 'height'});
+      });
+      it('should scale on x', function() {
+        expect(def.x).to.eql({scale: X, field: "data.year"});
+      });
+    });
+
+    describe('1D, vertical', function() {
+      var f = fixtures.lines['1d_ver'],
+          e = Encoding.fromSpec(f),
+          def = marks.line.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should end on x axis', function() {
+        expect(def.x).to.eql({value: 0});
+      });
+      it('should scale on y', function() {
+        expect(def.y).to.eql({scale: Y, field: "data.year"});
+      });
+    });
+
+    describe('2D, x and y', function() {
+      var f = fixtures.lines['x_and_y'],
+          e = Encoding.fromSpec(f),
+          def = marks.line.prop(e, mockLayout, {});
+      console.log("def: ");
+      console.log(def);
+      it('should have scale for x', function() {
+        expect(def.x).to.eql({scale: X, field: "data.year"});
+      });
+      it('should have scale for y', function(){
+        expect(def.y).to.eql({scale: Y, field: "data.yield"});
+      });
+    });
+
+    describe('3D', function() {
+      describe('color', function () {
+        var f = fixtures.lines.stroke,
+            e = Encoding.fromSpec(f),
+            def = marks.line.prop(e, mockLayout, {});
+        console.log("def: ");
+        console.log(def);
+        it('should have scale for color', function () {
+          expect(def.stroke).to.eql({scale: COLOR, field: "data.Acceleration"});
+        });
+      });
+    });
+  });
   // TODO add other type of marks
 });
--- test/fixtures.js
+++ test/fixtures.js
@@ -60,4 +60,100 @@ f.stack.binX = {
   }
 };
 
+// POINT
+
+f.points = {};
+
+f.points['1d_hor'] = {
+  "marktype": "point",
+  "encoding": {"x": {"name": "year","type": "O"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.points['1d_ver'] = {
+  "marktype": "point",
+  "encoding": {"y": {"name": "year","type": "O"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.points['x_and_y'] = {
+  "marktype": "point",
+  "encoding": {"x": {"name": "year","type": "O"},"y": {"name": "yield","type": "Q"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.points.size = {
+  "marktype": "point",
+  "encoding": {
+    "x": {"name": "year","type": "O"},
+    "y": {"name": "yield","type": "Q"},
+    "size": {"name": "*","type": "Q","aggregate": "count"}
+  },
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.points.stroke = {
+  "marktype": "point",
+  "encoding": {
+    "x": {"name": "year","type": "O"},
+    "y": {"name": "yield","type": "Q"},
+    "color": {"name": "yield","type": "Q"}
+  },
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.points.shape = {
+  "marktype": "point",
+  "encoding": {
+    "x": {"name": "year","type": "O"},
+    "y": {"name": "yield","type": "Q"},
+    "shape": {"bin": {"maxbins": 15},"name": "yield","type": "Q"}
+  },
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+// LINE
+
+f.lines = {};
+
+f.lines['1d_hor'] = {
+  "marktype": "line",
+  "encoding": {"x": {"name": "year","type": "O"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.lines['1d_ver'] = {
+  "marktype": "line",
+  "encoding": {"y": {"name": "year","type": "O"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.lines['x_and_y'] = {
+  "marktype": "line",
+  "encoding": {"x": {"name": "year","type": "O"},"y": {"name": "yield","type": "Q"}},
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/barley.json"}
+};
+
+f.lines.stroke = {
+  "marktype": "line",
+  "encoding": {
+    "x": {"name": "name","type": "N"},
+    "y": {"name": "Cylinders","type": "O"},
+    "color": {"name": "Acceleration","type": "Q"}
+  },
+  "config": {"singleHeight": 400,"singleWidth": 400,"largeBandMaxCardinality": 20},
+  "data": {"url": "data/cars.json"}
+};
+
+
+
 module.exports = f;
