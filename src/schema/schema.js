332d737512f82ce88322b6a0fa920846b9b26eb6
Dominik Moritz <domoritz@gmail.com>
2015-07-15 08:44:01 -0700

    Update schema.js

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -114,7 +114,7 @@ var typicalField = merge(clone(schema.field), {
           description: 'Use the raw data range as scale domain instead of ' +
                        'aggregated data for aggregate axis. ' +
                        'This option does not work with sum or count aggregate' +
-                       'as they might have substantially large scale range.'
+                       'as they might have a substantially larger scale range.'
         }
       }
     }
