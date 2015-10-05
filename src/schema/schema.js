3f0e517e4b0c6494c4c7ab520785c3f5713c732e
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-10-05 16:57:23 +0700

    Extract data.raw.transform.nullFilter from data.raw.transform.filter so that we can perform nullFilter before binning and timeUnit calculation but perform general filter afterward.

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -613,6 +613,7 @@ var config = {
     filterNull: {
       type: 'object',
       properties: {
+        N: {type:'boolean', default: false},
         O: {type:'boolean', default: false},
         Q: {type:'boolean', default: true},
         T: {type:'boolean', default: true}
