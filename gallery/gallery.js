50a070461ba09ab63809e8c0438547d5b97e28c4
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-10-07 20:38:35 +0700

    modify gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -37,7 +37,7 @@ var EXAMPLES = [
       marktype: 'line',
       encoding: {
         x: {'name': 'Year','type': 'T','timeUnit': 'year'},
-        y: {'name': 'Horsepower','type': 'Q','aggregate': 'avg'}
+        y: {'name': 'Horsepower','type': 'Q','aggregate': 'mean'}
       },
       data: {'url': 'data/cars.json'}
     }
@@ -133,8 +133,8 @@ var EXAMPLES = [
       'marktype': 'text',
       'encoding': {
         'row': {'name': 'Origin','type': 'O'},
-        'col': {'axis': {'maxLabelLength': 25},'name': 'Cylinders','type': 'O'},
-        'color': {'name': 'Horsepower','type': 'Q','aggregate': 'avg'},
+        'col': {'name': 'Cylinders','type': 'O'},
+        'color': {'name': 'Horsepower','type': 'Q','aggregate': 'mean'},
         'text': {'name': '*','type': 'Q','aggregate': 'count'}
       },
       'data': {'url': 'data/cars.json'}
@@ -146,11 +146,7 @@ var EXAMPLES = [
       'encoding': {
         'x': {'name': 'Year','type': 'T','timeUnit': 'year'},
         'y': {'name': 'Weight_in_lbs','type': 'Q','aggregate': 'sum'},
-        'color': {
-          'scale': {'quantitativeRange': ['#AFC6A3','#09622A']},
-          'name': 'Cylinders',
-          'type': 'O'
-        }
+        'color': {'name': 'Cylinders', 'type': 'O'}
       },
       'data': {'url': 'data/cars.json'}
     }
