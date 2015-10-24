27809e872271b75f2badfc4f983aecbab5688bc2
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-10-23 22:12:40 -0700

    bump version in package.json and remove version from bower.json #690

--- gulp/bumpver.js
+++ gulp/bumpver.js
@@ -3,14 +3,13 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-
 function inc(importance) {
-    // get all the files to bump version in
-    return gulp.src(['./package.json', './bower.json'])
-        // bump the version number in those files
-        .pipe($.bump({type: importance}))
-        // save it back to filesystem
-        .pipe(gulp.dest('./'));
+  // get all the files to bump version in
+  return gulp.src(['./package.json'])
+      // bump the version number in those files
+      .pipe($.bump({type: importance}))
+      // save it back to filesystem
+      .pipe(gulp.dest('./'));
 }
 
 gulp.task('patch', function() { return inc('patch'); });
