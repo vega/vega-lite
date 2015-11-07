e8d01a2bce18cb4d4a5d2b64b2d9c623cbd8fe22
Dominik Moritz <domoritz@gmail.com>
2015-11-06 23:20:39 -0800

    Rewrite constants

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+/// <reference path="globals.ts" />
+
+export const encodingTypes = [X, Y, ROW, COL, SIZE, SHAPE, COLOR, TEXT, DETAIL];
+
+export const shorthand = {
+  delim:  '|',
+  assign: '=',
+  type:   ',',
+  func:   '_'
+};
