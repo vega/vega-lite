8b853f586c6f69d96b375c9d1d247a00b1d347ec
Dominik Moritz <domoritz@gmail.com>
2015-11-06 23:19:59 -0800

    Rewrite enc

--- src/enc.ts
+++ src/enc.ts
@@ -1,17 +1,13 @@
 // utility for enc
 
-'use strict';
+import {shorthand as c} from './consts';
+import * as vlEncDef from './encdef';
+import * as util from './util';
 
-var consts = require('./consts'),
-  c = consts.shorthand,
-  vlEncDef = require('./encdef'),
-  util = require('./util'),
-  schema = require('./schema/schema'),
+var schema = require('./schema/schema'),
   encTypes = schema.encTypes;
 
-var vlenc = module.exports = {};
-
-vlenc.countRetinal = function(enc) {
+export function countRetinal(enc) {
   var count = 0;
   if (enc.color) count++;
   if (enc.size) count++;
@@ -19,54 +15,54 @@ vlenc.countRetinal = function(enc) {
   return count;
 };
 
-vlenc.has = function(enc, encType) {
+export function has(enc, encType) {
   var fieldDef = enc && enc[encType];
   return fieldDef && fieldDef.name;
 };
 
-vlenc.isAggregate = function(enc) {
+export function isAggregate(enc) {
   for (var k in enc) {
-    if (vlenc.has(enc, k) && enc[k].aggregate) {
+    if (has(enc, k) && enc[k].aggregate) {
       return true;
     }
   }
   return false;
 };
 
-vlenc.forEach = function(enc, f) {
+export function forEach(enc, f) {
   var i = 0;
   encTypes.forEach(function(k) {
-    if (vlenc.has(enc, k)) {
+    if (has(enc, k)) {
       f(enc[k], k, i++);
     }
   });
 };
 
-vlenc.map = function(enc, f) {
+export function map(enc, f) {
   var arr = [];
   encTypes.forEach(function(k) {
-    if (vlenc.has(enc, k)) {
+    if (has(enc, k)) {
       arr.push(f(enc[k], k, enc));
     }
   });
   return arr;
 };
 
-vlenc.reduce = function(enc, f, init) {
+export function reduce(enc, f, init) {
   var r = init;
   encTypes.forEach(function(k) {
-    if (vlenc.has(enc, k)) {
+    if (has(enc, k)) {
       r = f(r, enc[k], k,  enc);
     }
   });
   return r;
 };
 
-/*
+/**
  * return key-value pairs of field name and list of fields of that field name
  */
-vlenc.fields = function(enc) {
-  return vlenc.reduce(enc, function (m, field) {
+export function fields(enc) {
+  return reduce(enc, function (m, field) {
     var fieldList = m[field.name] = m[field.name] || [],
       containsType = fieldList.containsType = fieldList.containsType || {};
 
@@ -79,13 +75,13 @@ vlenc.fields = function(enc) {
   }, {});
 };
 
-vlenc.shorthand = function(enc) {
-  return vlenc.map(enc, function(field, et) {
+export function shorthand(enc) {
+  return map(enc, function(field, et) {
     return et + c.assign + vlEncDef.shorthand(field);
   }).join(c.delim);
 };
 
-vlenc.fromShorthand = function(shorthand) {
+export function fromShorthand(shorthand) {
   var enc = util.isArray(shorthand) ? shorthand : shorthand.split(c.delim);
   return enc.reduce(function(m, e) {
     var split = e.split(c.assign),
