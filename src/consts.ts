ebdd30bb189f9ebf1191c39e9e70686571d94767
Dominik Moritz <domoritz@gmail.com>
2015-11-07 22:33:00 -0800

    Move globals to consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,22 @@
-/// <reference path="globals.ts" />
+export const SUMMARY = 'summary'
+export const SOURCE = 'source'
+export const STACKED = 'stacked'
+export const INDEX = 'index'
+
+export const X = 'x'
+export const Y = 'y'
+export const ROW = 'row'
+export const COL = 'col'
+export const SIZE = 'size'
+export const SHAPE = 'shape'
+export const COLOR = 'color'
+export const TEXT = 'text'
+export const DETAIL = 'detail'
+
+export const N = 'N'
+export const O = 'O'
+export const Q = 'Q'
+export const T = 'T'
 
 export const encodingTypes = [X, Y, ROW, COL, SIZE, SHAPE, COLOR, TEXT, DETAIL];
 
