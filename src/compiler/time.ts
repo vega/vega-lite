9a553305333038f530bfe8c616206c16b2f0a3f8
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-08 22:00:48 -0800

    eliminate `exports.`

--- src/compiler/time.ts
+++ src/compiler/time.ts
@@ -91,8 +91,8 @@ export function scales(encoding) {
   var scales = encoding.reduce(function(scales, encDef) {
     var timeUnit = encDef.timeUnit;
     if (encDef.type === T && timeUnit && !scales[timeUnit]) {
-      var scale = exports.scale.def(encDef.timeUnit, encoding);
-      if (scale) scales[timeUnit] = scale;
+      var scaleDef = scale.def(encDef.timeUnit, encoding);
+      if (scaleDef) scales[timeUnit] = scaleDef;
     }
     return scales;
   }, {});
