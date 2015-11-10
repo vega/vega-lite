89aef1a44a66fe67391e8cc74780de098796613d
Dominik Moritz <domoritz@gmail.com>
2015-11-09 22:34:42 -0800

    get string constant before we access _enc

--- src/Encoding.ts
+++ src/Encoding.ts
@@ -86,18 +86,21 @@ export default class Encoding {
   };
 
   has(encType: Enctype) {
+    const name = Enctype[encType];
     // equivalent to calling vlenc.has(this._enc, encType)
-    return this._enc[encType].name !== undefined;
+    return this._enc[name].name !== undefined;
   };
 
   encDef(encType: Enctype) {
-    return this._enc[encType];
+    const name = Enctype[encType];
+    return this._enc[name];
   };
 
   // get "field" reference for vega
   fieldRef(encType: Enctype, opt?) {
+    const name = Enctype[encType];
     opt = opt || {};
-    return vlEncDef.fieldRef(this._enc[encType], opt);
+    return vlEncDef.fieldRef(this._enc[name], opt);
   };
 
   /*
@@ -107,24 +110,27 @@ export default class Encoding {
     return vlEnc.fields(this._enc);
   };
 
-  fieldTitle(et) {
-    if (vlEncDef.isCount(this._enc[et])) {
+  fieldTitle(encType: Enctype) {
+    const name = Enctype[encType];
+    if (vlEncDef.isCount(this._enc[name])) {
       return vlEncDef.COUNT_DISPLAYNAME;
     }
-    var fn = this._enc[et].aggregate || this._enc[et].timeUnit || (this._enc[et].bin && 'bin');
+    var fn = this._enc[name].aggregate || this._enc[name].timeUnit || (this._enc[name].bin && 'bin');
     if (fn) {
-      return fn.toUpperCase() + '(' + this._enc[et].name + ')';
+      return fn.toUpperCase() + '(' + this._enc[name].name + ')';
     } else {
-      return this._enc[et].name;
+      return this._enc[name].name;
     }
   };
 
-  scale(et: Enctype) {
-    return this._enc[et].scale || {};
+  scale(encType: Enctype) {
+    const name = Enctype[encType];
+    return this._enc[name].scale || {};
   };
 
-  axis(et: Enctype) {
-    return this._enc[et].axis || {};
+  axis(encType: Enctype) {
+    const name = Enctype[encType];
+    return this._enc[name].axis || {};
   };
 
   bandWidth(encType: Enctype, useSmallBand?: boolean) {
@@ -187,8 +193,9 @@ export default class Encoding {
     return vlEnc.forEach(this._enc, f);
   };
 
-  type(et): Type {
-    return this.has(et) ? this._enc[et].type : null;
+  type(encType: Enctype): Type {
+    const name = Enctype[encType];
+    return this.has(encType) ? this._enc[name].type : null;
   };
 
   isType(et: Enctype, type: Type) {
@@ -213,15 +220,15 @@ export default class Encoding {
     return vlEncDef.isMeasure(encoding.encDef(encType));
   };
 
-  isOrdinalScale(encType) {
+  isOrdinalScale(encType: Enctype) {
     return this.has(encType) && Encoding.isOrdinalScale(this, encType);
   };
 
-  isDimension(encType) {
+  isDimension(encType: Enctype) {
     return this.has(encType) && Encoding.isDimension(this, encType);
   };
 
-  isMeasure(encType) {
+  isMeasure(encType: Enctype) {
     return this.has(encType) && Encoding.isMeasure(this, encType);
   };
 
