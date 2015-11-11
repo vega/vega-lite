3eaac65afa6b42a44539ed720d7bc8f128523a2a
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-10 23:51:52 -0800

    config

---
+++ src/schema/config.schema.ts
@@ -0,0 +1,174 @@
+export var config = {
+  type: 'object',
+  properties: {
+    // template
+    width: {
+      type: 'integer',
+      default: undefined
+    },
+    height: {
+      type: 'integer',
+      default: undefined
+    },
+    viewport: {
+      type: 'array',
+      items: {
+        type: 'integer'
+      },
+      default: undefined
+    },
+    gridColor: {
+      type: 'string',
+      role: 'color',
+      default: '#000000'
+    },
+    gridOpacity: {
+      type: 'number',
+      minimum: 0,
+      maximum: 1,
+      default: 0.08
+    },
+
+    // filter null
+    // TODO(#597) revise this config
+    filterNull: {
+      type: 'object',
+      properties: {
+        N: {type:'boolean', default: false},
+        O: {type:'boolean', default: false},
+        Q: {type:'boolean', default: true},
+        T: {type:'boolean', default: true}
+      }
+    },
+    autoSortLine: {
+      type: 'boolean',
+      default: true
+    },
+
+    // single plot
+    singleHeight: {
+      // will be overwritten by bandWidth * (cardinality + padding)
+      type: 'integer',
+      default: 200,
+      minimum: 0
+    },
+    singleWidth: {
+      // will be overwritten by bandWidth * (cardinality + padding)
+      type: 'integer',
+      default: 200,
+      minimum: 0
+    },
+    // band size
+    largeBandWidth: {
+      type: 'integer',
+      default: 21,
+      minimum: 0
+    },
+    smallBandWidth: {
+      //small multiples or single plot with high cardinality
+      type: 'integer',
+      default: 12,
+      minimum: 0
+    },
+    largeBandMaxCardinality: {
+      type: 'integer',
+      default: 10
+    },
+    padding: {
+      type: 'number',
+      default: 1,
+      description: 'default scale padding for ordinal x/y scales.'
+    },
+    // small multiples
+    cellPadding: {
+      type: 'number',
+      default: 0.1,
+      description: 'default scale padding for row/column scales.'
+    },
+    cellGridColor: {
+      type: 'string',
+      role: 'color',
+      default: '#000000'
+    },
+    cellGridOpacity: {
+      type: 'number',
+      minimum: 0,
+      maximum: 1,
+      default: 0.25
+    },
+    cellGridOffset: {
+      type: 'number',
+      default: 6 // equal to tickSize
+    },
+    cellBackgroundColor: {
+      type: 'string',
+      role: 'color',
+      default: 'rgba(0,0,0,0)'
+    },
+    textCellWidth: {
+      type: 'integer',
+      default: 90,
+      minimum: 0
+    },
+
+    // marks
+    strokeWidth: {
+      type: 'integer',
+      default: 2,
+      minimum: 0
+    },
+    singleBarOffset: {
+      type: 'integer',
+      default: 5,
+      minimum: 0
+    },
+    // scales
+    timeScaleLabelLength: {
+      type: 'integer',
+      default: 3,
+      minimum: 0,
+      description: 'Max length for values in dayScaleLabel and monthScaleLabel.  Zero means using full names in dayScaleLabel/monthScaleLabel.'
+    },
+    dayScaleLabel: {
+      type: 'array',
+      items: {
+        type: 'string'
+      },
+      default: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      description: 'Axis labels for day of week, starting from Sunday.' +
+        '(Consistent with Javascript -- See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay.'
+    },
+    monthScaleLabel: {
+      type: 'array',
+      items: {
+        type: 'string'
+      },
+      default: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      description: 'Axis labels for month.'
+    },
+    // other
+    characterWidth: {
+      type: 'integer',
+      default: 6
+    },
+    numberFormat: {
+      type: 'string',
+      default: 's',
+      description: 'D3 Number format for axis labels and text tables.'
+    },
+    timeFormat: {
+      type: 'string',
+      default: '%Y-%m-%d',
+      description: 'Date format for axis labels.'
+    },
+    useRawDomain: {
+      type: 'boolean',
+      default: false,
+      description: 'Use the source data range as scale domain instead of ' +
+                   'aggregated data for aggregate axis. ' +
+                   'This option does not work with sum or count aggregate' +
+                   'as they might have a substantially larger scale range.' +
+                   'By default, use value from config.useRawDomain.'
+    }
+  }
+};
