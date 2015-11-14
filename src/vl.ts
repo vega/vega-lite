b4f1c1ea201c969b597972ea5bdc5f94f810ff80
Dominik Moritz <domoritz@gmail.com>
2015-11-14 15:39:09 -0800

    Clean up the exports in vl.ts #723

--- src/vl.ts
+++ src/vl.ts
@@ -1,29 +1,24 @@
-import Encoding from './Encoding';
+import VlEncoding from './Encoding';
 
-import * as util from './util';
-import * as consts from './consts';
-import * as data from './data';
-import * as enc from './enc';
-import * as encDef from './encdef';
-import * as compiler from './compiler/compiler';
-import * as schema from './schema/schema';
+import * as vlData from './data';
+import * as vlEnc from './enc';
+import * as vlEncDef from './encdef';
+import * as vlCompiler from './compiler/compiler';
+import * as vlSchema from './schema/schema';
 
-import {format} from 'd3-format';
+import {format as d3Format} from 'd3-format';
 
-var vl:any = {};
+export * from './util';
+export * from './consts';
 
-util.extend(vl, consts, util);
+export {default as Encoding} from './Encoding';
+export var compiler = vlCompiler;
+export var compile = vlCompiler.compile;
+export var data = vlData;
+export var enc = vlEnc;
+export var encDef = vlEncDef;
+export var schema = vlSchema;
+export var toShorthand = VlEncoding.shorthand;
+export var format = d3Format;
 
-vl.Encoding = Encoding;
-vl.compiler = compiler;
-vl.compile = vl.compiler.compile;
-vl.data = data;
-vl.enc = enc;
-vl.encDef = encDef;
-vl.schema = schema;
-vl.toShorthand = vl.Encoding.shorthand;
-vl.format = format;
-vl.version = '__VERSION__';
-
-declare var module;
-module.exports = vl;
+export const version = '__VERSION__';
