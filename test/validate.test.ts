99368b506476960a4a3b485e6bcff057badb0782
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-14 10:30:11 -0800

    add test for vl.validate

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {getEncodingMappingError} from '../src/validate';
 
 var ZSchema = require('z-schema'),
   inspect = require('util').inspect,
@@ -9,6 +10,78 @@ var vl = require('../src/vl'),
   vgSchema = require('../node_modules/vega/vega-schema.json'),
   examples = require('./examples');
 
+describe('vl.validate', function() {
+  describe('getEncodingMappingError()', function () {
+    it('should return no error for valid specs', function() {
+      expect(getEncodingMappingError({
+        marktype: 'bar',
+        encoding: {
+          x: {name: 'a'}
+        }
+      })).to.be.null;
+
+      expect(getEncodingMappingError({
+        marktype: 'line',
+        encoding: {
+          x: {name: 'b'},
+          y: {name: 'a'}
+        }
+      })).to.be.null;
+
+      expect(getEncodingMappingError({
+        marktype: 'area',
+        encoding: {
+          x: {name: 'a'},
+          y: {name: 'b'}
+        }
+      })).to.be.null;
+    });
+
+    it('should return error for invalid specs', function() {
+      expect(getEncodingMappingError({
+        marktype: 'line',
+        encoding: {
+          x: {name: 'b'} // missing y
+        }
+      })).to.be.ok;
+
+      expect(getEncodingMappingError({
+        marktype: 'area',
+        encoding: {
+          y: {name: 'b'} // missing x
+        }
+      })).to.be.ok;
+
+      expect(getEncodingMappingError({
+        marktype: 'text',
+        encoding: {
+          y: {name: 'b'} // missing text
+        }
+      })).to.be.ok;
+
+      expect(getEncodingMappingError({
+        marktype: 'line',
+        encoding: {
+          shape: {name: 'b'} // using shape with line
+        }
+      })).to.be.ok;
+
+      expect(getEncodingMappingError({
+        marktype: 'area',
+        encoding: {
+          shape: {name: 'b'} // using shape with area
+        }
+      })).to.be.ok;
+
+      expect(getEncodingMappingError({
+        marktype: 'bar',
+        encoding: {
+          shape: {name: 'b'} // using shape with bar
+        }
+      })).to.be.ok;
+    });
+  });
+});
 
 describe('Examples', function() {
 
