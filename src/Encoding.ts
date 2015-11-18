72b41a8cd6713844412131d7c6a8396f69c0c1fb
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-17 19:49:51 -0800

    head start on spec

--- src/Encoding.ts
+++ src/Encoding.ts
@@ -315,15 +315,4 @@ export default class Encoding {
   config(name) {
     return this._config[name];
   }
-
-  static transpose(spec) {
-    var oldenc = spec.encoding,
-      enc = util.duplicate(spec.encoding);
-    enc.x = oldenc.y;
-    enc.y = oldenc.x;
-    enc.row = oldenc.col;
-    enc.col = oldenc.row;
-    spec.encoding = enc;
-    return spec;
-  }
 }
---
+++ src/spec.ts
@@ -0,0 +1,12 @@
+import * as util from './util';
+
+export function transpose(spec) {
+  var oldenc = spec.encoding,
+    enc = util.duplicate(spec.encoding);
+  enc.x = oldenc.y;
+  enc.y = oldenc.x;
+  enc.row = oldenc.col;
+  enc.col = oldenc.row;
+  spec.encoding = enc;
+  return spec;
+};
