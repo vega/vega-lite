4c5a9e46bb6c03a29aa8c033e9467b159b173728
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-11-19 10:35:17 -0800

    add `alwaysNoOcclusion`, `getCleanSpec`

--- src/specification.ts
+++ src/specification.ts
@@ -1,3 +1,17 @@
+import * as vlEnc from './enc';
+import {Model} from './compiler/Model';
+
+
+export function alwaysNoOcclusion(spec) {
+  // FIXME raw OxQ with # of rows = # of O
+  return vlEnc.isAggregate(spec.encoding);
+}
+
+export function getCleanSpec(spec) {
+  // TODO: move toSpec to here!
+  return new Model(spec).toSpec(true);
+}
+
 // TODO revise
 export function transpose(spec) {
   var oldenc = spec.encoding,
