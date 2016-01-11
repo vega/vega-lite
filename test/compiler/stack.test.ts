606490f8125078eeb7b70799653cb587d6e84c12
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-01-10 17:38:20 -0800

    correctly write unit test for stack without calling the whole compile method

--- test/compiler/stack.test.ts
+++ test/compiler/stack.test.ts
@@ -1,35 +1,19 @@
 import {expect} from 'chai';
 
 import {stack} from '../fixtures';
-import {compile} from '../../src/compiler/compiler';
+import {Model} from '../../src/compiler/model';
+import {compileData} from '../../src/compiler/data';
+import {compileRootGroup} from '../../src/compiler/compiler';
 import {SUMMARY} from '../../src/data';
-
-const stats = {
-  'Cost__Total_$': {
-    min: 0,
-    max: 100,
-    length: { max: 5}
-  },
-  'Cost__Other': {
-    min: 0,
-    max: 100,
-    length: { max: 5}
-  },
-  'Effect__Amount_of_damage': {
-    min: 0,
-    max: 100,
-    length: { max: 5}
-  }
-};
 
 describe('vl.compile.stack()', function () {
 
   describe('bin-x', function () {
-    it('should put stack on y', function () {
-      // FIXME don't run the whole compile
-      var vgSpec = compile(stack.binX, stats).spec;
+    const model = new Model(stack.binX);
+    const dataSpec = compileData(model);
+    it('should aggregate data correctly', function () {
 
-      var tableData = vgSpec.data.filter(function(data) {
+      var tableData = dataSpec.filter(function(data) {
         return data.name === SUMMARY;
       });
       expect(tableData.length).to.equal(1);
@@ -39,22 +23,34 @@ describe('vl.compile.stack()', function () {
       })[0];
       expect(tableAggrTransform.groupby.length).to.equal(4);
       expect(tableAggrTransform.groupby.indexOf('bin_Cost__Total_$_start')).to.gt(-1);
+    });
 
-      var stackedData = vgSpec.data.filter(function(data) {
+    it('should create stack summary data correctly', function() {
+      var stackedData = dataSpec.filter(function(data) {
         return data.name === 'stacked';
       });
       expect(stackedData.length).to.equal(1);
       var stackedAggrTransform = stackedData[0].transform[0];
       expect(stackedAggrTransform.groupby[0]).to.equal('bin_Cost__Total_$_start');
     });
+
+    it('should stack data correctly', function() {
+      const rootGroup = compileRootGroup(model);
+      const stackTransform = rootGroup.marks[0].from.transform[0];
+      expect(stackTransform.type).to.equal('stack');
+
+      expect(stackTransform.groupby).to.eql(['bin_Cost__Total_$_start']);
+      expect(stackTransform.field).to.eql('sum_Cost__Other');
+      expect(stackTransform.sortby).to.eql(['-Effect__Amount_of_damage']);
+    });
   });
 
   describe('bin-y', function () {
-    it('should put stack on x', function () {
-      // FIXME don't run the whole compile
-      var vgSpec = compile(stack.binY, stats).spec;
+    const model = new Model(stack.binY);
+    const dataSpec = compileData(model);
+    it('should aggregate data correctly', function () {
 
-      var tableData = vgSpec.data.filter(function(data) {
+      var tableData = dataSpec.filter(function(data) {
         return data.name === SUMMARY;
       });
       expect(tableData.length).to.equal(1);
@@ -64,14 +60,26 @@ describe('vl.compile.stack()', function () {
       })[0];
       expect(tableAggrTransform.groupby.length).to.equal(4);
       expect(tableAggrTransform.groupby.indexOf('bin_Cost__Total_$_start')).to.gt(-1);
+    });
 
-      var stackedData = vgSpec.data.filter(function(data) {
+    it('should create stack summary data correctly', function() {
+      var stackedData = dataSpec.filter(function(data) {
         return data.name === 'stacked';
       });
 
       expect(stackedData.length).to.equal(1);
       var stackedAggrTransform = stackedData[0].transform[0];
       expect(stackedAggrTransform.groupby[0]).to.equal('bin_Cost__Total_$_start');
+    });
+
+    it('should stack data correctly', function() {
+      const rootGroup = compileRootGroup(model);
+      const stackTransform = rootGroup.marks[0].from.transform[0];
+      expect(stackTransform.type).to.equal('stack');
+
+      expect(stackTransform.groupby).to.eql(['bin_Cost__Total_$_start']);
+      expect(stackTransform.field).to.eql('sum_Cost__Other');
+      expect(stackTransform.sortby).to.eql(['-Effect__Amount_of_damage']);
     });
   });
 });
