6e5f4324d102c2b0c28c00a936e3c586a7dba874
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-01-15 21:22:54 -0800

    check examples, add a test

--- test/compile/mark-text.test.ts
+++ test/compile/mark-text.test.ts
@@ -1,5 +1,42 @@
 /* tslint:disable quote */
 
+import {assert} from 'chai';
+import {parseModel} from '../util';
+import {extend} from '../../src/util';
+import {text} from '../../src/compile/mark-text';
+import {X, Y} from '../../src/channel';
+
 describe('Mark: Text', function() {
-  // TODO: write
+  function textXY(moreEncoding = {}) {
+    const spec = {
+      "mark": "text",
+      "encoding": extend(
+        {
+          "x": {"field": "Acceleration", "type": "ordinal"},
+          "y": {"field": "Displacement", "type": "quantitative"}
+        },
+        moreEncoding
+      ),
+      "data": {"url": "data/cars.json"}
+    };
+    return spec;
+  }
+
+  describe('with x, y', function() {
+    const model = parseModel(textXY());
+    const props = text.properties(model);
+
+    it('should scale on x', function() {
+      assert.deepEqual(props.x, {scale: X, field: 'Acceleration'});
+    });
+    it('should scale on y', function(){
+      assert.deepEqual(props.y, {scale: Y, field: 'Displacement'});
+    });
+
+    it('should be centered', function() {
+      assert.deepEqual(props.align, {value: "center"});
+    });
+
+
+  });
 });
