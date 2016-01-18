1e0130aa684b3f7d58d4ac7a5f94fb864e3111cf
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-01-18 10:03:37 -0800

    consolidate point-based marks + make square and circle support `config.mark.filled` (Fixes #963) + unit-test scaffolding

--- src/compile/util.ts
+++ src/compile/util.ts
@@ -3,17 +3,21 @@ import {COLOR} from '../channel';
 
 export enum ColorMode {
   ALWAYS_FILLED,
-  ALWAYS_STROKED
+  ALWAYS_STROKED,
+  FILLED_BY_DEFAULT,
+  STROKED_BY_DEFAULT
 }
 
 export const FILL_STROKE_CONFIG = ['fill', 'fillOpacity',
   'stroke', 'strokeWidth', 'strokeDash', 'strokeDashOffset', 'strokeOpacity',
   'opacity'];
 
-export function applyColorAndOpacity(p, model: Model, colorMode?: ColorMode) {
+export function applyColorAndOpacity(p, model: Model, colorMode: ColorMode = ColorMode.STROKED_BY_DEFAULT) {
   const filled = colorMode === ColorMode.ALWAYS_FILLED ? true :
     colorMode === ColorMode.ALWAYS_STROKED ? false :
-    model.config().mark.filled;
+    model.config().mark.filled !== undefined ? model.config().mark.filled :
+    colorMode  === ColorMode.FILLED_BY_DEFAULT ? true :
+    false; // ColorMode.STROKED_BY_DEFAULT
 
   // Apply fill and stroke config first
   // so that `color.value` can override `fill` and `stroke` config
