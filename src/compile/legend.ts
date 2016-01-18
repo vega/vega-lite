4a05ab795d1d93a7e58372a6a609c05bd67d529f
Dominik Moritz <domoritz@gmail.com>
2016-01-17 17:00:49 -0800

    Don’t add format to legends if we use bin because we have a special format that displays the range.

--- src/compile/legend.ts
+++ src/compile/legend.ts
@@ -37,7 +37,7 @@ export function compileLegend(model: Model, channel: Channel, def) {
 
   // 1.1 Add properties with special rules
   def.title = title(fieldDef);
-  model.format(channel, (typeof legend !== 'boolean' && legend.format) || undefined, def);
+  format(model, channel, def);
 
   // 1.2 Add properties without rules
   ['orient', 'values'].forEach(function(property) {
@@ -69,6 +69,16 @@ export function title(fieldDef: FieldDef) {
   }
 
   return fieldTitle(fieldDef);
+}
+
+export function format(model: Model, channel: Channel, def) {
+  const fieldDef = model.fieldDef(channel);
+  const legend = fieldDef.legend;
+
+  // If the channel is binned, we should not set the formatter because we have a range label
+  if (!fieldDef.bin) {
+    model.format(channel, (typeof legend !== 'boolean' && legend.format) || undefined, def);
+  }
 }
 
 namespace properties {
