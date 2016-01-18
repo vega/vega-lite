d76c290766eb92d8b23face312c24ae14d9fcf99
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-01-17 17:13:07 -0800

    Update bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -4,10 +4,9 @@ export function autoMaxBins(channel: Channel): number {
   switch (channel) {
     case ROW:
     case COLUMN:
-      // Facet shouldn't have too many bins
-      // We choose 6 like shape to simplify the rule
     case SIZE:
-      // It is really hard to distinguish different sizes
+      // Facets and Size shouldn't have too many bins
+      // We choose 6 like shape to simplify the rule
     case SHAPE:
       return 6; // Vega's "shape" has 6 distinct values
     default:
