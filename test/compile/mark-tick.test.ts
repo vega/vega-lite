2439b0492e1fe296fb9e7b68d3755926b9f7d4ac
Yuhan (Zoe) Lu <yuhanlu9@cs.washington.edu>
2016-01-26 13:37:23 -0800

    Add mark tick test

--- test/compile/mark-tick.test.ts
+++ test/compile/mark-tick.test.ts
@@ -5,6 +5,9 @@
 // After finishing all test, make sure all lines in mark-tick.ts is tested
 // (except the scaffold labels() method)
 import {assert} from 'chai';
+import {parseModel} from '../util';
+import {extend} from '../../src/util';
+import {X, Y} from '../../src/channel';
 import {tick} from '../../src/compile/mark-tick';
 
 describe('Mark: Tick', function() {
@@ -12,33 +15,65 @@ describe('Mark: Tick', function() {
     assert.equal(tick.markType(), 'rect');
   });
 
+  function tickXY(moreEncoding = {}) {
+    const spec = {
+      'mark': 'tick',
+      'encoding': extend(
+        {
+          'x': {'field': 'Horsepower', 'type': 'quantitative'},
+          'y': {'field': 'Cylinders','type': 'ordinal'}
+        },
+        moreEncoding
+      ),
+      'data': {'url': 'data/cars.json'},
+    };
+    return spec;
+  }
+
   describe('with quantitative x', function() {
+    const model = parseModel({
+      'mark': 'tick',
+      'encoding': {'x': {'field': 'Horsepower', 'type': 'quantitative'}},
+      'data': {'url': 'data/cars.json'}
+    });
+
+    const props = tick.properties(model);
     it('should be centered on y', function() {
-      // TODO
+      assert.deepEqual(props.yc, {value: 10.5});
     });
 
     it('should scale on x', function() {
-      // TODO
+      assert.deepEqual(props.xc, {scale: X, field: 'Horsepower'});
     });
   });
 
   describe('with quantitative y', function() {
+    const model = parseModel({
+      'mark': 'tick',
+      'encoding': {'y': {'field': 'Cylinders','type': 'quantitative'}},
+      'data': {'url': 'data/cars.json'}
+    });
+
+    const props = tick.properties(model);
     it('should be centered on x', function() {
-      // TODO
+      assert.deepEqual(props.xc, {value: 10.5});
     });
 
     it('should scale on y', function() {
-      // TODO
+      assert.deepEqual(props.yc, {scale: Y, field: 'Cylinders'});
     });
   });
 
   describe('with quantitative x and ordinal y', function() {
+    const model = parseModel(tickXY());
+    const props = tick.properties(model);
+
     it('should scale on x', function() {
-      // TODO
+      assert.deepEqual(props.xc, {scale: X, field: 'Horsepower'});
     });
 
     it('should scale on y', function() {
-      // TODO
+      assert.deepEqual(props.yc, {scale: Y, field: 'Cylinders'});
     });
   });
 });
