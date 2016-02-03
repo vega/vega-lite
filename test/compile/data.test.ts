724bb4a37b9d619ada589bd6b82384936f12770a
Youying Lin <liny33@cs.washington.edu>
2016-02-02 22:23:08 -0800

    Fix layout calculation.
    Make cellWidth under layout to be fixed when we automatically augment domain.
    
    Test: All time units but date, date currently is not doing something unexpected.
    
    Issue: #981

--- test/compile/data.test.ts
+++ test/compile/data.test.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:quotemark */
 
 import {assert} from 'chai';
-import {compileData, source, summary, dates} from '../../src/compile/data';
+import {compileData, source, summary, dates, layout} from '../../src/compile/data';
 import {parseModel} from '../util';
 import {mergeDeep} from '../../src/schema/schemautil';
 
@@ -345,6 +345,213 @@ describe('data.summary', function () {
           'Acceleration': ['mean']
         }
       }]
+    });
+  });
+});
+
+describe('Handle fix mode', function() {
+  it('seconds should have cardinality of 60', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "seconds"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "seconds_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(60 + 1) * 21"
+        }]
+    });
+  });
+
+  it('minutes should have cardinality of 60', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "minutes"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "minutes_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(60 + 1) * 21"
+        }]
+    });
+  });
+
+  it('hours should have cardinality of 24', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "hours"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "hours_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(24 + 1) * 21"
+        }]
+    });
+  });
+
+  it('day should have cardinality of 7', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "day"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "day_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(7 + 1) * 21"
+        }]
+    });
+  });
+
+/*
+  it('date should have cardinality of 31', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "date"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "date_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(31 + 1) * 21"
+        }]
+    });
+  });
+*/
+  it('month should have cardinality of 12', function() {
+    const model = parseModel({
+        "mark": "line",
+        "encoding": {
+          "x": {
+            "field": "Year",
+            "type": "temporal",
+            "timeUnit": "month"
+          },
+          "y": {
+            "aggregate": "mean",
+            "field": "Horsepower",
+            "type": "quantitative"
+          }
+        }
+      });
+
+    const defs = layout.def(model);
+    assert.deepEqual(defs, {
+      "name": "layout",
+      "source": "summary",
+      "transform": [
+        {
+          "type": "aggregate",
+          "summarize": [{"field": "month_Year","ops": ["distinct"]}]
+        },
+        {
+          "type": "formula",
+          "field": "cellWidth",
+          "expr": "(12 + 1) * 21"
+        }]
     });
   });
 });
