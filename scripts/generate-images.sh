7145295d266521a1ac7d1e6f2ab3cf0c77fba742
Dominik Moritz <domoritz@gmail.com>
2016-02-15 19:06:37 -0800

    Don’t output .json in filenames

--- scripts/generate-images.sh
+++ scripts/generate-images.sh
@@ -5,11 +5,11 @@ mkdir examples/images
 echo "Generating PNGs..."
 for file in examples/specs/*.json; do
   name=${file##*/}
-  bin/vl2png $file examples/images/${name}.png
+  bin/vl2png $file examples/images/${name%.*}.png
 done
 
 echo "Generating SVGs..."
 for file in examples/specs/*.json; do
   name=${file##*/}
-  bin/vl2svg $file examples/images/${name}.svg
+  bin/vl2svg $file examples/images/${name%.*}.svg
 done
