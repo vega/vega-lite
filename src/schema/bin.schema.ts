03abbe228304a613d2c3e67d5e26b324d7731b5d
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-02-16 16:13:04 -0800

    Rename `Bin` to `BinProperties` because in fact `Bin = boolean | BinProperties` + remove all `default: undefined`

--- src/schema/bin.schema.ts
+++ src/schema/bin.schema.ts
@@ -1,7 +1,7 @@
 import {QUANTITATIVE} from '../type';
 import {toMap} from '../util';
 
-export interface Bin {
+export interface BinProperties {
   min?: number;
   max?: number;
   base?: number;
@@ -12,50 +12,43 @@ export interface Bin {
   maxbins?: number;
 }
 
-export var bin = {
-  default: false,
+
+export var binProperties = {
+  default: false, // TODO: remove this once we remove schema.instantiate
   description: 'binning properties or boolean flag for determining whether to bin data or not.',
   oneOf: [{
     type: 'object',
     properties: {
       min: {
         type: 'number',
-        default: undefined,
         description: 'The minimum bin value to consider. If unspecified, the minimum value of the specified field is used.'
       },
       max: {
         type: 'number',
-        default: undefined,
         description: 'The maximum bin value to consider. If unspecified, the maximum value of the specified field is used.'
       },
       base: {
         type: 'number',
-        default: undefined,
         description: 'The number base to use for automatic bin determination (default is base 10).'
       },
       step: {
         type: 'number',
-        default: undefined,
         description: 'An exact step size to use between bins. If provided, options such as maxbins will be ignored.'
       },
       steps: {
         type: 'array',
-        default: undefined,
         description: 'An array of allowable step sizes to choose from.'
       },
       minstep: {
         type: 'number',
-        default: undefined,
         description: 'A minimum allowable step size (particularly useful for integer values).'
       },
       div: {
         type: 'array',
-        default: undefined,
         description: 'Scale factors indicating allowable subdivisions. The default value is [5, 2], which indicates that for base 10 numbers (the default base), the method may consider dividing bin sizes by 5 and/or 2. For example, for an initial step size of 10, the method can check if bin sizes of 2 (= 10/5), 5 (= 10/2), or 1 (= 10/(5*2)) might also satisfy the given constraints.'
       },
       maxbins: {
         type: 'integer',
-        default: undefined,
         minimum: 2,
         description: 'Maximum number of bins.'
       }
--- src/schema/fielddef.schema.ts
+++ src/schema/fielddef.schema.ts
@@ -1,5 +1,5 @@
 import {axis, AxisProperties} from './axis.schema';
-import {bin, Bin} from './bin.schema';
+import {binProperties, BinProperties} from './bin.schema';
 import {legend, LegendProperties} from './legend.schema';
 import {typicalScale, ordinalOnlyScale, Scale} from './scale.schema';
 import {sortEnum, sort, SortField, SortEnum} from './sort.schema';
@@ -22,8 +22,7 @@ export interface FieldDef {
 
   // function
   timeUnit?: string;
-  bin?: boolean | Bin;
-
+  bin?: boolean | BinProperties;
   aggregate?: string;
 
   // TODO: remove these one by one
@@ -60,7 +59,7 @@ const fieldDef = {
       type: 'string',
       enum: [NOMINAL, ORDINAL, QUANTITATIVE, TEMPORAL]
     },
-    bin: bin,
+    bin: binProperties,
     timeUnit: {
       type: 'string',
       enum: TIMEUNITS,
