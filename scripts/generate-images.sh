20064d149f9bcfb439e5645d8ac00f7fda958a73
Dominik Moritz <domoritz@gmail.com>
2016-02-17 17:06:48 -0800

    Only generate svgs

--- scripts/generate-images.sh
+++ scripts/generate-images.sh
@@ -5,8 +5,5 @@ set -e
 
 mkdir -p examples/images
 
-echo "Generating PNGs..."
-ls examples/specs/*.json | parallel --eta --halt 1 "bin/vl2png {} examples/images/{/.}.png"
-
 echo "Generating SVGs..."
 ls examples/specs/*.json | parallel --eta --halt 1 "bin/vl2svg {} examples/images/{/.}.svg"
