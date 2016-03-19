78bc6a6cb519794f8d0e6891d7a7b7c2be2697cb
Dominik Moritz <domoritz@gmail.com>
2016-03-18 17:03:16 -0700

    hamlint

--- src/compile/layer.ts
+++ src/compile/layer.ts
@@ -246,7 +246,7 @@ export class LayerModel extends Model {
   public compatibleSource(child: UnitModel) {
     const sourceUrl = this.data().url;
     const childData = child.component.data;
-    const compatible = !childData.source || sourceUrl && sourceUrl === childData.source.url;
+    const compatible = !childData.source || (sourceUrl && sourceUrl === childData.source.url);
     return compatible;
 }
 }
