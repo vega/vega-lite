b0f01963346c6aaf85b344ca71d9da4d4e48e5c7
Dominik Moritz <domoritz@gmail.com>
2016-05-25 11:19:58 +0200

    Release 1.0.10 a6accf0a5c1b73825e22d60d9b838b0e4db73da5

---
+++ src/compile/time.js
@@ -0,0 +1,112 @@
+"use strict";
+var util_1 = require('../util');
+var channel_1 = require('../channel');
+var timeunit_1 = require('../timeunit');
+function smallestUnit(timeUnit) {
+    if (!timeUnit) {
+        return undefined;
+    }
+    if (timeUnit.indexOf('second') > -1) {
+        return 'second';
+    }
+    if (timeUnit.indexOf('minute') > -1) {
+        return 'minute';
+    }
+    if (timeUnit.indexOf('hour') > -1) {
+        return 'hour';
+    }
+    if (timeUnit.indexOf('day') > -1 || timeUnit.indexOf('date') > -1) {
+        return 'day';
+    }
+    if (timeUnit.indexOf('month') > -1) {
+        return 'month';
+    }
+    if (timeUnit.indexOf('year') > -1) {
+        return 'year';
+    }
+    return undefined;
+}
+exports.smallestUnit = smallestUnit;
+function parseExpression(timeUnit, fieldRef, onlyRef) {
+    if (onlyRef === void 0) { onlyRef = false; }
+    var out = 'datetime(';
+    var timeString = timeUnit.toString();
+    function get(fun, addComma) {
+        if (addComma === void 0) { addComma = true; }
+        if (onlyRef) {
+            return fieldRef + (addComma ? ', ' : '');
+        }
+        else {
+            return fun + '(' + fieldRef + ')' + (addComma ? ', ' : '');
+        }
+    }
+    if (timeString.indexOf('year') > -1) {
+        out += get('year');
+    }
+    else {
+        out += '2006, ';
+    }
+    if (timeString.indexOf('month') > -1) {
+        out += get('month');
+    }
+    else {
+        out += '0, ';
+    }
+    if (timeString.indexOf('day') > -1) {
+        out += get('day', false) + '+1, ';
+    }
+    else if (timeString.indexOf('date') > -1) {
+        out += get('date');
+    }
+    else {
+        out += '1, ';
+    }
+    if (timeString.indexOf('hours') > -1) {
+        out += get('hours');
+    }
+    else {
+        out += '0, ';
+    }
+    if (timeString.indexOf('minutes') > -1) {
+        out += get('minutes');
+    }
+    else {
+        out += '0, ';
+    }
+    if (timeString.indexOf('seconds') > -1) {
+        out += get('seconds');
+    }
+    else {
+        out += '0, ';
+    }
+    if (timeString.indexOf('milliseconds') > -1) {
+        out += get('milliseconds', false);
+    }
+    else {
+        out += '0';
+    }
+    return out + ')';
+}
+exports.parseExpression = parseExpression;
+function rawDomain(timeUnit, channel) {
+    if (util_1.contains([channel_1.ROW, channel_1.COLUMN, channel_1.SHAPE, channel_1.COLOR], channel)) {
+        return null;
+    }
+    switch (timeUnit) {
+        case timeunit_1.TimeUnit.SECONDS:
+            return util_1.range(0, 60);
+        case timeunit_1.TimeUnit.MINUTES:
+            return util_1.range(0, 60);
+        case timeunit_1.TimeUnit.HOURS:
+            return util_1.range(0, 24);
+        case timeunit_1.TimeUnit.DAY:
+            return util_1.range(0, 7);
+        case timeunit_1.TimeUnit.DATE:
+            return util_1.range(1, 32);
+        case timeunit_1.TimeUnit.MONTH:
+            return util_1.range(0, 12);
+    }
+    return null;
+}
+exports.rawDomain = rawDomain;
+//# sourceMappingURL=time.js.map
