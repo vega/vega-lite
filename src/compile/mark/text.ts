cd0aa2ce9da2ea7ad05c53b413954a6d6ece1a79
Youying Lin <bobocandys@gmail.com>
2016-06-27 19:05:02 +0800

    Make all paths return value.

--- src/compile/mark/text.ts
+++ src/compile/mark/text.ts
@@ -67,7 +67,7 @@ export namespace text {
         };
       }
     }
-
+    // TODO: support x.value, x.datum
     if (textFieldDef && textFieldDef.type === QUANTITATIVE) {
       return { field: { group: 'width' }, offset: -5 };
     } else {
@@ -98,27 +98,9 @@ export namespace text {
           field: field(sizeFieldDef)
         };
       }
-<<<<<<< HEAD
       if (sizeFieldDef.value) {
         return {value: sizeFieldDef.value};
       }
-=======
-    } else {
-      return { value: sizeValue(fieldDefSize, config) };
-    }
-  }
-
-  // when color is apply to background, only set opacity here.
-  function applyColorAndOpacity(p, model: UnitModel) {
-    if (model.config().mark.applyColorToBackground && !model.has(X) && !model.has(Y)) {
-      p.fill = {value: 'black'}; // TODO: add rules for swapping between black and white
-
-      // opacity
-      const opacity = model.config().mark.opacity;
-      if (opacity) { p.opacity = { value: opacity }; };
-    } else {
-      applyColorAndOpacity(p, model);
->>>>>>> Add local applyColorAndOpacity method
     }
     return { value: config.mark.fontSize };
   }
