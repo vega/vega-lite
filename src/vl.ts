69ff827df0f789d1648d8f1e4ae30e89fafd6c2e
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-07-06 13:11:32 -0700

    add missing imports

--- src/vl.ts
+++ src/vl.ts
@@ -1,15 +1,22 @@
-
+export import axis = require('./axis');
 export import aggregate = require('./aggregate');
-export const compile = require('./compile/compile').compile;
 export import bin = require('./bin');
 export import channel = require('./channel');
+export const compile = require('./compile/compile').compile;
 export import config = require('./config');
 export import data = require('./data');
 export import encoding = require('./encoding');
+export import facet = require('./facet');
 export import fieldDef = require('./fieldDef');
+export import legend = require('./legend');
+export import mark = require('./mark');
+export import scale = require('./scale');
 export import shorthand = require('./shorthand');
+export import sort = require('./sort');
 export import spec = require('./spec');
+export import stack = require('./stack');
 export import timeUnit = require('./timeUnit');
+export import transform = require('./transform');
 export import type = require('./type');
 export import util = require('./util');
 export import validate = require('./validate');
