02fee54710102571677ddc3f2e1bed01b4800284
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-07-07 14:04:10 -0700

    Add backward compatibility support for deprecated `formatType`.

--- src/compile/data/source.ts
+++ src/compile/data/source.ts
@@ -22,7 +22,7 @@ export namespace source {
 
       let sourceData: VgData = { name: model.dataName(SOURCE) };
       if (data.values && data.values.length > 0) {
-        sourceData.values = model.data().values;
+        sourceData.values = data.values;
         sourceData.format = { type: 'json' };
       } else if (data.url) {
         sourceData.url = data.url;
@@ -33,10 +33,13 @@ export namespace source {
         if (!contains(['json', 'csv', 'tsv', 'topojson'], defaultExtension)) {
           defaultExtension = 'json';
         }
-        const dataFormat: DataFormat = model.data().format || {};
+        const dataFormat: DataFormat = data.format || {};
+
+        // For backward compatability
+        const formatType: DataFormat = dataFormat.type || data['formatType'];
         sourceData.format =
           extend(
-            { type: dataFormat.type ? model.data().format.type : defaultExtension },
+            { type: formatType ? formatType : defaultExtension },
             dataFormat.property ? { property: dataFormat.property } : {},
             // Feature and mesh are two mutually exclusive properties
             dataFormat.feature ?
