dd7f280e00131f270ecaf1c106deba0618d9705c
Younghoon Kim <yhoonkim01@gmail.com>
2016-07-14 15:41:46 -0700

    Remove ComparateFilter and gt/lt/gte/lte stuffs.

--- test/compile/data/filter.test.ts
+++ test/compile/data/filter.test.ts
@@ -35,27 +35,7 @@ describe('compile/data/filter', () => {
       const expr = filter.getFilterExpression({field: 'x', range: [0, 5], negate: true});
       assert.equal(expr, '!(inrange(datum.x, 0, 5))');
     });
-
-    it('should return a correct expression for a RangeFilter having gt', () => {
-      const expr = filter.getFilterExpression({field: 'x', gt: 0});
-      assert.equal(expr, 'datum.x > 0');
-    });
-
-    it('should return a correct expression for a RangeFilter having lte', () => {
-      const expr = filter.getFilterExpression({field: 'x', lte: 0});
-      assert.equal(expr, 'datum.x <= 0');
-    });
-
-    it('should return a correct expression for a RangeFilter having combination of gte and lt', () => {
-      const expr = filter.getFilterExpression({field: 'x', gte: 0, lt: 10});
-      assert.equal(expr, 'datum.x >= 0 && datum.x < 10');
-    });
-
-    it('should return a correct expression for a RangeFilter having combination of gte and lt with negate', () => {
-      const expr = filter.getFilterExpression({field: 'x', gte: 0, lt: 10, negate: true});
-      assert.equal(expr, '!(datum.x >= 0 && datum.x < 10)');
-    });
-
+    
     it('should return a correct expression for an expression filter', () => {
       const expr = filter.getFilterExpression('datum.x===5');
       assert.equal(expr, 'datum.x===5');
@@ -72,7 +52,6 @@ describe('compile/data/filter', () => {
             {field: 'color', equal: 'red', negate: true},
             {field: 'color', in: ['red', 'yellow']},
             {field: 'x', range: [0, 5]},
-            {field: 'y', gt: 0, lte: 10 },
             'datum.x===5'
           ]
         }
@@ -81,7 +60,6 @@ describe('compile/data/filter', () => {
       assert.equal(expr, '(!(datum.color==="red")) && ' +
         '(indexof(["red","yellow"], datum.color) !== -1) && ' +
         '(inrange(datum.x, 0, 5)) && ' +
-        '(datum.y > 0 && datum.y <= 10) && ' +
         '(datum.x===5)');
     });
 
--- test/filter.test.ts
+++ test/filter.test.ts
@@ -1,13 +1,10 @@
 import {assert} from 'chai';
-import {isEqualFilter, isInFilter, isRangeFilter, isCompareFilter} from '../src/filter';
+import {isEqualFilter, isInFilter, isRangeFilter} from '../src/filter';
 
 describe('filter', () => {
   const equalFilter = {field: 'color', equal: 'red'};
   const inFilter = {field: 'color', in: ['red', 'yellow']};
   const rangeFilter = {field: 'x', range: [0, 5]};
-  const compareFilter1 = {field: 'x', gte: 0};
-  const compareFilter2 = {field: 'x', gte: 0, lt: 5 };
-
   const exprFilter = 'datum.x===5';
 
   describe('isEqualFilter', () => {
@@ -40,21 +37,8 @@ describe('filter', () => {
     });
 
     it('should return false for other filters', () => {
-      [inFilter, equalFilter, exprFilter, compareFilter1, compareFilter2].forEach((filter) => {
+      [inFilter, equalFilter, exprFilter].forEach((filter) => {
         assert.isFalse(isRangeFilter(filter));
-      });
-    });
-  });
-
-  describe('isCompareFilter', () => {
-    it('should return true for a compare filter', () => {
-      assert.isTrue(isCompareFilter(compareFilter1));
-      assert.isTrue(isCompareFilter(compareFilter2));
-    });
-
-    it('should return false for other filters', () => {
-      [inFilter, equalFilter, exprFilter, rangeFilter].forEach((filter) => {
-        assert.isFalse(isCompareFilter(filter));
       });
     });
   });
