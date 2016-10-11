acfb209f29d62a4a34e94d53f9d6d0370adfd1bb
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-10-11 17:52:49 +0700

    Add test for format parse

---
+++ test/compile/data/formatparse.test.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:quotemark */
+import {assert} from 'chai';
+
+import {parseUnitModel} from '../../util';
+import {formatParse} from '../../../src/compile/data/formatparse';
+
+describe('compile/data/formatparse', () => {
+  describe('parse', () => {
+    it('should return a correct parse for encoding mapping', () => {
+      const model = parseUnitModel({
+        "data": {"url": "a.json"},
+        "mark": "point",
+        "encoding": {
+          "x": {"field": "a", "type": "quantitative"},
+          "y": {"field": "b", "type": "temporal"},
+          "color": {"field": "c", "type": "ordinal"},
+          "shape": {"field": "d", "type": "nominal"}
+        }
+      });
+      const parseComponent = formatParse.parseUnit(model);
+      assert.deepEqual(parseComponent,{
+        a: 'number',
+        b: 'date'
+      });
+    });
+  });
+});
