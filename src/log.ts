9022a33b4631b5ff9a9a7b6f1e7d18486401ff62
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2016-10-29 17:51:42 -0700

    [WIP HACK] Copied and paste logger code instead of import

--- src/log.ts
+++ src/log.ts
@@ -2,8 +2,51 @@
  * Vega-Lite's singleton logger utility.
  */
 
-import Logger from 'vega-util/src/logger';
-import {LoggerInterface, Warn} from 'vega-util/src/logger';
+// import Logger from 'vega-util/src/logger';
+// import {LoggerInterface, Warn} from 'vega-util/src/logger';
+
+// ========== TEMPORARY SOLUTION FOR FAILED IMPORT (copied from vega-util/log) =========
+/* tslint:disable */
+export interface LoggerInterface {
+  level: (_: number) => number | LoggerInterface;
+  warn(...args): LoggerInterface;
+  info(...args): LoggerInterface;
+  debug(...args): LoggerInterface;
+}
+
+function log(method, level, input) {
+  var args = [level].concat([].slice.call(input));
+  console[method].apply(console, args); // eslint-disable-line no-console
+}
+
+export var None  = 0;
+export var Warn  = 1;
+export var Info  = 2;
+export var Debug = 3;
+
+
+export function Logger(_): LoggerInterface {
+  var level = _ || None;
+  return {
+    level: function(_) {
+      return arguments.length ? (level = +_, this) : level;
+    },
+    warn: function() {
+      if (level >= Warn) log('warn', 'WARN', arguments);
+      return this;
+    },
+    info: function() {
+      if (level >= Info) log('log', 'INFO', arguments);
+      return this;
+    },
+    debug: function() {
+      if (level >= Debug) log('log', 'DEBUG', arguments);
+      return this;
+    }
+  }
+}
+/* tslint:enable */
+// ========== END OF TEMPORARY SOLUTION FOR FAILED IMPORT =========
 
 import {AggregateOp} from './aggregate';
 import {X, Channel} from './channel';
