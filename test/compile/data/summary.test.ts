5954c459f83ba1d55007bc85e34e090fdc298499
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-11-04 16:44:12 -0700

    Use new aggregation function format

--- test/compile/data/summary.test.ts
+++ test/compile/data/summary.test.ts
@@ -7,27 +7,26 @@ import {summary} from '../../../src/compile/data/summary';
 import {parseUnitModel} from '../../util';
 
 describe('compile/data/summary', function () {
-  describe('unit (aggregated)', function() {
-    const model = parseUnitModel({
-      mark: "point",
-      encoding: {
-        'y': {
-          'aggregate': 'sum',
-          'field': 'Acceleration',
-          'type': "quantitative"
-        },
-        'x': {
-          'field': 'Origin',
-          'type': "ordinal"
-        },
-        color: {field: '*', type: "quantitative", aggregate: 'count'}
-      }
-    });
+  describe('parseUnit', function() {
+    it('should produce the correct summary component for sum(Acceleration) and count(*)' , () => {
+      const model = parseUnitModel({
+        mark: "point",
+        encoding: {
+          'y': {
+            'aggregate': 'sum',
+            'field': 'Acceleration',
+            'type': "quantitative"
+          },
+          'x': {
+            'field': 'Origin',
+            'type': "ordinal"
+          },
+          color: {field: '*', type: "quantitative", aggregate: 'count'}
+        }
+      });
 
-    model.component.data = {} as DataComponent;
-    model.component.data.summary = summary.parseUnit(model);
-
-    it('should produce the correct summary component' ,function() {
+      model.component.data = {} as DataComponent;
+      model.component.data.summary = summary.parseUnit(model);
       assert.deepEqual(model.component.data.summary, [{
         name: 'summary',
         // source will be added in assemble step
@@ -36,36 +35,17 @@ describe('compile/data/summary', function () {
       }]);
     });
 
-    it('should assemble the correct aggregate transform', function() {
-      const summaryData = summary.assemble(model.component.data, 'source')[0];
-      assert.deepEqual(summaryData, {
-        'name': "summary",
-        'source': 'source',
-        'transform': [{
-          'type': 'aggregate',
-          'groupby': ['Origin'],
-          'summarize': {
-            '*': ['count'],
-            'Acceleration': ['sum']
-          }
-        }]
+    it('should produce the correct summary component for aggregated plot with detail arrays', function() {
+      const model = parseUnitModel({
+        mark: "point",
+        encoding: {
+          'x': { 'aggregate': 'mean', 'field': 'Displacement', 'type': "quantitative"},
+          'detail': [
+            {'field': 'Origin', 'type': "ordinal"},
+            {'field': 'Cylinders', 'type': "quantitative"}
+          ]
+        }
       });
-    });
-  });
-
-  describe('unit (aggregated with detail arrays)', function() {
-    const model = parseUnitModel({
-      mark: "point",
-      encoding: {
-        'x': { 'aggregate': 'mean', 'field': 'Displacement', 'type': "quantitative"},
-        'detail': [
-          {'field': 'Origin', 'type': "ordinal"},
-          {'field': 'Cylinders', 'type': "quantitative"}
-        ]
-      }
-    });
-
-    it('should produce the correct summary component', function() {
       model.component.data = {} as DataComponent;
       model.component.data.summary = summary.parseUnit(model);
       assert.deepEqual(model.component.data.summary, [{
@@ -75,18 +55,59 @@ describe('compile/data/summary', function () {
         measures: {Displacement: {mean: true}}
       }]);
     });
+  });
+
+  describe('parseLayer', function() {
+    // TODO: write test
+  });
+
+  describe('parseFacet', function() {
+    it('should produce child\'s filter if child has no source and the facet has no filter', function() {
+      // TODO: write
+    });
+
+    it('should produce child\'s filter and its own filter if child has no source and the facet has filter', function() {
+      // TODO: write
+    });
+  });
+
+  describe('assemble', function() {
+    it('should assemble the correct summary data', function() {
+      const summaryComponent = [{
+        name: 'summary',
+        // source will be added in assemble step
+        dimensions: {Origin: true},
+        measures: {'*':{count: true}, Acceleration: {sum: true}}
+      }];
+      const summaryData = summary.assemble(summaryComponent, 'source')[0];
+      assert.deepEqual(summaryData, {
+        'name': "summary",
+        'source': 'source',
+        'transform': [{
+          'type': 'aggregate',
+          'groupby': ['Origin'],
+          'fields': ['*', 'Acceleration', ],
+          'ops': ['count', 'sum', ]
+        }]
+      });
+    });
 
     it('should assemble the correct summary data', function() {
-      const summaryData = summary.assemble(model.component.data, 'source')[0];
+      const summaryComponent = [{
+        name: 'summary',
+        // source will be added in assemble step
+        dimensions: {Origin: true, Cylinders: true},
+        measures: {Displacement: {mean: true}}
+      }];
+      const summaryData = summary.assemble(summaryComponent, 'source')[0];
       assert.deepEqual(summaryData, {
         'name': "summary",
         'source': 'source',
         'transform': [{
           'type': 'aggregate',
           'groupby': ['Origin', 'Cylinders'],
-          'summarize': {
-            'Displacement': ['mean']
-          }
+          'fields': ['Displacement'],
+          'ops': ['mean']
         }]
       });
     });
