39bc690bb5981dd4a216a9b9b137422e2ca66a4a
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2016-12-13 21:23:07 +0700

    move channelScalePropertyIncompatability to scale/scale.ts

--- src/compile/scale/init.ts
+++ src/compile/scale/init.ts
@@ -1,10 +1,11 @@
 import * as log from '../../log';
 
 import {Channel} from '../../channel';
-import {ChannelDefWithScale} from '../../fielddef';
+import {ChannelDefWithScale, FieldDef} from '../../fielddef';
 import {Mark} from '../../mark';
 import {Scale, ScaleConfig, ScaleType, scaleTypeSupportProperty} from '../../scale';
 
+import {channelScalePropertyIncompatability} from './scale';
 import {rangeStep} from './range';
 import * as rules from './rules';
 import {type} from './type';
@@ -100,51 +101,3 @@ function getDefaultValue(property: string, scale: Scale, channel: Channel, field
   return scaleConfig[property];
 }
 
-function channelScalePropertyIncompatability(channel: Channel, propName: string): string {
-  switch (propName) {
-    case 'range':
-      // User should not customize range for position and facet channel directly.
-      if (channel === 'x' || channel === 'y') {
-        return log.message.CANNOT_USE_RANGE_WITH_POSITION;
-      }
-      if (channel === 'row' || channel === 'column') {
-        return log.message.cannotUseRangePropertyWithFacet('range');
-      }
-      return undefined; // GOOD!
-    // band / point
-    case 'rangeStep':
-      if (channel === 'row' || channel === 'column') {
-        return log.message.cannotUseRangePropertyWithFacet('rangeStep');
-      }
-      return undefined; // GOOD!
-    case 'padding':
-    case 'paddingInner':
-    case 'paddingOuter':
-      if (channel === 'row' || channel === 'column') {
-        /*
-         * We do not use d3 scale's padding for row/column because padding there
-         * is a ratio ([0, 1]) and it causes the padding to be decimals.
-         * Therefore, we manually calculate "spacing" in the layout by ourselves.
-         */
-        return log.message.CANNOT_USE_PADDING_WITH_FACET;
-      }
-      return undefined; // GOOD!
-    case 'scheme':
-      if (channel !== 'color') {
-        return log.message.CANNOT_USE_SCHEME_WITH_NON_COLOR;
-      }
-      return undefined;
-    case 'type':
-    case 'domain':
-    case 'round':
-    case 'clamp':
-    case 'exponent':
-    case 'nice':
-    case 'zero':
-    case 'useRawDomain':
-      // These channel do not have strict requirement
-      return undefined; // GOOD!
-  }
-  /* istanbul ignore next: it should never reach here */
-  throw new Error('Invalid scale property "${propName}".');
-}
--- src/compile/scale/scale.ts
+++ src/compile/scale/scale.ts
@@ -1,5 +1,10 @@
 import {VgScale} from '../../vega.schema';
 
+import {Channel} from '../../channel';
+import * as log from '../../log';
+
+
+
 /** Scale suffix for scale used to get drive binned legends. */
 export const BIN_LEGEND_SUFFIX = '_bin_legend';
 /** Scale suffix for scale for binned field's legend labels, which maps a binned field's quantitative values to range strings. */
@@ -15,4 +20,53 @@ export type ScaleComponents = {
   main: ScaleComponent;
   binLegend?: ScaleComponent;
   binLegendLabel?: ScaleComponent;
+}
+
+export function channelScalePropertyIncompatability(channel: Channel, propName: string): string {
+  switch (propName) {
+    case 'range':
+      // User should not customize range for position and facet channel directly.
+      if (channel === 'x' || channel === 'y') {
+        return log.message.CANNOT_USE_RANGE_WITH_POSITION;
+      }
+      if (channel === 'row' || channel === 'column') {
+        return log.message.cannotUseRangePropertyWithFacet('range');
+      }
+      return undefined; // GOOD!
+    // band / point
+    case 'rangeStep':
+      if (channel === 'row' || channel === 'column') {
+        return log.message.cannotUseRangePropertyWithFacet('rangeStep');
+      }
+      return undefined; // GOOD!
+    case 'padding':
+    case 'paddingInner':
+    case 'paddingOuter':
+      if (channel === 'row' || channel === 'column') {
+        /*
+         * We do not use d3 scale's padding for row/column because padding there
+         * is a ratio ([0, 1]) and it causes the padding to be decimals.
+         * Therefore, we manually calculate "spacing" in the layout by ourselves.
+         */
+        return log.message.CANNOT_USE_PADDING_WITH_FACET;
+      }
+      return undefined; // GOOD!
+    case 'scheme':
+      if (channel !== 'color') {
+        return log.message.CANNOT_USE_SCHEME_WITH_NON_COLOR;
+      }
+      return undefined;
+    case 'type':
+    case 'domain':
+    case 'round':
+    case 'clamp':
+    case 'exponent':
+    case 'nice':
+    case 'zero':
+    case 'useRawDomain':
+      // These channel do not have strict requirement
+      return undefined; // GOOD!
+  }
+  /* istanbul ignore next: it should never reach here */
+  throw new Error('Invalid scale property "${propName}".');
 }
