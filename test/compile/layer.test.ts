d3b743cbb74cf9b1ae5017f041fb32980929d14d
Dominik Moritz <domoritz@gmail.com>
2017-01-22 16:40:38 -0800

    Test for domain merging in layering.

--- test/compile/layer.test.ts
+++ test/compile/layer.test.ts
@@ -1,7 +1,9 @@
 import {assert} from 'chai';
 
+import * as log from '../../src/log';
 import {LayerModel} from '../../src/compile/layer';
 import {LayerSpec} from '../../src/spec';
+import {parseLayerModel} from '../util';
 
 describe('Layer', function() {
   it('should say it is layer', function() {
@@ -9,5 +11,88 @@ describe('Layer', function() {
     assert(!model.isUnit());
     assert(!model.isFacet());
     assert(model.isLayer());
+  });
+
+  describe('merge scale domains', () => {
+    it('should merge domains', () => {
+      const model = parseLayerModel({
+        layers: [{
+          mark: 'point',
+          encoding: {
+            x: {field: 'a', type: 'ordinal'}
+          }
+        },{
+          mark: 'point',
+          encoding: {
+            x: {field: 'b', type: 'ordinal'}
+          }
+        }]
+      });
+      assert.equal(model.children().length, 2);
+      model.parseScale();
+
+      assert.deepEqual(model.component.scale['x'].main.domain, {
+        fields: [{
+          data: 'layer_0_source',
+          field: 'a'
+        },{
+          data: 'layer_1_source',
+          field: 'b'
+        }],
+        sort: true
+      });
+    });
+
+    it('should merge unioned domains', () => {
+      const model = parseLayerModel({
+        layers: [{
+          mark: 'point',
+          encoding: {
+            x: {bin: true, field: 'a', type: 'quantitative'}
+          }
+        },{
+          mark: 'point',
+          encoding: {
+            x: {field: 'b', type: 'ordinal'}
+          }
+        }]
+      });
+      assert.equal(model.children().length, 2);
+      model.parseScale();
+
+      assert.deepEqual(model.component.scale['x'].main.domain, {
+        fields: [{
+          data: 'layer_0_source',
+          field: 'bin_a_start'
+        },{
+          data: 'layer_0_source',
+          field: 'bin_a_end'
+        },{
+          data: 'layer_1_source',
+          field: 'b'
+        }],
+        sort: true
+      });
+    });
+
+    it('should fail to unioned explicit and referenced domains', () => {
+      log.runLocalLogger((localLogger) => {
+        const model = parseLayerModel({
+          layers: [{
+            mark: 'point',
+            encoding: {
+              x: {scale: {domain: [1, 2, 3]}, field: 'b', type: 'ordinal'}
+            }
+          },{
+            mark: 'point',
+            encoding: {
+              x: {field: 'b', type: 'ordinal'}
+            }
+          }]
+        });
+        model.parseScale();
+        assert.equal(localLogger.warns[0], log.message.CANNOT_UNION_CUSTOM_DOMAIN_WITH_FIELD_DOMAIN);
+      });
+    });
   });
 });
--- test/util.ts
+++ test/util.ts
@@ -1,33 +1,23 @@
 import {buildModel} from '../src/compile/common';
 import {UnitModel} from '../src/compile/unit';
 import {FacetModel} from '../src/compile/facet';
-import {ExtendedUnitSpec, FacetSpec, normalize, ExtendedSpec} from '../src/spec';
+import {LayerModel} from '../src/compile/layer';
+import {ExtendedUnitSpec, FacetSpec, LayerSpec, normalize, ExtendedSpec} from '../src/spec';
 import {Model} from '../src/compile/model';
 
-/**
- * Call new Model without worrying about types.
- * We use this in tests to allow using raw JSON.
- */
 export function parseModel(inputSpec: ExtendedSpec): Model {
   const spec = normalize(inputSpec);
   return buildModel(spec, null, '');
 }
 
-/**
- * Call new UnitModel without worrying about types.
- * We use this in tests to allow using raw JSON.
- */
 export function parseUnitModel(spec: ExtendedUnitSpec) {
-  // TODO: support other type of model as well
   return new UnitModel(spec, null, '');
 }
 
+export function parseLayerModel(spec: LayerSpec) {
+  return new LayerModel(spec, null, '');
+}
 
-/**
- * Call new FacetModel without worrying about types.
- * We use this in tests to allow using raw JSON.
- */
 export function parseFacetModel(spec: FacetSpec) {
-  // TODO: support other type of model as well
-  return new FacetModel(spec as FacetSpec, null, '');
+  return new FacetModel(spec, null, '');
 }
