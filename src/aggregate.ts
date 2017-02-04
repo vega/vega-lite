104b96dea6e4842ab749eb555b9fa36d4a0dec33
Sira Horradarn <sirah@uw.edu>
2017-02-03 20:56:43 -0800

    test script

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -19,6 +19,7 @@ export namespace AggregateOp {
     export const MIN: 'min' = 'min';
     export const MAX: 'max' = 'max';
     export const ARGMIN: 'argmin' = 'argmin';
+    export const AAA: 'aaa' = 'aaa';
     export const ARGMAX: 'argmax' = 'argmax';;
 }
 
