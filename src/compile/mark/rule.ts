1d18d28d72860752a89aa7dbada498cef0cd7dc4
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-18 17:12:28 -0800

    Rename default value ref types: `base` => `zeroOrMin`, `baseOrMax` => `zeroOrMax`

--- src/compile/mark/rule.ts
+++ src/compile/mark/rule.ts
@@ -7,9 +7,9 @@ export const rule: MarkCompiler = {
   role: undefined,
   encodeEntry: (model: UnitModel) => {
     return {
-      ...mixins.pointPosition('x', model, 'base'),
-      ...mixins.pointPosition('y', model, 'base'),
-      ...mixins.pointPosition2(model, 'baseOrMax'),
+      ...mixins.pointPosition('x', model, 'zeroOrMin'),
+      ...mixins.pointPosition('y', model, 'zeroOrMin'),
+      ...mixins.pointPosition2(model, 'zeroOrMax'),
 
       ...mixins.color(model),
       ...mixins.nonPosition('opacity', model),
