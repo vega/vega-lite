a25c7ffd8a5ea5fa722945ed43acfafe489fb44b
Dominik Moritz <domoritz@gmail.com>
2017-02-21 16:28:34 -0800

    Test rename map

--- test/compile/model.test.ts
+++ test/compile/model.test.ts
@@ -1,7 +1,30 @@
 import {assert} from 'chai';
 import {parseFacetModel} from '../util';
+import {NameMap} from '../../src/compile/model';
 
 describe('Model', () => {
+  describe('NameMap', function () {
+    it('should rename correctly', function () {
+      const map = new NameMap();
+      assert.equal(map.get('a'), 'a');
+
+      map.rename('a', 'b');
+      assert.equal(map.get('a'), 'b');
+      assert.equal(map.get('b'), 'b');
+
+      map.rename('b', 'c');
+      assert.equal(map.get('a'), 'c');
+      assert.equal(map.get('b'), 'c');
+      assert.equal(map.get('c'), 'c');
+
+      map.rename('z', 'a');
+      assert.equal(map.get('a'), 'c');
+      assert.equal(map.get('b'), 'c');
+      assert.equal(map.get('c'), 'c');
+      assert.equal(map.get('z'), 'c');
+    });
+  });
+
   describe('hasDescendantWithFieldOnChannel', () => {
     it('should return true if a child plot has a field on x', () => {
       const model = parseFacetModel({
