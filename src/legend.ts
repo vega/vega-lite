2890affaf64d3ade21a9883bd3e0de6721de823f
Yuhan Lu <yuhanlu9@gmail.com>
2017-02-23 20:37:48 -0800

    refactor interface

--- src/legend.ts
+++ src/legend.ts
@@ -1,122 +1,12 @@
 import {DateTime} from './datetime';
-import {VgLegendEncode, VgLegendBase} from './vega.schema';
+import { VgLegendEncode, VgLegendBase, VgLegendConfig } from './vega.schema';
 
-export interface LegendConfig extends LegendBase {
-  // ---------- Gradient ----------
-  /**
-   * The color of the gradient stroke, can be in hex color code or regular color name.
-   */
-  gradientStrokeColor?: string;
-
-  /**
-   * The width of the gradient stroke, in pixels.
-   * @mimimum 0
-   */
-  gradientStrokeWidth?: number;
-
-  /**
-   * The height of the gradient, in pixels.
-   * @mimimum 0
-   */
-  gradientHeight?: number;
-
-  /**
-   * The width of the gradient, in pixels.
-   * @mimimum 0
-   */
-  gradientWidth?: number;
-
-  // ---------- Label ----------
-  /**
-   * The alignment of the legend label, can be left, middle or right.
-   */
-  labelAlign?: string;
-
-  /**
-   * The position of the baseline of legend label, can be top, middle or bottom.
-   */
-  labelBaseline?: string;
-
-  /**
-   * The color of the legend label, can be in hex color code or regular color name.
-   */
-  labelColor?: string;
-
-  /**
-   * The font of the legend label.
-   */
-  labelFont?: string;
-
-  /**
-   * The font size of legend label.
-   * @mimimum 0
-   */
-  labelFontSize?: number;
-
-  /**
-   * The offset of the legend label.
-   * @mimimum 0
-   */
-  labelOffset?: number;
-
-  // ---------- Symbols ----------
-  /**
-   * The color of the legend symbol,
-   */
-  symbolColor?: string;
-
-  /**
-   * The shape of the legend symbol, can be the 'circle', 'square', 'cross', 'diamond',
-   * 'triangle-up', 'triangle-down', or else a custom SVG path string.
-   */
-  symbolType?: string;
-
-  /**
-   * The size of the legend symbol, in pixels.
-   * @mimimum 0
-   */
-  symbolSize?: number;
-
-  /**
-   * The width of the symbol's stroke.
-   * @minimum 0
-   */
-  symbolStrokeWidth?: number;
-
-  // ---------- Title ----------
-  /**
-   * Optional mark property definitions for custom legend styling.
-   */
-  /**
-   * The color of the legend title, can be in hex color code or regular color name.
-   */
-  titleColor?: string;
-
-  /**
-   * The font of the legend title.
-   */
-  titleFont?: string;
-
-  /**
-   * The font size of the legend title.
-   */
-  titleFontSize?: number;
-
-  /**
-   * The font weight of the legend title.
-   */
-  titleFontWeight?: string | number;
-
-  /**
-   * The padding, in pixels, between title and legend.
-   */
-  titlePadding?: number;
-}
+export interface LegendConfig extends VgLegendConfig, VlOnlyLegendBase {}
 
 /**
  * Properties of a legend or boolean flag for determining whether to show it.
  */
-export interface Legend extends LegendBase {
+export interface Legend extends VgLegendBase, VlOnlyLegendBase {
   /**
    * Optional mark definitions for custom legend encoding.
    */
@@ -158,14 +48,12 @@ export interface Legend extends LegendBase {
   zindex?: number;
 }
 
-export interface VlLegendBase {
+export interface VlOnlyLegendBase {
   /**
    * Whether month names and weekday names should be abbreviated.
    */
   shortTimeLabels?: boolean;
 }
-
-export interface LegendBase extends VgLegendBase, VlLegendBase {}
 
 export const defaultLegendConfig: LegendConfig = {
   orient: undefined, // implicitly "right"
@@ -173,4 +61,4 @@ export const defaultLegendConfig: LegendConfig = {
 
 export const LEGEND_PROPERTIES:(keyof Legend)[] = ['entryPadding', 'format', 'offset', 'orient', 'tickCount', 'title', 'type', 'values' ,'zindex'];
 
-export const LEGEND_BASE_PROPERTIES:(keyof VlLegendBase)[] = ['shortTimeLabels'];
+export const VL_ONLY_LEGEND_PROPERTIES:(keyof VlOnlyLegendBase)[] = ['shortTimeLabels'];
--- src/vega.schema.ts
+++ src/vega.schema.ts
@@ -374,11 +374,135 @@ export interface VgAxisConfig extends VgAxisBase {
 }
 
 export interface VgLegendBase {
+  /**
+   * Padding (in pixels) between legend entries in a symbol legend.
+   */
   entryPadding?: number;
 
+  /**
+   * The orientation of the legend. One of "left" or "right". This determines how the legend is positioned within the scene. The default is "right".
+   */
   orient?: string;
 
+  /**
+   * The offset, in pixels, by which to displace the legend from the edge of the enclosing group or data rectangle.
+   */
   offset?: number;
 
+  /**
+   * The padding, in pixels, between the legend and axis.
+   */
   padding?: number;
+}
+
+export interface VgLegendConfig extends VgLegendBase {
+  // ---------- Gradient ----------
+  /**
+   * The color of the gradient stroke, can be in hex color code or regular color name.
+   */
+  gradientStrokeColor?: string;
+
+  /**
+   * The width of the gradient stroke, in pixels.
+   * @mimimum 0
+   */
+  gradientStrokeWidth?: number;
+
+  /**
+   * The height of the gradient, in pixels.
+   * @mimimum 0
+   */
+  gradientHeight?: number;
+
+  /**
+   * The width of the gradient, in pixels.
+   * @mimimum 0
+   */
+  gradientWidth?: number;
+
+  // ---------- Label ----------
+  /**
+   * The alignment of the legend label, can be left, middle or right.
+   */
+  labelAlign?: string;
+
+  /**
+   * The position of the baseline of legend label, can be top, middle or bottom.
+   */
+  labelBaseline?: string;
+
+  /**
+   * The color of the legend label, can be in hex color code or regular color name.
+   */
+  labelColor?: string;
+
+  /**
+   * The font of the legend label.
+   */
+  labelFont?: string;
+
+  /**
+   * The font size of legend label.
+   * @mimimum 0
+   */
+  labelFontSize?: number;
+
+  /**
+   * The offset of the legend label.
+   * @mimimum 0
+   */
+  labelOffset?: number;
+
+  // ---------- Symbols ----------
+  /**
+   * The color of the legend symbol,
+   */
+  symbolColor?: string;
+
+  /**
+   * The shape of the legend symbol, can be the 'circle', 'square', 'cross', 'diamond',
+   * 'triangle-up', 'triangle-down', or else a custom SVG path string.
+   */
+  symbolType?: string;
+
+  /**
+   * The size of the legend symbol, in pixels.
+   * @mimimum 0
+   */
+  symbolSize?: number;
+
+  /**
+   * The width of the symbol's stroke.
+   * @minimum 0
+   */
+  symbolStrokeWidth?: number;
+
+  // ---------- Title ----------
+  /**
+   * Optional mark property definitions for custom legend styling.
+   */
+  /**
+   * The color of the legend title, can be in hex color code or regular color name.
+   */
+  titleColor?: string;
+
+  /**
+   * The font of the legend title.
+   */
+  titleFont?: string;
+
+  /**
+   * The font size of the legend title.
+   */
+  titleFontSize?: number;
+
+  /**
+   * The font weight of the legend title.
+   */
+  titleFontWeight?: string | number;
+
+  /**
+   * The padding, in pixels, between title and legend.
+   */
+  titlePadding?: number;
 }
