ab65cea99254893ad05ba19333c305a4bb097488
mwc22 <mwc22@cs.washington.edu>
2017-03-02 12:32:37 -0800

    Added missing encodingWithoutX_X2_Y_Y2 variable. Added checked to throw error if neither x2 or y2 is provided.

--- src/compositemark.ts
+++ src/compositemark.ts
@@ -42,6 +42,11 @@ add(ERRORBAR, (spec: GenericUnitSpec<ERRORBAR, Encoding>): LayerSpec => {
   const {mark: _m, encoding: encoding, ...outerSpec} = spec;
   const {size: _s, ...encodingWithoutSize} = encoding;
   const {x2: _x2, y2: _y2, ...encodingWithoutX2Y2} = encoding;
+  const {x: _x, y: _y, ...encodingWithoutX_X2_Y_Y2} = encodingWithoutX2Y2;
+
+  if (!encoding.x2 && !encoding.y2) {
+    throw new Error('Neither x2 or y2 provided');
+  }
 
   return {
     ...outerSpec,
