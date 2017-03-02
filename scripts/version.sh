d4d0ec8509c3fc2474f05195bdef586a0c540b94
Dominik Moritz <domoritz@gmail.com>
2017-03-01 16:09:52 -0800

    Fixes #1998

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-npm list $1 | head -n 1 | sed 's/.*@//' | awk '{print $1}'
+if [[ $1 == "vega-lite" ]]; then
+  npm list vega-lite | head -n 1 | sed 's/.*@//' | awk '{print $1}'
+else
+  npm list $1 | head -n 2 | tail -n 1 | sed 's/.*@//' | awk '{print $1}'
+fi
