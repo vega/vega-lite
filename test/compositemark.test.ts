ff4e0d076fe839480ad2815df4d91016a0782751
mwc22 <mwc22@cs.washington.edu>
2017-03-07 12:48:47 -0800

    Undo double quotes to single quotes by disabling linter.

--- test/compositemark.test.ts
+++ test/compositemark.test.ts
@@ -1,75 +1,77 @@
+/* tslint:disable:quotemark */
+
 import {assert} from 'chai';
 import {normalize} from '../src/spec';
 
-describe('normalizeErrorBar', () => {
+describe("normalizeErrorBar", () => {
 
-    it('should produce correct layered specs for horizontal error bar', () => {
+    it("should produce correct layered specs for horizontal error bar", () => {
       assert.deepEqual(normalize({
-        'description': 'A error bar plot showing mean, min, and max in the US population distribution of age groups in 2000.',
-        'data': {'url': 'data/population.json'},
-        'transform': {'filter': 'datum.year == 2000'},
-        mark: 'error-bar',
+        "description": "A error bar plot showing mean, min, and max in the US population distribution of age groups in 2000.",
+        "data": {"url": "data/population.json"},
+        "transform": {"filter": "datum.year == 2000"},
+        mark: "error-bar",
         encoding: {
-          'y': {'field': 'age','type': 'ordinal'},
-          'x': {
-            'aggregate': 'min',
-            'field': 'people',
-            'type': 'quantitative',
-            'axis': {'title': 'population'}
+          "y": {"field": "age","type": "ordinal"},
+          "x": {
+            "aggregate": "min",
+            "field": "people",
+            "type": "quantitative",
+            "axis": {"title": "population"}
           },
-          'x2': {
-            'aggregate': 'max',
-            'field': 'people',
-            'type': 'quantitative'
+          "x2": {
+            "aggregate": "max",
+            "field": "people",
+            "type": "quantitative"
           },
-          'size': {'value': 5}
+          "size": {"value": 5}
         }
       }), {
-        'description': 'A error bar plot showing mean, min, and max in the US population distribution of age groups in 2000.',
-        'data': {'url': 'data/population.json'},
-        'transform': {'filter': 'datum.year == 2000'},
-        'layer': [
+        "description": "A error bar plot showing mean, min, and max in the US population distribution of age groups in 2000.",
+        "data": {"url": "data/population.json"},
+        "transform": {"filter": "datum.year == 2000"},
+        "layer": [
           {
-            'mark': 'rule',
-            'encoding': {
-              'y': {'field': 'age','type': 'ordinal'},
-              'x': {
-                'aggregate': 'min',
-                'field': 'people',
-                'type': 'quantitative',
-                'axis': {'title': 'population'}
+            "mark": "rule",
+            "encoding": {
+              "y": {"field": "age","type": "ordinal"},
+              "x": {
+                "aggregate": "min",
+                "field": "people",
+                "type": "quantitative",
+                "axis": {"title": "population"}
               },
-              'x2': {
-                'aggregate': 'max',
-                'field': 'people',
-                'type': 'quantitative'
+              "x2": {
+                "aggregate": "max",
+                "field": "people",
+                "type": "quantitative"
               }
             }
           },
           {
-            'mark': 'tick',
-            'encoding': {
-              'y': {'field': 'age','type': 'ordinal'},
-              'x': {
-                'aggregate': 'min',
-                'field': 'people',
-                'type': 'quantitative',
-                'axis': {'title': 'population'}
+            "mark": "tick",
+            "encoding": {
+              "y": {"field": "age","type": "ordinal"},
+              "x": {
+                "aggregate": "min",
+                "field": "people",
+                "type": "quantitative",
+                "axis": {"title": "population"}
               },
-              'size': {'value': 5}
+              "size": {"value": 5}
             }
           },
           {
-            'mark': 'tick',
-            'encoding': {
-              'y': {'field': 'age','type': 'ordinal'},
-              'x': {
-                'aggregate': 'max',
-                'field': 'people',
-                'type': 'quantitative',
-                // 'axis': {'title': 'population'}
+            "mark": "tick",
+            "encoding": {
+              "y": {"field": "age","type": "ordinal"},
+              "x": {
+                "aggregate": "max",
+                "field": "people",
+                "type": "quantitative",
+                // "axis": {"title": "population"}
               },
-              'size': {'value': 5}
+              "size": {"value": 5}
             }
           }
         ]
