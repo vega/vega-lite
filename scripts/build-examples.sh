349a7e19e0eae1a2c14f8ace2ba19a2b186e0925
Yuhan (Zoe) Lu <yuhanlu9@cs.washington.edu>
2017-03-31 14:11:05 -0700

    fix sequential naming hack

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -5,15 +5,16 @@ dir=${dir-"examples/vg-specs"}
 
 echo "compiling examples to $dir"
 
-mkdir -p $dir
-rm -f $dir/*
+rm -rf $dir
+mkdir $dir
 
-type parallel >/dev/null 2>&1 || { for file in examples/specs/*.vl.json; do filename=$(basename "$file"); name="${filename%.vl.json}"; bin/vl2vg -p $file > $dir/$name.vg.json & done; exit 0; }
-
-ls examples/specs/*.vl.json | parallel --halt 1 "bin/vl2vg -p {} > examples/vg-specs/{/.}.json"
-
-for file in examples/vg-specs/*.vl.json; do
-  filename=$(basename "$file")
-  name="${filename%.vl.json}"
-  mv $file $dir/$name.vg.json
-done
+if type parallel >/dev/null 2>&1
+then
+  for file in examples/specs/*.vl.json; do
+    filename=$(basename "$file")
+    name="${filename%.vl.json}"
+    bin/vl2vg -p $file > $dir/$name.vg.json
+  done
+else
+  ls examples/specs/*.vl.json | parallel  --plus --halt 1 "bin/vl2vg -p {} > examples/vg-specs/{/..}.vg.json"
+fi
