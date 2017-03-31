a3a502fc78495d1152c4d1deb6e3561a143ede99
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-03-31 15:43:29 -0700

    Update echo

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -10,12 +10,13 @@ mkdir $dir
 
 if type parallel >/dev/null 2>&1
 then
+  echo "Parallel not found! Sequentially generate vega specs from examples."
   for file in examples/specs/*.vl.json; do
     filename=$(basename "$file")
     name="${filename%.vl.json}"
     bin/vl2vg -p $file > $dir/$name.vg.json
   done
 else
-  echo "yeah we use parallel!"
+  echo "Using parallel to generate vega specs from examples in parallel."
   ls examples/specs/*.vl.json | parallel  --plus --halt 1 "bin/vl2vg -p {} > examples/vg-specs/{/..}.vg.json"
 fi
