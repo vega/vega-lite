2505fc80a4040ecb6290f33232bccf33f1cbe4fd
Yuhan (Zoe) Lu <yuhanlu9@cs.washington.edu>
2017-04-03 14:45:47 -0700

    fix bug parallel build

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -10,13 +10,13 @@ mkdir $dir
 
 if type parallel >/dev/null 2>&1
 then
+  echo "Using parallel to generate vega specs from examples in parallel."
+  ls examples/specs/*.vl.json | parallel  --plus --halt 1 "bin/vl2vg -p {} > examples/vg-specs/{/..}.vg.json"
+else
   echo "Parallel not found! Sequentially generate vega specs from examples."
   for file in examples/specs/*.vl.json; do
     filename=$(basename "$file")
     name="${filename%.vl.json}"
     bin/vl2vg -p $file > $dir/$name.vg.json
   done
-else
-  echo "Using parallel to generate vega specs from examples in parallel."
-  ls examples/specs/*.vl.json | parallel  --plus --halt 1 "bin/vl2vg -p {} > examples/vg-specs/{/..}.vg.json"
 fi
