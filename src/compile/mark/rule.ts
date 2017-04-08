430965fc149ed3056ff96cb793db1e87fd78e4a0
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-04-08 16:18:29 -0700

    Make 1D rule centered on the non-encoded axis.
    This will allow rules in 1D box plot to align with bar and ticks.

--- src/compile/mark/rule.ts
+++ src/compile/mark/rule.ts
@@ -1,14 +1,18 @@
 import {UnitModel} from '../unit';
 import {MarkCompiler} from './base';
 import * as mixins from './mixins';
+import * as ref from './valueref';
 
 export const rule: MarkCompiler = {
   vgMark: 'rule',
   defaultRole: undefined,
   encodeEntry: (model: UnitModel) => {
+    const {config, markDef} = model;
+    const orient = markDef.orient;
+
     return {
-      ...mixins.pointPosition('x', model, 'zeroOrMin'),
-      ...mixins.pointPosition('y', model, 'zeroOrMin'),
+      ...mixins.pointPosition('x', model, orient === 'horizontal' ? 'zeroOrMin' : ref.midX(config)),
+      ...mixins.pointPosition('y', model, orient === 'vertical' ? 'zeroOrMin' : ref.midY(config)),
       ...mixins.pointPosition2(model, 'zeroOrMax'),
 
       ...mixins.color(model),
