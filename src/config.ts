83d96a423e34ad446cd1ea3378de1950503ed8e3
Swojit Mohapatra <neelmohapatra@gmail.com>
2017-04-10 21:03:43 -0700

    Removed maxbins from config

--- src/config.ts
+++ src/config.ts
@@ -116,13 +116,6 @@ export interface Config  extends TopLevelProperties {
   timeFormat?: string;
 
   /**
-   * Default maximum number of bins for binned variables.
-   * @minimum 0
-   * @TJS-type integer
-   */
-  maxbins?: number;
-
-  /**
    * Default axis and legend title for count fields.
    * @type {string}
    */
@@ -247,7 +240,6 @@ export const defaultConfig: Config = {
   padding: 5,
   numberFormat: 's',
   timeFormat: '%b %d, %Y',
-  maxbins: 10,
   countTitle: 'Number of Records',
 
   cell: defaultCellConfig,
