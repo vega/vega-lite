a2e88d294a44b023b08dcd1327537799e3e486fe
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-04-17 15:14:09 -0700

    Update selection predicate to use `datum.id` instead of `parent.id` since we flatten the top-level spec.
    Note: we remove `"main-group"` and thus flatten the top-level spec.  Using `parent.id` cause the following error
    
    ```
    Uncaught Error: Unrecognized signal name: "parent"
    ```
    
    cc: @arvind -- Could you please check if the selections are still working okay

--- src/compile/selection/selection.ts
+++ src/compile/selection/selection.ts
@@ -92,6 +92,7 @@ export function parseUnitSelection(model: UnitModel, selDefs: Dict<SelectionDef>
   return selCmpts;
 }
 
+// FIXME: rename to assembleSelectionUnitSignals
 export function assembleUnitSignals(model: UnitModel, signals: any[]) {
   forEachSelection(model, (selCmpt, selCompiler) => {
     const name = selCmpt.name,
@@ -203,7 +204,7 @@ export function predicate(selCmpt: SelectionComponent, datum?: string): string {
   const store = stringValue(selCmpt.name + STORE),
         op = PREDICATES_OPS[selCmpt.resolve];
   datum = datum || 'datum';
-  return compiler(selCmpt).predicate + `(${store}, parent._id, ${datum}, ${op})`;
+  return compiler(selCmpt).predicate + `(${store}, datum._id, ${datum}, ${op})`;
 }
 
 // Utility functions
