76ff54a40ce5138aba39d1127698685140beaf66
Yuhan (Zoe) Lu <yuhanlu9@cs.washington.edu>
2017-04-19 14:13:36 -0700

    add check in check-schema.sh

--- scripts/check-schema.sh
+++ scripts/check-schema.sh
@@ -4,6 +4,10 @@ if ! git diff --exit-code HEAD -- ./build/vega-lite-schema.json
 then
   echo "vega-lite-schema.json is different from the committed one."
   exit 1
+elif grep 'Generic.*Spec<' ./build/vega-lite-schema.json
+then
+  echo "vega-lite-schema.json is different from the committed one."
+  exit 1
 else
   exit 0
 fi
