c9c42dc2cb061f51122ff2f5ee155ea3f43d88b3
Swojit Mohapatra <neelmohapatra@gmail.com>
2017-04-20 19:36:19 -0700

    Removed unnecessary imports from bin.test.ts (#2250)

--- test/bin.test.ts
+++ test/bin.test.ts
@@ -1,9 +1,6 @@
 import {assert} from 'chai';
 import {autoMaxBins, binToString} from '../src/bin';
-import {Channel, COLOR, COLUMN, hasScale, OPACITY, rangeType, ROW, SHAPE, supportScaleType} from '../src/channel';
-import {CHANNELS, NONSPATIAL_CHANNELS, NONSPATIAL_SCALE_CHANNELS, SCALE_CHANNELS, UNIT_CHANNELS, UNIT_SCALE_CHANNELS} from '../src/channel';
-import {SCALE_TYPES, ScaleType} from '../src/scale';
-import {some, without} from '../src/util';
+import {COLOR, COLUMN, OPACITY, ROW, SHAPE} from '../src/channel';
 
 describe('autoMaxBins', () => {
   it('should assign generate correct defaults for different channels', () => {
