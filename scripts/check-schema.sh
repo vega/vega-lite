ef72fc329a9a97e6cb45da14f3d3b6606030f6b4
Zoe <yuhanlu9@gmail.com>
2017-04-28 21:32:43 -0700

    Fix schema (#2310)

--- scripts/check-schema.sh
+++ scripts/check-schema.sh
@@ -8,6 +8,14 @@ elif grep 'Generic.*Spec<' ./build/vega-lite-schema.json
 then
   echo "Generic*Spec in the schema have not been replaced."
   exit 1
+elif grep 'UnitSpec<Encoding' ./build/vega-lite-schema.json
+then
+  echo "UnitSpec<...> in the schema have not been replaced."
+  exit 1
+elif grep '<Field>' ./build/vega-lite-schema.json
+then
+  echo "...<Field> in the schema have not been replaced."
+  exit 1
 else
   exit 0
 fi
