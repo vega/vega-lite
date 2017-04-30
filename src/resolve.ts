eced759232fa74ad6dcd69f12735bf5d22704556
Dominik Moritz <domoritz@gmail.com>
2017-04-29 19:57:35 -0700

    Review follow up and rebuild schema

--- src/resolve.ts
+++ src/resolve.ts
@@ -5,43 +5,34 @@ import {contains} from './util';
 export type ResolveMode = 'independent' | 'shared';
 
 export interface SpatialResolve {
-  scale: ResolveMode;
+  scale?: ResolveMode;
   axis?: ResolveMode;
 }
 
 export interface NonspatialResolve {
-  scale: ResolveMode;
+  scale?: ResolveMode;
   legend?: ResolveMode;
 }
 
 export type Resolve = SpatialResolve | NonspatialResolve;
 
-export function isSpatialResolve(resolve: Resolve): resolve is SpatialResolve {
-  return 'axis' in resolve;
-}
-
-export function isNonspatialResolve(resolve: Resolve): resolve is NonspatialResolve {
-  return 'legend' in resolve;
-}
-
-export type ResolveMapping = {[P in NonspatialScaleChannel]?: NonspatialResolve} | {[P in SpatialScaleChannel]?: SpatialResolve};
+export type ResolveMapping = {[P in NonspatialScaleChannel]?: NonspatialResolve} & {[P in SpatialScaleChannel]?: SpatialResolve};
 
 export function initLayerResolve(resolve: ResolveMapping): ResolveMapping {
+  const out: ResolveMapping = {};
   UNIT_SCALE_CHANNELS.forEach(channel => {
     const res: Resolve = resolve[channel] || {scale: 'shared'};
     const guide = contains(SPATIAL_SCALE_CHANNELS, channel) ? 'axis' : 'legend';
 
-    if (res.scale === 'independent' && (
-      (isSpatialResolve(res) && res.axis === 'shared') ||
-      (isNonspatialResolve(res) && res.legend === 'shared'))) {
+    if (res.scale === 'independent' && (res['axis'] === 'shared' || res['legend'] === 'shared')) {
       log.warn(log.message.independentScaleMeansIndependentGuide(channel));
     }
 
-    resolve[channel] = {
+    out[channel] = {
       scale: res.scale || 'shared',
       [guide]: res.scale === 'independent' ? 'independent' : (res[guide] || 'shared')
     };
   });
 
-  return resolve;
+  return out;
 }
