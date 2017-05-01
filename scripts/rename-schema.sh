60da117e8b0a1fce72b9ee8453eaaec4324ddcd8
Yuhan (Zoe) Lu <yuhanlu9@cs.washington.edu>
2017-05-01 15:18:13 -0700

    split rename schema

---
+++ scripts/rename-schema.sh
@@ -0,0 +1,5 @@
+sed -i -e 's/GenericLayerSpec<.*..\\./LayerSpec/g' build/vega-lite-schema.json
+sed -i -e 's/GenericRepeatSpec<.*..\\./RepeatSpec/g' build/vega-lite-schema.json
+sed -i -e 's/GenericUnitSpec<Encoding<.*..\\./UnitSpec/g' build/vega-lite-schema.json
+sed -i -e 's/GenericUnitSpec<EncodingWithFacet<.*..\\./FacetedUnitSpec/g' build/vega-lite-schema.json
+sed -i -e 's/<Field>//g' build/vega-lite-schema.json
