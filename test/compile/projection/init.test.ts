e428420feb2f62eda7c0874677b06ddef0403bb1
Will Strimling <w@ml.to>
2017-05-22 00:22:07 -0700

    add projcetion init tests

--- test/compile/projection/init.test.ts
+++ test/compile/projection/init.test.ts
@@ -1,11 +1,20 @@
 import {assert} from 'chai';
 
 import {initLayerProjection, initProjection} from '../../../src/compile/projection/init';
+import {Config} from '../../../src/config';
+import {Encoding} from '../../../src/encoding';
+import {Field} from '../../../src/fielddef';
+import {Projection} from '../../../src/projection';
+import {LayerSpec, UnitSpec} from '../../../src/spec';
 
 describe('compile/projection', () => {
+  const specified: Projection = {type: 'mercator'};
+  const specifiedAlternate: Projection = {type: 'albersUsa'};
+
   describe('init', () => {
     it('should output a projection if one is specified', () => {
-      assert.isTrue(true);
+      const output = initProjection(undefined, specified, undefined, undefined, undefined);
+      assert.deepEqual(specified, output);
     });
 
     // it('should output a projection if mark is geoshape', () => {
@@ -13,25 +22,81 @@ describe('compile/projection', () => {
     // });
 
     it('should output a projection if channel is projection (latitude or longitude)', () => {
-      assert.isTrue(true);
+      const encodingLat: Encoding<Field> = {
+        'y': {'field': 'y', 'type': 'latitude'}
+      };
+      const outputLat = initProjection(undefined, undefined, specified, undefined, encodingLat);
+      assert.deepEqual(specified, outputLat);
+
+      const encodingLong: Encoding<Field> = {
+        'x': {'field': 'x', 'type': 'longitude'}
+      };
+      const outputLong = initProjection(undefined, undefined, specified, undefined, encodingLong);
+      assert.deepEqual(specified, outputLong);
     });
 
     it('should output a projection such that a specified projection overrides a parent-specified projection which overrides a config-specified projection', () => {
-      assert.isUndefined(undefined);
+      const config: Config = {projection: {type: 'gnomonic'}};
+      const parent: Projection = {type: 'albers'};
+
+      let output = initProjection(config, specified, undefined, undefined, undefined);
+      assert.deepEqual(specified, output);
+      output = initProjection(config, specified, parent, undefined, undefined);
+      assert.deepEqual(specified, output);
+
+      const encoding: Encoding<Field> = {
+        'x': {'field': 'x', 'type': 'longitude'}
+      };
+
+      output = initProjection(config, undefined, parent, undefined, encoding);
+      assert.deepEqual(parent, output);
+
+      output = initProjection(config, undefined, undefined, undefined, encoding);
+      assert.deepEqual(config.projection, output);
     });
 
     it('should not output a projection if none is specified up inheritance chain', () => {
-      assert.isUndefined(undefined);
+      let output = initProjection(undefined, undefined, undefined, undefined, undefined);
+      assert.isUndefined(output);
+
+      const encoding: Encoding<Field> = {
+        'x': {'field': 'x', 'type': 'longitude'}
+      };
+
+      output = initProjection(undefined, undefined, undefined, undefined, encoding);
+      assert.isUndefined(output);
     });
   });
 
   describe('initLayer', () => {
     it('should output the projection specified first in layers', () => {
-      assert.isTrue(true);
+      const layers: UnitSpec[] = [
+        {
+          'mark': 'point',
+          ...specified,
+          encoding: {}
+        },
+        {
+          'mark': 'point',
+          ...specifiedAlternate,
+          encoding: {}
+        }
+      ];
+      assert.deepEqual(specified, initLayerProjection(layers));
     });
 
     it('should not output a projection if none are specified in layers', () => {
-      assert.isUndefined(undefined);
+      const layers: UnitSpec[] = [
+        {
+          'mark': 'point',
+          encoding: {}
+        },
+        {
+          'mark': 'point',
+          encoding: {}
+        }
+      ];
+      assert.isUndefined(initLayerProjection(layers));
     });
   });
 });
