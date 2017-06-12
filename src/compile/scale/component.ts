846bfda6b535f871af0543a596fef267445d5056
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-06-12 14:21:58 -0700

    Refactor (#2489)
    - Add Scale/Axis/LegendIndex
    - Always uses plural for `scales: Dict<Scale>`

--- src/compile/scale/component.ts
+++ src/compile/scale/component.ts
@@ -1,6 +1,9 @@
 import {ScaleChannel} from '../../channel';
+import {Scale} from '../../scale';
 import {VgScale} from '../../vega.schema';
 
 export type ScaleComponent = VgScale;
 
 export type ScaleComponentIndex = {[P in ScaleChannel]?: ScaleComponent};
+
+export type ScaleIndex = {[P in ScaleChannel]?: Scale};
