edee0a6130b933ba17cf691207ed6bf6773f7c62
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-07-19 10:54:55 -0700

    Add `config.axis.quantitativeExtent`

--- src/compile/axis/parse.ts
+++ src/compile/axis/parse.ts
@@ -288,11 +288,11 @@ function getProperty<K extends keyof (Axis|VgAxis)>(property: K, specifiedAxis: 
     }
     case 'minExtent': {
       const scaleType = model.component.scales[channel].get('type');
-      return getSpecifiedOrDefaultValue(specifiedAxis.minExtent, rules.minMaxExtent(isGridAxis, scaleType));
+      return getSpecifiedOrDefaultValue(specifiedAxis.minExtent, rules.minMaxExtent(isGridAxis, scaleType, model.config.axis));
     }
     case 'maxExtent': {
       const scaleType = model.component.scales[channel].get('type');
-      return getSpecifiedOrDefaultValue(specifiedAxis.maxExtent, rules.minMaxExtent(isGridAxis, scaleType));
+      return getSpecifiedOrDefaultValue(specifiedAxis.maxExtent, rules.minMaxExtent(isGridAxis, scaleType, model.config.axis));
     }
     case 'orient':
       return getSpecifiedOrDefaultValue(specifiedAxis.orient, rules.orient(channel));
