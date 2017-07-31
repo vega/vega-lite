28b8993449242f85e43ce75b304ef37f3f1249b5
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-07-31 14:16:22 -0700

    Make `build-examples` also include SVGs

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -11,12 +11,13 @@ mkdir $dir
 if type parallel >/dev/null 2>&1
 then
   echo "Using parallel to generate vega specs from examples in parallel."
-  ls examples/specs/*.vl.json | parallel --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json"
+  ls examples/specs/*.vl.json | parallel --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json && node_modules/vega/bin/vg2svg examples/compiled/{/..}.vg.json examples/compiled/{/..}.svg -b node_modules/vega-datasets/"
 else
   echo "Parallel not found! Sequentially generate vega specs from examples."
   for file in examples/specs/*.vl.json; do
     filename=$(basename "$file")
     name="${filename%.vl.json}"
     bin/vl2vg -p $file > $dir/$name.vg.json
+    node_modules/vega/bin/vg2svg $dir/$name.vg.json $dir/$name.svg -b node_modules/vega-datasets/
   done
 fi
