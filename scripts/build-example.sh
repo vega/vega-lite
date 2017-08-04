9f6798ebf22bba271de0da7019aa0a6a8088e91e
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-04 15:09:32 -0700

    Add build-example for compiling only one example
    This would be useful for writing new example for docs

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -1,1 +1,15 @@
-bin/vl2vg -p
+#!/bin/bash
+
+# Script for compiling one example
+# ./scripts/build-example <example-name> [-s]
+# -s : always compile svg
+
+if [ -z "$1" ]
+  then
+    echo "No example name supplied"
+fi
+
+rm -f examples/compiled/$1.vg.json
+bin/vl2vg -p examples/specs/$1.vl.json > examples/compiled/$1.vg.json
+rm -f examples/compiled/$1.svg
+node_modules/vega/bin/vg2svg examples/compiled/$1.vg.json > examples/compiled/$1.svg -b .
