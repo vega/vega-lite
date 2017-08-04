229d4fead6f2e40df63bd704e557eca332cd49c3
Dominik Moritz <domoritz@gmail.com>
2017-08-03 22:04:44 -0700

    Fix version script so it works even when vl is linked

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,5 +3,5 @@
 if [[ $1 == "vega-lite" ]]; then
   npm list vega-lite | head -n 1 | sed 's/.*@//' | awk '{print $1}'
 else
-  npm list $1 | head -n 2 | tail -n 1 | sed 's/.*@//' | awk '{print $1}'
+  npm list $1 | tail -n 2 | head -n 1 | sed 's/.*@//' | awk '{print $1}'
 fi
