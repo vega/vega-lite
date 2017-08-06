d9c69c98741414622076866e845846aa205999eb
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-06 13:39:45 -0700

    Try changing build-examples script

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -11,13 +11,13 @@ mkdir $dir
 if type parallel >/dev/null 2>&1
 then
   echo "Using parallel to generate vega specs from examples in parallel."
-  ls examples/specs/*.vl.json | parallel --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json && node_modules/.bin/vg2svg examples/compiled/{/..}.vg.json examples/compiled/{/..}.svg -b ."
+  ls examples/specs/*.vl.json | parallel --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json && node_modules/vega/bin/vg2svg examples/compiled/{/..}.vg.json examples/compiled/{/..}.svg -b ."
 else
   echo "Parallel not found! Sequentially generate vega specs from examples."
   for file in examples/specs/*.vl.json; do
     filename=$(basename "$file")
     name="${filename%.vl.json}"
     bin/vl2vg -p $file > $dir/$name.vg.json
-    node_modules/.bin/vg2svg $dir/$name.vg.json $dir/$name.svg -b .
+    node_modules/vega/bin/vg2svg $dir/$name.vg.json $dir/$name.svg -b .
   done
 fi
