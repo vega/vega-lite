3f1507038237d18ee47a26c306675f41a47d05d1
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-08 11:31:00 -0700

    Make check-examples only check Vega

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if ! git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/*
+# Check only Vega file for now as Travis is having problem when generating SVG
+if ! git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/*.vg.json
 then
   echo "Output examples vega specs are outdated."
   exit 1
