2db5c3dee74cbb443e74b85d541499d0d9a6d9d2
Dominik Moritz <domoritz@gmail.com>
2017-08-12 21:25:59 -0700

    Show ETA when building examples

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -12,7 +12,7 @@ rm -f $dir/*.svg
 if type parallel >/dev/null 2>&1
 then
   echo "Using parallel to generate vega specs from examples in parallel."
-  ls examples/specs/*.vl.json | parallel --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json && node_modules/.bin/vg2svg examples/compiled/{/..}.vg.json examples/compiled/{/..}.svg -b ."
+  ls examples/specs/*.vl.json | parallel --eta --no-notice --plus --halt 1 "bin/vl2vg -p {} > examples/compiled/{/..}.vg.json && node_modules/.bin/vg2svg examples/compiled/{/..}.vg.json examples/compiled/{/..}.svg -b ."
 else
   echo "Parallel not found! Sequentially generate vega specs from examples."
   for file in examples/specs/*.vl.json; do
