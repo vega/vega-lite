901e01cbb184cf1d83b65fe6e7c892915b0b16a0
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-08-22 16:37:02 -0700

    Extract fieldFilterExpression so we can use it with FilterTransform in Voyager

--- src/filter.ts
+++ src/filter.ts
@@ -8,7 +8,13 @@ import {fieldExpr as timeUnitFieldExpr, isSingleTimeUnit, normalizeTimeUnit, Tim
 import {isArray, isString, logicalExpr} from './util';
 
 
-export type Filter = EqualFilter | RangeFilter | OneOfFilter | SelectionFilter | string;
+export type Filter =
+  // FieldFilter (but we don't type FieldFilter here so the schema has no nesting
+  // and thus the documentation shows all of the types clearly)
+  EqualFilter | RangeFilter | OneOfFilter |
+  SelectionFilter | string;
+
+export type FieldFilter = EqualFilter | RangeFilter | OneOfFilter;
 
 export interface SelectionFilter {
   /**
@@ -120,38 +126,45 @@ export function expression(model: Model, filterOp: LogicalOperand<Filter>, node?
     } else if (isSelectionFilter(filter)) {
       return predicate(model, filter.selection, node);
     } else { // Filter Object
-      const fieldExpr = filter.timeUnit ?
-        // For timeUnit, cast into integer with time() so we can use ===, inrange, indexOf to compare values directly.
-          // TODO: We calculate timeUnit on the fly here. Consider if we would like to consolidate this with timeUnit pipeline
-          // TODO: support utc
-        ('time(' + timeUnitFieldExpr(filter.timeUnit, filter.field) + ')') :
-        field(filter, {expr: 'datum'});
+      return fieldFilterExpression(filter);
+    }
+  });
+}
 
-      if (isEqualFilter(filter)) {
-        return fieldExpr + '===' + valueExpr(filter.equal, filter.timeUnit);
-      } else if (isOneOfFilter(filter)) {
-        // "oneOf" was formerly "in" -- so we need to add backward compatibility
-        const oneOf: OneOfFilter[] = filter.oneOf || filter['in'];
-        return 'indexof([' +
-          oneOf.map((v) => valueExpr(v, filter.timeUnit)).join(',') +
-          '], ' + fieldExpr + ') !== -1';
-      } else if (isRangeFilter(filter)) {
-        const lower = filter.range[0];
-        const upper = filter.range[1];
+export function fieldFilterExpression(filter: FieldFilter) {
+  const fieldExpr = filter.timeUnit ?
+    // For timeUnit, cast into integer with time() so we can use ===, inrange, indexOf to compare values directly.
+      // TODO: We calculate timeUnit on the fly here. Consider if we would like to consolidate this with timeUnit pipeline
+      // TODO: support utc
+    ('time(' + timeUnitFieldExpr(filter.timeUnit, filter.field) + ')') :
+    field(filter, {expr: 'datum'});
 
-        if (lower !== null &&  upper !== null) {
-          return 'inrange(' + fieldExpr + ', [' +
-            valueExpr(lower, filter.timeUnit) + ', ' +
-            valueExpr(upper, filter.timeUnit) + '])';
-        } else if (lower !== null) {
-          return fieldExpr + ' >= ' + lower;
-        } else if (upper !== null) {
-          return fieldExpr + ' <= ' + upper;
-        }
-      }
+  if (isEqualFilter(filter)) {
+    return fieldExpr + '===' + valueExpr(filter.equal, filter.timeUnit);
+  } else if (isOneOfFilter(filter)) {
+    // "oneOf" was formerly "in" -- so we need to add backward compatibility
+    const oneOf: OneOfFilter[] = filter.oneOf || filter['in'];
+    return 'indexof([' +
+      oneOf.map((v) => valueExpr(v, filter.timeUnit)).join(',') +
+      '], ' + fieldExpr + ') !== -1';
+  } else if (isRangeFilter(filter)) {
+    const lower = filter.range[0];
+    const upper = filter.range[1];
+
+    if (lower !== null &&  upper !== null) {
+      return 'inrange(' + fieldExpr + ', [' +
+        valueExpr(lower, filter.timeUnit) + ', ' +
+        valueExpr(upper, filter.timeUnit) + '])';
+    } else if (lower !== null) {
+      return fieldExpr + ' >= ' + lower;
+    } else if (upper !== null) {
+      return fieldExpr + ' <= ' + upper;
     }
     return undefined;
-  });
+  }
+
+  /* istanbul ignore next: it should never reach here */
+  throw new Error(`Invalid field filter: ${JSON.stringify(filter)}`);
 }
 
 function valueExpr(v: any, timeUnit: TimeUnit) {
