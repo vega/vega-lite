5a746d01aa480fde9f23fc7ffa2a510d6c59a19c
starry <starrynight0225@gmail.com>
2017-08-28 17:01:18 -0700

    Add vega typings

--- typings/vega.d.ts
+++ typings/vega.d.ts
@@ -10,6 +10,7 @@ declare module 'vega' {
     public run(): View;
     public change(name: string, changeset: any): View;
     public changeset(): any;
+    public data(name: string): object[];
   }
   export const Warn: number;
   export const changeset: any;
