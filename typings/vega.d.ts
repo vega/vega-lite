0b5494af53449fdc5332e515b9e645e3e97150ab
Dominik Moritz <domoritz@gmail.com>
2017-10-17 22:01:15 -0700

    Update vega typings (#3134)
    * Update vega typings
    
    * Update vega.d.ts
    
    * Update vega.d.ts
    
    * Update vega.d.ts

--- typings/vega.d.ts
+++ typings/vega.d.ts
@@ -1,8 +1,9 @@
 declare module 'vega' {
   export const version: string;
-  export function parse(spec: any): any;
+  export function parse(spec: any, opt?: any): any;
+  export function isString(value: any): value is string;
   export class View {
-    constructor(runtime: any);
+    constructor(runtime: any, config?: any);
     public logLevel(level: number): View;
     public initialize(dom: Element | string): View;
     public renderer(renderer: string): View;
@@ -13,6 +14,12 @@ declare module 'vega' {
     public change(name: string, changeset: any): View;
     public changeset(): any;
     public data(name: string): object[];
+
+    public width(w: number): View;
+    public height(h: number): View;
+    public padding(p: number | {left?: number, right?: number, top?: number, bottom?: number}): View;
+
+    public toImageURL(type: string): Promise<string>;
   }
   export const Warn: number;
   export const changeset: any;
