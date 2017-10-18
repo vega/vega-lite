e8ec0231addde2267da75e9c3505e8f18977fd6c
Dominik Moritz <domoritz@gmail.com>
2017-10-17 21:34:59 -0700

    Update vega.d.ts

--- typings/vega.d.ts
+++ typings/vega.d.ts
@@ -1,27 +1,27 @@
 declare module 'vega' {
-    export const version: string;
-    export function parse(spec: any, opt?: any): any;
-    export function isString(value: any): value is string;
-    export class View {
-      constructor(runtime: any, config?: any);
-      public logLevel(level: number): View;
-      public initialize(dom: Element | string): View;
-      public renderer(renderer: string): View;
-      public finalize(): void;
+  export const version: string;
+  export function parse(spec: any, opt?: any): any;
+  export function isString(value: any): value is string;
+  export class View {
+    constructor(runtime: any, config?: any);
+    public logLevel(level: number): View;
+    public initialize(dom: Element | string): View;
+    public renderer(renderer: string): View;
+    public finalize(): void;
 
-      public hover(): View;
-      public run(): View;
-      public change(name: string, changeset: any): View;
-      public changeset(): any;
-      public data(name: string): object[];
+    public hover(): View;
+    public run(): View;
+    public change(name: string, changeset: any): View;
+    public changeset(): any;
+    public data(name: string): object[];
 
-      public width(w: number): View;
-      public height(h: number): View;
-      public padding(p: number | {left?: number, right?: number, top?: number, bottom?: number}): View;
+    public width(w: number): View;
+    public height(h: number): View;
+    public padding(p: number | {left?: number, right?: number, top?: number, bottom?: number}): View;
 
-      public toImageURL(type: string): Promise<string>;
-    }
-    export const Warn: number;
-    export const changeset: any;
-    export const loader: any;
+    public toImageURL(type: string): Promise<string>;
   }
+  export const Warn: number;
+  export const changeset: any;
+  export const loader: any;
+}
