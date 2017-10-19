b868e721166ac6a650c5331d7a3973034303d260
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-10-18 21:35:23 -0700

    Remove unused lines in the schema rename script

--- scripts/rename-schema.sh
+++ scripts/rename-schema.sh
@@ -17,11 +17,9 @@ perl -pi -e s,'Condition<(.*)>','Condition\1',g build/vega-lite-schema.json
 
 perl -pi -e s,'ConditionalFieldDef<TextFieldDef\>','ConditionalTextFieldDef',g build/vega-lite-schema.json
 perl -pi -e s,'ConditionalValueDef<TextFieldDef\>','ConditionalTextValueDef',g build/vega-lite-schema.json
-perl -pi -e s,'ConditionOnlyDef<TextFieldDef>','ConditionOnlyTextDef',g build/vega-lite-schema.json
 
 perl -pi -e s,'ConditionalFieldDef<LegendFieldDef\>','ConditionalLegendFieldDef',g build/vega-lite-schema.json
 perl -pi -e s,'ConditionalValueDef<LegendFieldDef\>','ConditionalLegendValueDef',g build/vega-lite-schema.json
-perl -pi -e s,'ConditionOnlyDef<LegendFieldDef\>','ConditionOnlyLegendDef',g build/vega-lite-schema.json
 
 perl -pi -e s,'LogicalOperand<string>','SelectionOperand',g build/vega-lite-schema.json
 perl -pi -e s,'LogicalAnd<string>','SelectionAnd',g build/vega-lite-schema.json
