00ac7289cd89ba7c5b2f1caee00c47533c51f1a8
Dominik Moritz <domoritz@gmail.com>
2017-10-22 21:27:27 -0700

    Update vega.d.ts

--- typings/vega.d.ts
+++ typings/vega.d.ts
@@ -2,12 +2,21 @@ declare module 'vega' {
   export const version: string;
   export function parse(spec: any, opt?: any): any;
   export function isString(value: any): value is string;
+
+  export type Loader = {
+    load: (uri: string, options?: any) => Promise<string>
+    sanitize: (uri: string, options: any) => Promise<{href: string}>
+    http: (uri: string, options: any) => Promise<string>
+    file: (filename: string) => Promise<string>
+  }
+
   export class View {
     constructor(runtime: any, config?: any);
-    public logLevel(level: number): View;
     public initialize(dom: Element | string): View;
-    public renderer(renderer: string): View;
     public finalize(): void;
+    public logLevel(level: number): View;
+    public renderer(renderer: 'canvas' | 'svg'): View;
+    public loader(loader: Loader): View;
 
     public hover(): View;
     public run(): View;
@@ -21,9 +30,10 @@ declare module 'vega' {
 
     public toImageURL(type: string): Promise<string>;
   }
+
   export const Warn: number;
   export const changeset: any;
-  export const loader: any;
+  export const loader: () => Loader;
 }
 
 declare module 'vega-event-selector' {
