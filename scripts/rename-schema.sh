f28c71c2a7614c1f52a491a8f7f8115a8b385c90
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-03-11 18:29:23 -0700

    Simplify rename-schema.sh

--- scripts/rename-schema.sh
+++ scripts/rename-schema.sh
@@ -6,11 +6,7 @@ perl -pi -e s,'<\(string\|RepeatRef\)\,','<',g build/vega-lite-schema.json
 perl -pi -e s,'FacetedCompositeUnitSpec','FacetedUnitSpec',g build/vega-lite-schema.json
 perl -pi -e s,'ExtendedLayerSpec','LayerSpec',g build/vega-lite-schema.json
 perl -pi -e s,'GenericLayerSpec<CompositeUnitSpec>','LayerSpec',g build/vega-lite-schema.json
-perl -pi -e s,'GenericSpec<CompositeUnitSpec\,LayerSpec>','Spec',g build/vega-lite-schema.json
-perl -pi -e s,'GenericFacetSpec<CompositeUnitSpec\,LayerSpec>','FacetSpec',g build/vega-lite-schema.json
-perl -pi -e s,'GenericRepeatSpec<CompositeUnitSpec\,LayerSpec>','RepeatSpec',g build/vega-lite-schema.json
-perl -pi -e s,'GenericVConcatSpec<CompositeUnitSpec\,LayerSpec>','VConcatSpec',g build/vega-lite-schema.json
-perl -pi -e s,'GenericHConcatSpec<CompositeUnitSpec\,LayerSpec>','HConcatSpec',g build/vega-lite-schema.json
+perl -pi -e s,'Generic(.*)<CompositeUnitSpec\,LayerSpec>','\1',g build/vega-lite-schema.json
 
 perl -pi -e s,'GenericUnitSpec<EncodingWithFacet\,AnyMark>','FacetedCompositeUnitSpecAlias',g build/vega-lite-schema.json
 perl -pi -e s,'GenericUnitSpec<Encoding\,AnyMark>','CompositeUnitSpecAlias',g build/vega-lite-schema.json
