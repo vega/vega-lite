b7a800c2f32c8a4882fb0503eeb2e8d132d508da
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-03-11 19:22:28 -0700

    Make Split include `Partial` by default

--- src/compile/legend/component.ts
+++ src/compile/legend/component.ts
@@ -4,7 +4,7 @@ import {VgLegend} from '../../vega.schema';
 import {Split} from '../split';
 
 
-export class LegendComponent extends Split<Partial<VgLegend>> {}
+export class LegendComponent extends Split<VgLegend> {}
 
 // Using Mapped Type to declare type (https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types)
 export type LegendComponentIndex = {[P in NonPositionScaleChannel]?: LegendComponent};
