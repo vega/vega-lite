73bc68bb3fe8351f53ff3c431d4d0a14f6bfca9d
mwc22 <mwc22@cs.washington.edu>
2018-03-16 16:07:04 -0700

    Improve Box Plot Docs

--- src/compositemark/boxplot.ts
+++ src/compositemark/boxplot.ts
@@ -39,15 +39,17 @@ export interface BoxPlotConfig extends BoxPlotPartsMixins {
   /**
    * The extent of the whiskers. Available options include:
    * - `"min-max": min and max are the lower and upper whiskers respectively.
-   * - `"number": multiples of the interquartile range (Q3-Q1) A number that will be multiplied by the IQR and the product will be added to the third quartile to get the upper whisker and subtracted from the first quartile to get the lower whisker.
+   * - A number representing multiple of the interquartile range (Q3-Q1).  This number will be multiplied by the IQR. the product will be added to the third quartile to get the upper whisker and subtracted from the first quartile to get the lower whisker.
    *
-   * __Default value:__ `"1.5"`.
+   * __Default value:__ `1.5`.
    */
   extent?: 'min-max' | number;
 }
 export interface BoxPlotDef extends GenericMarkDef<BoxPlot>, BoxPlotConfig {
   /**
-   * Orientation of the box plot.  This is normally automatically determined, but can be specified when the orientation is ambiguous and cannot be automatically determined.
+   * Orientation of the box plot.  This is normally automatically determined based on types of fields on x and y channels. However, an explicit `orient` be specified when the orientation is ambiguous.
+   *
+   * __Default value:__ `"vertical"`.
    */
   orient?: Orient;
 }
