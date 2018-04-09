f05cbfa0e3c8dba99426152a0f73bfd0191d3762
Souvik Sen <souviksen44@gmail.com>
2018-04-09 18:40:32 +0530

    Add missed Variable Rename

--- test-gallery/main.ts
+++ test-gallery/main.ts
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 import * as vega from 'vega';
 import * as vl from '../src/index';
-import {TopLevelExtendedSpec} from '../src/spec';
+import {TopLevelSpec} from '../src/spec';
 
 d3.select('#vl-version').text(vl.version);
 d3.select('#vg-version').text(vega.version);
@@ -28,7 +28,7 @@ loadJSON('examples/all-examples.json', function(examples: string[]) {
   vizEnter.append('div').attr('class', 'view');
 
   examples.forEach(function(example) {
-    loadJSON('examples/specs/' + example + '.vl.json', function(vlSpec: TopLevelExtendedSpec) {
+    loadJSON('examples/specs/' + example + '.vl.json', function(vlSpec: TopLevelSpec) {
       const vgSpec = vl.compile(vlSpec).spec;
       const runtime = vega.parse(vgSpec); // may throw an Error if parsing fails
       new vega.View(runtime)
