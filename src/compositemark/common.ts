3a7ab3d6e51f5a2dc0850e4a5468d7ff62334775
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-04-21 14:09:22 -0700

    Add `partLayerMixins` so we allow parts of composite mark to be excluded

--- src/compositemark/common.ts
+++ src/compositemark/common.ts
@@ -1,11 +1,29 @@
-import {GenericMarkDef, MarkConfig, VL_ONLY_MARK_CONFIG_PROPERTIES} from '../mark';
+import {GenericMarkDef, isMarkDef, MarkConfig, VL_ONLY_MARK_CONFIG_PROPERTIES} from '../mark';
+import {NormalizedUnitSpec} from '../spec';
 
 export type PartsMixins<P extends string> = {
   [part in P]?: MarkConfig
 };
 
-export function getMarkDefMixins<P extends PartsMixins<any>>(
-  markDef: GenericMarkDef<any> & P, part: keyof P, cmarkConfig: P
+export function partLayerMixins<P extends PartsMixins<any>>(
+  markDef: GenericMarkDef<any> & P, part: keyof P, compositeMarkConfig: P,
+  baseSpec: NormalizedUnitSpec,
+  shownByDefault: boolean = true
+): NormalizedUnitSpec[] {
+  if (markDef[part] || (markDef[part] === undefined && shownByDefault)) {
+    return [{
+      ...baseSpec,
+      mark: {
+        ...(isMarkDef(baseSpec.mark) ? baseSpec.mark : {type: baseSpec.mark}),
+        ...getMarkDefMixins(markDef, part, compositeMarkConfig)
+      }
+    }];
+  }
+  return [];
+}
+
+function getMarkDefMixins<P extends PartsMixins<any>>(
+  markDef: GenericMarkDef<any> & P, part: keyof P, compositeMarkConfig: P
 ) {
   const mark = markDef.type;
   const partMarkDef: MarkConfig = markDef[part];
@@ -16,8 +34,8 @@ export function getMarkDefMixins<P extends PartsMixins<any>>(
   };
 
   for (const prop of VL_ONLY_MARK_CONFIG_PROPERTIES) {
-    if (cmarkConfig && cmarkConfig[part] && cmarkConfig[part][prop] !== undefined) {
-      mixins[prop] = cmarkConfig[part][prop];
+    if (compositeMarkConfig && compositeMarkConfig[part] && compositeMarkConfig[part][prop] !== undefined) {
+      mixins[prop] = compositeMarkConfig[part][prop];
     }
   }
 
