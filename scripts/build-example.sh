a4bd6fab3048f28d7656de69be4758620286e9e1
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-04-25 20:54:59 -0700

    Make build:example correctly build SVG for new examples

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -14,7 +14,11 @@ do
   rm -f examples/compiled/$name.vg.json
   bin/vl2vg -p examples/specs/$name.vl.json > examples/compiled/$name.vg.json
 
-  if (! git diff $nopatch --exit-code HEAD -- $dir/$name.vg.json || $forcesvg)
+  # compile SVG if one of the following condition is true
+  # 1) Vega spec has changed
+  # 2) The SVG file does not exist (new example would not have vg file diff)
+  # or 3) the forcesvg environment variable is true
+  if (! git diff $nopatch --exit-code HEAD -- $dir/$name.vg.json || [ ! -f $dir/$name.svg ] || $forcesvg)
   then
     rm -f examples/compiled/$name.svg
     node_modules/vega/bin/vg2svg --seed 123456789 examples/compiled/$name.vg.json > examples/compiled/$name.svg -b .
