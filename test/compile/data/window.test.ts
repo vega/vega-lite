684c1c021e0169ed2613f2aedeb946ecdd203b8f
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-05-24 11:38:34 -0700

    Refactor window unit test tabbing

--- test/compile/data/window.test.ts
+++ test/compile/data/window.test.ts
@@ -5,93 +5,93 @@ import {WindowTransformNode} from '../../../src/compile/data/window';
 import {Transform} from '../../../src/transform';
 
 describe('compile/data/window', () => {
-  describe('TestWindowTransform()', () => {
-    it ('should return a proper vg transform', () => {
-      const transform: Transform = {
-        window: [
-          {
-            op: 'row_number',
-            as: 'ordered_row_number',
-          },
-        ],
-        ignorePeers: false,
-        sort:
-          [
-            {
-              field:'f',
-              order:'ascending'
-            }
-          ],
-        groupby: ['f'],
-        frame: [null, 0]
-      };
-      const window = new WindowTransformNode(null, transform);
-      assert.deepEqual(window.assemble(), {
-        type: 'window',
-        ops: ['row_number'],
-        fields: [null],
-        params: [null],
-        sort : {
-          field: ["f"],
-          order: ["ascending"],
+  it('should return a proper vg transform', () => {
+    const transform: Transform = {
+      window: [
+        {
+          op: 'row_number',
+          as: 'ordered_row_number',
         },
-        ignorePeers: false,
-        as: ['ordered_row_number'],
-        frame: [null, 0],
-        groupby: ['f']
-      });
-    });
-    it ('should return a producer proper fields', () => {
-      const transform: Transform = {
-        window: [
+      ],
+      ignorePeers: false,
+      sort:
+        [
           {
-            op: 'row_number',
-            as: 'ordered_row_number',
-          },
-          {
-            op: 'count',
-            as: 'count_field'
-          },
-          {
-            op: 'sum',
-            as: 'sum_field'
+            field:'f',
+            order:'ascending'
           }
         ],
-        ignorePeers: false,
-        sort:
-          [
-            {
-              field:'f',
-              order:'ascending'
-            }
-          ],
-        groupby: ['f'],
-        frame: [null, 0]
-      };
-      const window = new WindowTransformNode(null, transform);
-      assert.deepEqual({"count_field": true, "ordered_row_number": true, "sum_field": true}, window.producedFields());
+      groupby: ['f'],
+      frame: [null, 0]
+    };
+    const window = new WindowTransformNode(null, transform);
+    assert.deepEqual(window.assemble(), {
+      type: 'window',
+      ops: ['row_number'],
+      fields: [null],
+      params: [null],
+      sort : {
+        field: ["f"],
+        order: ["ascending"],
+      },
+      ignorePeers: false,
+      as: ['ordered_row_number'],
+      frame: [null, 0],
+      groupby: ['f']
     });
-    it ('should clone to an equivalent version', () => {
-      const transform: Transform = {
-        window: [
+  });
+
+  it('should return a proper produced fields', () => {
+    const transform: Transform = {
+      window: [
+        {
+          op: 'row_number',
+          as: 'ordered_row_number',
+        },
+        {
+          op: 'count',
+          as: 'count_field'
+        },
+        {
+          op: 'sum',
+          as: 'sum_field'
+        }
+      ],
+      ignorePeers: false,
+      sort:
+        [
           {
-            op: 'row_number',
-            as: 'ordered_row_number',
-          },
+            field:'f',
+            order:'ascending'
+          }
         ],
-        ignorePeers: false,
-        sort:
-          [
-            {
-              field:'f',
-              order:'ascending'
-            }
-          ],
-        groupby: ['f'],
-        frame: [null, 0]
-      };
-      const window = new WindowTransformNode(null, transform);
-      assert.deepEqual(window, window.clone());
-    });
+      groupby: ['f'],
+      frame: [null, 0]
+    };
+    const window = new WindowTransformNode(null, transform);
+    assert.deepEqual({"count_field": true, "ordered_row_number": true, "sum_field": true}, window.producedFields());
+  });
+
+  it('should clone to an equivalent version', () => {
+    const transform: Transform = {
+      window: [
+        {
+          op: 'row_number',
+          as: 'ordered_row_number',
+        },
+      ],
+      ignorePeers: false,
+      sort:
+        [
+          {
+            field:'f',
+            order:'ascending'
+          }
+        ],
+      groupby: ['f'],
+      frame: [null, 0]
+    };
+    const window = new WindowTransformNode(null, transform);
+    assert.deepEqual(window, window.clone());
   });
 });
