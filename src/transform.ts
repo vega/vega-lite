c76c1f3ae4523d9f50425daa5ebdb714c050885d
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-05-24 11:48:43 -0700

    Add lt, lte, gt, gte predicates (#3561) (#3809)
    * Add lt, lte, gt, gte filters (#3561)
    
    * Add the filter predicates
    
    * Add tests for lt, lte, gt, gte filters
    
    * Clean up comments and fix typo
    
    * Increase stack-size while building the schema
    
    * Remove unused imports.
    
    * Add new schema.
    
    * Add docs for lt, lte, gt, gte transforms
    
    * Remove unnecessary comment.
    
    * Refactor to use FieldPredicateBase
    
    * Add docs with suggested changes
    
    * Add more explanation of string comparison
    
    * Fix explanation and remove field, timeUnit from range and oneOf
    
    * Value in backticks should be the actual operator, not an explanatory text.
    
    * Fix filter description in the schema
    
    * When extracting properties, comment should be extracted too

--- src/transform.ts
+++ src/transform.ts
@@ -10,12 +10,21 @@ import {TimeUnit} from './timeunit';
 export interface FilterTransform {
   /**
    * The `filter` property must be one of the predicate definitions:
-   * (1) an [expression](https://vega.github.io/vega-lite/docs/types.html#expression) string,
-   * where `datum` can be used to refer to the current data object;
-   * (2) one of the field predicates: [equal predicate](https://vega.github.io/vega-lite/docs/filter.html#equal-predicate);
-   * [range predicate](https://vega.github.io/vega-lite/docs/filter.html#range-predicate), [one-of predicate](https://vega.github.io/vega-lite/docs/filter.html#one-of-predicate);
-   * (3) a [selection predicate](https://vega.github.io/vega-lite/docs/filter.html#selection-predicate);
-   * or (4) a logical operand that combines (1), (2), or (3).
+   *
+   * 1) an [expression](https://vega.github.io/vega-lite/docs/types.html#expression) string,
+   * where `datum` can be used to refer to the current data object
+   *
+   * 2) one of the field predicates: [`equal`](https://vega.github.io/vega-lite/docs/filter.html#equal-predicate),
+   * [`lt`](https://vega.github.io/vega-lite/docs/filter.html#lt-predicate),
+   * [`lte`](https://vega.github.io/vega-lite/docs/filter.html#lte-predicate),
+   * [`gt`](https://vega.github.io/vega-lite/docs/filter.html#gt-predicate),
+   * [`gte`](https://vega.github.io/vega-lite/docs/filter.html#gte-predicate),
+   * [`range`](https://vega.github.io/vega-lite/docs/filter.html#range-predicate),
+   * or [`oneOf`](https://vega.github.io/vega-lite/docs/filter.html#one-of-predicate).
+   *
+   * 3) a [selection predicate](https://vega.github.io/vega-lite/docs/filter.html#selection-predicate)
+   *
+   * 4) a logical operand that combines (1), (2), or (3).
    */
   // TODO: https://github.com/vega/vega-lite/issues/2901
   filter: LogicalOperand<Predicate>;
