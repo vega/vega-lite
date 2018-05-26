a010413073b33c51cbdcfe412f4b41ec7761fe52
Chanwut Kittivorawong <chanwutk@gmail.com>
2018-05-25 23:05:13 -0700

    Initial Commit for error band
    Adding return type to errorBarParams function
    
    Remove unused imports and update schema

--- src/compositemark/errorbar.ts
+++ src/compositemark/errorbar.ts
@@ -128,6 +128,10 @@ export function errorBarParams<M extends ErrorBar | ErrorBand, MD extends Generi
   let errorbarSpecificAggregate: AggregatedFieldDef[] = [];
   let postAggregateCalculates: CalculateTransform[] = [];
 
+  if ((center === 'median') !== (extent === 'iqr')) {
+    log.warn(`${center} is not usually used with ${extent} for error bar.`);
+  }
+
   if (extent === 'stderr' || extent === 'stdev') {
     errorbarSpecificAggregate = [{
       op: extent,
