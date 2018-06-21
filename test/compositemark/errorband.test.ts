871cb61ac8fdcbc4fb71d0235054c90c39ac0162
Chanwut Kittivorawong <chanwutk@gmail.com>
2018-06-21 14:20:08 -0700

    Add title to an axis in extractTransformFromEncoding

--- test/compositemark/errorband.test.ts
+++ test/compositemark/errorband.test.ts
@@ -73,7 +73,8 @@ describe('normalizeErrorBand', () => {
             },
             "x": {
               "field": "age",
-              "type": "ordinal"
+              "type": "ordinal",
+              "title": "age"
             }
           }
         }
