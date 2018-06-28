5ca1ec60cc64f67e20180206a1621701e6eb8faa
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-06-27 22:45:55 -0700

    Make `sort` apply `timeUnit`
    Fix #3919
    
    ...

--- src/compile/data/calculate.ts
+++ src/compile/data/calculate.ts
@@ -1,4 +1,5 @@
-import {FieldDef, isScaleFieldDef, ScaleFieldDef, vgField} from '../../fielddef';
+import {FieldDef, isScaleFieldDef, vgField} from '../../fielddef';
+import {fieldFilterExpression} from '../../predicate';
 import {isSortArray} from '../../sort';
 import {duplicate} from '../../util';
 import {VgFormulaTransform} from '../../vega.schema';
@@ -21,26 +22,24 @@ export class CalculateNode extends DataFlowNode {
 
   public static parseAllForSortIndex(parent: DataFlowNode, model: ModelWithField) {
     // get all the encoding with sort fields from model
-    model.forEachFieldDef((fieldDef: ScaleFieldDef<string>, channel: SingleDefChannel) => {
-      if (isScaleFieldDef(fieldDef) && isSortArray(fieldDef.sort)) {
-        const transform: CalculateTransform = {
-          calculate: CalculateNode.calculateExpressionFromSortField(fieldDef.field, fieldDef.sort),
+    model.forEachFieldDef((fieldDef: FieldDef<string>, channel: SingleDefChannel) => {
+      if (!isScaleFieldDef(fieldDef)) {
+        return;
+      }
+      if (isSortArray(fieldDef.sort)) {
+        const {field, timeUnit, sort} = fieldDef;
+        // generate `datum["a"] === val0 ? 0 : datum["a"] === val1 ? 1 : ... : n` via FieldEqualPredicate
+        const calculate = sort.map((sortValue, i) => {
+          return `${fieldFilterExpression({field, timeUnit, equal: sortValue})} ? ${i} : `;
+        }).join('') + sort.length;
+
+        parent = new CalculateNode(parent, {
+          calculate,
           as: sortArrayIndexField(fieldDef, channel)
-        };
-        parent = new CalculateNode(parent, transform);
+        });
       }
     });
     return parent;
-  }
-
-  public static calculateExpressionFromSortField(field: string, sortFields: (string | number | boolean)[]): string {
-    let expression = '';
-    let i: number;
-    for (i = 0; i < sortFields.length; i++) {
-      expression += `datum.${field} === '${sortFields[i]}' ? ${i} : `;
-    }
-    expression += i;
-    return expression;
   }
 
   public producedFields() {
