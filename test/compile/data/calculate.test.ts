6afa3d7f7a55b10de5aa862e48030c4ea33c4126
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-06-27 22:45:55 -0700

    Make `sort` apply `timeUnit`
    Fix #3919
    
    ...

--- test/compile/data/calculate.test.ts
+++ test/compile/data/calculate.test.ts
@@ -1,9 +1,8 @@
 /* tslint:disable:quotemark */
-import {assert} from 'chai';
-
 import {CalculateNode} from '../../../src/compile/data/calculate';
 import {ModelWithField} from '../../../src/compile/model';
 import {parseUnitModel} from '../../util';
+
 
 
 function assembleFromSortArray(model: ModelWithField) {
@@ -12,29 +11,26 @@ function assembleFromSortArray(model: ModelWithField) {
 }
 
 describe('compile/data/calculate', () => {
-  it('makeAllForSortIndex', () => {
-    const model = parseUnitModel({
-      data: {
-        values: [
-          {a: 'A',b: 28}, {a: 'B',b: 55}, {a: 'C',b: 43}
-        ]
-      },
-      mark: 'bar',
-        encoding: {
-          x: {field: 'a', type: 'ordinal', sort: ['B', 'A', 'C']},
-          y: {field: 'b', type: 'quantitative'}
-        }
+  describe('makeAllForSortIndex', () => {
+    it('produces correct formula transform', () => {
+      const model = parseUnitModel({
+        data: {
+          values: [
+            {a: 'A',b: 28}, {a: 'B',b: 55}, {a: 'C',b: 43}
+          ]
+        },
+        mark: 'bar',
+          encoding: {
+            x: {field: 'a', type: 'ordinal', sort: ['B', 'A', 'C']},
+            y: {field: 'b', type: 'quantitative'}
+          }
+      });
+      const nodes = assembleFromSortArray(model);
+      expect(nodes).toEqual({
+        type: 'formula',
+        expr: 'datum["a"]==="B" ? 0 : datum["a"]==="A" ? 1 : datum["a"]==="C" ? 2 : 3',
+        as: 'x_a_sort_index'
+      });
     });
-    const nodes = assembleFromSortArray(model);
-    assert.deepEqual(nodes, {
-      type: 'formula',
-      expr: "datum.a === 'B' ? 0 : datum.a === 'A' ? 1 : datum.a === 'C' ? 2 : 3",
-      as: 'x_a_sort_index'
-    });
-  });
-
-  it('calculateExpressionFromSortField', () => {
-    const expression = CalculateNode.calculateExpressionFromSortField('a', ["B", "A", "C"]);
-    assert.equal(expression, "datum.a === 'B' ? 0 : datum.a === 'A' ? 1 : datum.a === 'C' ? 2 : 3");
   });
 });
