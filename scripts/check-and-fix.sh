fde2b5a8be259554b75912849ed75d375ebc0ea5
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-07-07 17:09:28 -0700

    Make check-and-fix work for new examples too (#3997)
    * Make check-and-fix works for new examples too
    
    * Update check-and-fix.sh

--- scripts/check-and-fix.sh
+++ scripts/check-and-fix.sh
@@ -27,13 +27,16 @@ echo "------- Checking Examples -------"
 echo ""
 
 # Commit examples if outdated
-if ! git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/specs/normalized/*.vl.json
+
+# Note: we need to add all files first so that new files are included in `git diff --cached` too.
+git add ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
+
+if ! git diff --cached --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
 then
   if [[ $TRAVIS_BRANCH == 'master' ]]; then
     echo "Outdated examples."
     exit 1
   else
-    git add ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/specs/normalized/*.vl.json
     git commit -m "[Travis] Update examples (build: $TRAVIS_BUILD_NUMBER)"
   fi
 fi
