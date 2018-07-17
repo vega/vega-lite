a07f6b3489b08f7d6c005c4dcfcc00bd13e7b8a5
Dominik Moritz <domoritz@gmail.com>
2018-07-17 10:41:35 -0700

    Remove support for format parse auto. Fixes #3970 (#4046)
    * Remove support for format parse auto. Fixes #3970
    
    * [Travis] Update schema (build: 18338)

--- src/data.ts
+++ src/data.ts
@@ -11,7 +11,6 @@ export interface Parse {
 
 export interface DataFormatBase {
   /**
-   * If set to `"auto"` (the default), perform automatic type inference to determine the desired data types.
    * If set to `null`, disable type inference based on the spec and only use type inference based on the data.
    * Alternatively, a parsing directive object can be provided for explicit data types. Each property of the object corresponds to a field name, and the value to the desired data type (one of `"number"`, `"boolean"`, `"date"`, or null (do not parse the field)).
    * For example, `"parse": {"modified_on": "date"}` parses the `modified_on` field in each input record a Date value.
@@ -19,7 +18,7 @@ export interface DataFormatBase {
    * For `"date"`, we parse data based using Javascript's [`Date.parse()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse).
    * For Specific date formats can be provided (e.g., `{foo: 'date:"%m%d%Y"'}`), using the [d3-time-format syntax](https://github.com/d3/d3-time-format#locale_format). UTC date format parsing is supported similarly (e.g., `{foo: 'utc:"%m%d%Y"'}`). See more about [UTC time](https://vega.github.io/vega-lite/docs/timeunit.html#utc)
    */
-  parse?: 'auto' | Parse | null;
+  parse?: Parse | null;
 }
 
 export interface CsvDataFormat extends DataFormatBase {
