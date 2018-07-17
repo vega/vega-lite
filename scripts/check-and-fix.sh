89484cd5f4bcdd975b5d1ac4ae716260b8ebf3f6
Dominik Moritz <domoritz@gmail.com>
2018-07-16 22:31:41 -0700

    Update dependencies. Vega 4.1 (#4027)
    * [SVG] Update dependencies. Vega 4.1
    
    * [Build] Make check-and-fix re-build all SVGs if vega version changes
    
    * Only run test on Node 8
    
    * [Travis] Update examples (build: 18270)
    
    * Don't fail the test if there is SVG diff
    
    * Also don't diff vega_version

--- scripts/check-and-fix.sh
+++ scripts/check-and-fix.sh
@@ -31,6 +31,10 @@ if git log -1 | grep "\[SVG\]" && [[ $TRAVIS_BRANCH != 'master' ]]
 then
   echo "As the latest commit includes [SVG], let's force rebuilding all SVGs"
   yarn build:examples-full
+elif git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version
+then
+  echo "Different Vega version, let's force rebuilding all SVGs"
+  yarn build:examples-full
 else
   yarn build:examples
 fi
@@ -40,12 +44,16 @@ fi
 # Note: we need to add all files first so that new files are included in `git diff --cached` too.
 git add ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
 
-if ! git diff --cached --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
-then
-  if [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  # Don't diff SVG as floating point calculation is not always consistent
+  if ! git diff --cached --word-diff=color --exit-code HEAD -- ./examples/compiled/*.vg.json ./examples/specs/normalized/*.vl.json
+  then
     echo "Outdated examples."
     exit 1
-  else
+  fi
+else
+  if ! git diff --cached --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
+  then
     git commit -m "[Travis] Update examples (build: $TRAVIS_BUILD_NUMBER)"
   fi
 fi
