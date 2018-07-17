5308e4d129d04d0b0f9cdbeb56c842d344e3ae3c
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-07-16 19:13:26 -0700

    [Build] Make check-and-fix re-build all SVGs if vega version changes

--- scripts/check-and-fix.sh
+++ scripts/check-and-fix.sh
@@ -31,6 +31,10 @@ if git log -1 | grep "\[SVG\]" && [[ $TRAVIS_BRANCH != 'master' ]]
 then
   echo "As the latest commit includes [SVG], let's force rebuilding all SVGs"
   yarn build:examples-full
+elif git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version
+then
+  echo "Different Vega version, let's force rebuilding all SVGs"
+  yarn build:examples-full
 else
   yarn build:examples
 fi
