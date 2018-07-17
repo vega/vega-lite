99d14e3f97ea56b817ab67807faa0c89201af7e6
Dominik Moritz <domoritz@gmail.com>
2018-07-16 22:33:46 -0700

    Run push examples only on node 8

--- scripts/check-and-fix.sh
+++ scripts/check-and-fix.sh
@@ -4,6 +4,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git checkout $TRAVIS_BRANCH
 
+node_version=$(node --version | cut -d. -f1 | cut -c2-)
 
 echo ""
 echo "------- Checking Schema -------"
@@ -27,7 +28,7 @@ echo "------- Checking Examples -------"
 echo ""
 
 
-if git log -1 | grep "\[SVG\]" && [[ $TRAVIS_BRANCH != 'master' ]]
+if git log -1 | grep "\[SVG\]" && [[ $TRAVIS_BRANCH != 'master' ]] && [[ $node_version == '8' ]]
 then
   echo "As the latest commit includes [SVG], let's force rebuilding all SVGs"
   yarn build:examples-full
@@ -44,7 +45,7 @@ fi
 # Note: we need to add all files first so that new files are included in `git diff --cached` too.
 git add ./examples/compiled/vega_version ./examples/compiled/*.vg.json ./examples/compiled/*.svg ./examples/specs/normalized/*.vl.json
 
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' ]] || [[ $node_version != '8' ]]; then
   # Don't diff SVG as floating point calculation is not always consistent
   if ! git diff --cached --word-diff=color --exit-code HEAD -- ./examples/compiled/*.vg.json ./examples/specs/normalized/*.vl.json
   then
@@ -64,7 +65,7 @@ echo "------- Checking Code Formatting -------"
 echo ""
 
 ## For non-master branch, commit tslint fix and prettier changes if outdated
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
+if [[ $TRAVIS_BRANCH != 'master' ]] && [[ $node_version == '8' ]]; then
   yarn format
   if ! git diff --word-diff=color --exit-code HEAD -- src test test-runtime
   then
