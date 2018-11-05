5eb9213c4aee485fecb4cecd024e74817d49406b
Souvik Sen <souviksen44@gmail.com>
2018-11-05 22:14:11 +0530

    Move optimizer functions to correct file

--- src/compile/data/optimizers.ts
+++ src/compile/data/optimizers.ts
@@ -1,10 +1,16 @@
-import {fieldIntersection, keys} from '../../util';
+import {MAIN} from '../../data';
+import {fieldIntersection, flatten, keys} from '../../util';
+import {AggregateNode} from './aggregate';
 import {DataFlowNode, OutputNode} from './dataflow';
 import {FacetNode} from './facet';
 import {ParseNode} from './formatparse';
+import {FACET_SCALE_PREFIX} from './optimize';
 import {BottomUpOptimizer, TopDownOptimizer} from './optimizer';
+import * as optimizers from './optimizers';
 import {SourceNode} from './source';
+import {StackNode} from './stack';
 import {TimeUnitNode} from './timeunit';
+import {WindowTransformNode} from './window';
 
 export interface OptimizerFlags {
   /**
@@ -155,6 +161,136 @@ export class RemoveDuplicateTimeUnits extends BottomUpOptimizer {
         this.fields = {...this.fields, ...pfields};
       }
     }
+    return this.flags;
+  }
+}
+
+/**
+ * Clones the subtree and ignores output nodes except for the leaves, which are renamed.
+ */
+function cloneSubtree(facet: FacetNode) {
+  function clone(node: DataFlowNode): DataFlowNode[] {
+    if (!(node instanceof FacetNode)) {
+      const copy = node.clone();
+
+      if (copy instanceof OutputNode) {
+        const newName = FACET_SCALE_PREFIX + copy.getSource();
+        copy.setSource(newName);
+
+        facet.model.component.data.outputNodes[newName] = copy;
+      } else if (copy instanceof AggregateNode || copy instanceof StackNode || copy instanceof WindowTransformNode) {
+        copy.addDimensions(facet.fields);
+      }
+      flatten(node.children.map(clone)).forEach((n: DataFlowNode) => (n.parent = copy));
+
+      return [copy];
+    }
+
+    return flatten(node.children.map(clone));
+  }
+  return clone;
+}
+
+/**
+ * Move facet nodes down to the next fork or output node. Also pull the main output with the facet node.
+ * After moving down the facet node, make a copy of the subtree and make it a child of the main output.
+ */
+export function moveFacetDown(node: DataFlowNode) {
+  if (node instanceof FacetNode) {
+    if (node.numChildren() === 1 && !(node.children[0] instanceof OutputNode)) {
+      // move down until we hit a fork or output node
+      const child = node.children[0];
+
+      if (child instanceof AggregateNode || child instanceof StackNode || child instanceof WindowTransformNode) {
+        child.addDimensions(node.fields);
+      }
+
+      child.swapWithParent();
+      moveFacetDown(node);
+    } else {
+      // move main to facet
+
+      const facetMain = node.model.component.data.main;
+      moveMainDownToFacet(facetMain);
+
+      // replicate the subtree and place it before the facet's main node
+      const cloner = cloneSubtree(node);
+      const copy: DataFlowNode[] = flatten(node.children.map(cloner));
+      for (const c of copy) {
+        c.parent = facetMain;
+      }
+    }
+  } else {
+    node.children.map(moveFacetDown);
+  }
+}
+
+function moveMainDownToFacet(node: DataFlowNode) {
+  if (node instanceof OutputNode && node.type === MAIN) {
+    if (node.numChildren() === 1) {
+      const child = node.children[0];
+      if (!(child instanceof FacetNode)) {
+        child.swapWithParent();
+        moveMainDownToFacet(node);
+      }
+    }
+  }
+}
+
+/**
+ * Remove nodes that are not required starting from a root.
+ */
+export class RemoveUnnecessaryNodes extends TopDownOptimizer {
+  public run(node: DataFlowNode): boolean {
+    // remove output nodes that are not required
+    if (node instanceof OutputNode && !node.isRequired()) {
+      this.setMutated();
+      node.remove();
+    }
+    for (const child of node.children) {
+      this.run(child);
+    }
+
+    return this.mutatedFlag;
+  }
+}
+
+/**
+ * Inserts an Intermediate ParseNode containing all non-conflicting Parse fields and removes the empty ParseNodes
+ */
+export class MergeParse extends BottomUpOptimizer {
+  public run(node: DataFlowNode): optimizers.OptimizerFlags {
+    const parent = node.parent;
+    const parseChildren = parent.children.filter((x): x is ParseNode => x instanceof ParseNode);
+
+    if (parseChildren.length > 1) {
+      const commonParse = {};
+      for (const parseNode of parseChildren) {
+        const parse = parseNode.parse;
+        for (const k of keys(parse)) {
+          if (commonParse[k] === undefined) {
+            commonParse[k] = parse[k];
+          } else if (commonParse[k] !== parse[k]) {
+            delete commonParse[k];
+          }
+        }
+      }
+      if (keys(commonParse).length !== 0) {
+        this.setMutated();
+        const mergedParseNode = new ParseNode(parent, commonParse);
+        for (const parseNode of parseChildren) {
+          for (const key of keys(commonParse)) {
+            delete parseNode.parse[key];
+          }
+          parent.removeChild(parseNode);
+          parseNode.parent = mergedParseNode;
+          if (keys(parseNode.parse).length === 0) {
+            parseNode.remove();
+          }
+        }
+      }
+    }
+    this.setContinue();
     return this.flags;
   }
 }
