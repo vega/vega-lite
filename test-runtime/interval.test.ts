ee819b3f8efd9b26e6b55f67b80bf4b99b4bd47d
Dominik Moritz <domoritz@gmail.com>
2018-12-04 20:43:00 -0800

    Update runtime tests

--- test-runtime/interval.test.ts
+++ test-runtime/interval.test.ts
@@ -1,4 +1,3 @@
-import {assert} from 'chai';
 import {brush, embedFn, hits as hitsMaster, spec, testRenderFn, tuples} from './util';
 
 describe('interval selections at runtime in unit views', () => {
@@ -11,17 +10,17 @@ describe('interval selections at runtime in unit views', () => {
     for (let i = 0; i < hits.drag.length; i++) {
       embed(spec('unit', i, {type}));
       const store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 2);
-      assert.lengthOf(store[0].values, 2);
-      assert.equal(store[0].fields[0].channel, 'x');
-      assert.equal(store[0].fields[0].field, 'a');
-      assert.equal(store[0].fields[0].type, 'R');
-      assert.equal(store[0].fields[1].channel, 'y');
-      assert.equal(store[0].fields[1].field, 'b');
-      assert.equal(store[0].fields[1].type, 'R');
-      assert.lengthOf(store[0].values[0], 2);
-      assert.lengthOf(store[0].values[1], 2);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(2);
+      expect(store[0].values.length).toBe(2);
+      expect(store[0].fields[0].channel).toEqual('x');
+      expect(store[0].fields[0].field).toEqual('a');
+      expect(store[0].fields[0].type).toEqual('R');
+      expect(store[0].fields[1].channel).toEqual('y');
+      expect(store[0].fields[1].field).toEqual('b');
+      expect(store[0].fields[1].type).toEqual('R');
+      expect(store[0].values[0].length).toBe(2);
+      expect(store[0].values[1].length).toBe(2);
       testRender(`drag_${i}`);
     }
   });
@@ -30,26 +29,26 @@ describe('interval selections at runtime in unit views', () => {
     embed(spec('unit', 0, {type, encodings: ['x']}));
     for (let i = 0; i < hits.drag.length; i++) {
       const store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 1);
-      assert.lengthOf(store[0].values, 1);
-      assert.equal(store[0].fields[0].channel, 'x');
-      assert.equal(store[0].fields[0].field, 'a');
-      assert.equal(store[0].fields[0].type, 'R');
-      assert.lengthOf(store[0].values[0], 2);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(1);
+      expect(store[0].values.length).toBe(1);
+      expect(store[0].fields[0].channel).toEqual('x');
+      expect(store[0].fields[0].field).toEqual('a');
+      expect(store[0].fields[0].type).toEqual('R');
+      expect(store[0].values[0].length).toBe(2);
       testRender(`x_${i}`);
     }
 
     embed(spec('unit', 1, {type, encodings: ['y']}));
     for (let i = 0; i < hits.drag.length; i++) {
       const store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 1);
-      assert.lengthOf(store[0].values, 1);
-      assert.equal(store[0].fields[0].channel, 'y');
-      assert.equal(store[0].fields[0].field, 'b');
-      assert.equal(store[0].fields[0].type, 'R');
-      assert.lengthOf(store[0].values[0], 2);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(1);
+      expect(store[0].values.length).toBe(1);
+      expect(store[0].fields[0].channel).toEqual('y');
+      expect(store[0].fields[0].field).toEqual('b');
+      expect(store[0].fields[0].type).toEqual('R');
+      expect(store[0].values[0].length).toBe(2);
       testRender(`y_${i}`);
     }
   });
@@ -58,10 +57,10 @@ describe('interval selections at runtime in unit views', () => {
     for (let i = 0; i < hits.drag_clear.length; i++) {
       embed(spec('unit', i, {type}));
       let store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
+      expect(store.length).toBe(1);
 
       store = browser.execute(brush('drag_clear', i)).value;
-      assert.lengthOf(store, 0);
+      expect(store.length).toBe(0);
       testRender(`clear_${i}`);
     }
   });
@@ -81,18 +80,18 @@ describe('interval selections at runtime in unit views', () => {
     );
     for (let i = 0; i < hits.bins.length; i++) {
       const store = browser.execute(brush('bins', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 1);
-      assert.lengthOf(store[0].values, 1);
-      assert.equal(store[0].fields[0].channel, 'y');
-      assert.equal(store[0].fields[0].field, 'b');
-      assert.equal(store[0].fields[0].type, 'R');
-      assert.lengthOf(store[0].values[0], 2);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(1);
+      expect(store[0].values.length).toBe(1);
+      expect(store[0].fields[0].channel).toEqual('y');
+      expect(store[0].fields[0].field).toEqual('b');
+      expect(store[0].fields[0].type).toEqual('R');
+      expect(store[0].values[0].length).toBe(2);
       testRender(`bins_${i}`);
     }
 
     const store = browser.execute(brush('bins_clear', 0)).value;
-    assert.lengthOf(store, 0);
+    expect(store.length).toBe(0);
   });
 
   it('should brush over ordinal/nominal domains', () => {
@@ -105,22 +104,22 @@ describe('interval selections at runtime in unit views', () => {
     for (let i = 0; i < hits.drag.length; i++) {
       embed(spec('unit', i, {type}, {x: {type: 'ordinal'}, y: {type: 'nominal'}}));
       const store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 2);
-      assert.lengthOf(store[0].values, 2);
-      assert.equal(store[0].fields[0].channel, 'x');
-      assert.equal(store[0].fields[0].field, 'a');
-      assert.equal(store[0].fields[0].type, 'E');
-      assert.equal(store[0].fields[1].channel, 'y');
-      assert.equal(store[0].fields[1].field, 'b');
-      assert.equal(store[0].fields[1].type, 'E');
-      assert.sameMembers(store[0].values[0], xextents[i]);
-      assert.sameMembers(store[0].values[1], yextents[i]);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(2);
+      expect(store[0].values.length).toBe(2);
+      expect(store[0].fields[0].channel).toEqual('x');
+      expect(store[0].fields[0].field).toEqual('a');
+      expect(store[0].fields[0].type).toEqual('E');
+      expect(store[0].fields[1].channel).toEqual('y');
+      expect(store[0].fields[1].field).toEqual('b');
+      expect(store[0].fields[1].type).toEqual('E');
+      expect(store[0].values[0]).toEqual(xextents[i]);
+      expect(store[0].values[1]).toEqual(yextents[i]);
       testRender(`ord_${i}`);
     }
 
     const store = browser.execute(brush('drag_clear', 0)).value;
-    assert.lengthOf(store, 0);
+    expect(store.length).toBe(0);
   });
 
   it('should brush over temporal domains', () => {
@@ -131,24 +130,24 @@ describe('interval selections at runtime in unit views', () => {
     let extents = [[1485969714000, 1493634384000], [1496346498000, 1504364922000]];
     for (let i = 0; i < hits.drag.length; i++) {
       const store = browser.execute(brush('drag', i) + toNumber).value;
-      assert.sameMembers(store, extents[i]);
+      expect(store).toEqual(extents[i]);
       testRender(`temporal_${i}`);
     }
 
     let cleared = browser.execute(brush('drag_clear', 0)).value;
-    assert.lengthOf(cleared, 0);
+    expect(cleared.length).toBe(0);
 
     embed(spec('unit', 1, {type, encodings: ['x']}, {values, x: {type: 'temporal', timeUnit: 'day'}}));
 
     extents = [[1136190528000, 1136361600000], [1136449728000, 1136535264000]];
     for (let i = 0; i < hits.drag.length; i++) {
       const store = browser.execute(brush('drag', i) + toNumber).value;
-      assert.sameMembers(store, extents[i]);
+      expect(store).toEqual(extents[i]);
       testRender(`dayTimeUnit_${i}`);
     }
 
     cleared = browser.execute(brush('drag_clear', 0)).value;
-    assert.lengthOf(cleared, 0);
+    expect(cleared.length).toBe(0);
   });
 
   it('should brush over log/pow scales', () => {
@@ -165,11 +164,11 @@ describe('interval selections at runtime in unit views', () => {
         )
       );
       const store = browser.execute(brush('drag', i)).value;
-      assert.lengthOf(store, 1);
-      assert.lengthOf(store[0].fields, 2);
-      assert.lengthOf(store[0].values, 2);
-      assert.lengthOf(store[0].values[0], 2);
-      assert.lengthOf(store[0].values[1], 2);
+      expect(store.length).toBe(1);
+      expect(store[0].fields.length).toBe(2);
+      expect(store[0].values.length).toBe(2);
+      expect(store[0].values[0].length).toBe(2);
+      expect(store[0].values[1].length).toBe(2);
       testRender(`logpow_${i}`);
     }
   });
--- test-runtime/translate.test.ts
+++ test-runtime/translate.test.ts
@@ -1,4 +1,3 @@
-import {assert} from 'chai';
 import {
   bound,
   brush,
