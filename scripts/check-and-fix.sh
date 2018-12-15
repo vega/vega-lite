b32d8e68702909dfabe4cf7a8549377e40d23e42
Chanwut Kittivorawong <chanwutk@gmail.com>
2018-12-14 20:03:05 -0800

    add check-and-fix

--- scripts/check-and-fix.sh
+++ scripts/check-and-fix.sh
@@ -34,11 +34,11 @@ echo ""
 
 if git log -1 | grep "\[SVG\]" && [[ $TRAVIS_BRANCH != 'master' ]]; then
   echo "As the latest commit includes [SVG], let's force rebuilding all SVGs"
-  yarn build:examples-full
+  yarn build:examples-full 1
 elif git diff --word-diff=color --exit-code HEAD -- ./examples/compiled/vega_version
 then
   echo "Different Vega version, let's force rebuilding all SVGs"
-  yarn build:examples-full
+  yarn build:examples-full 1
 else
   yarn build:examples
 fi
