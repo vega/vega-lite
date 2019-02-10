8de8c7a5e63a95590315cf5eec83dbbe7f6e3215
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-02-10 11:14:42 -0800

    Add generic `ConcatSpec` that supports wrapping with `columns` (#4535)
    * Add generic `ConcatSpec` that supports wrapping with `columns`
    
    * [Travis] Update examples (build: 21063)
    
    * [Travis] Auto-formatting (build: 21063)

--- src/spec/concat.ts
+++ src/spec/concat.ts
@@ -1,14 +1,13 @@
 import {Resolve} from '../resolve';
-import {BaseSpec, BoundsMixins} from './base';
+import {BaseSpec, BoundsMixins, GenericCompositionLayoutWithColumns} from './base';
 import {GenericSpec} from './index';
 import {GenericLayerSpec, NormalizedLayerSpec} from './layer';
 import {GenericUnitSpec, NormalizedUnitSpec} from './unit';
 
 /**
- * Base layout mixins for V/HConcatSpec.
- * Concat layout should not have RowCol<T> generic fo its property.
+ * Base layout mixins for V/HConcatSpec, which should not have RowCol<T> generic fo its property.
  */
-export interface ConcatLayout extends BoundsMixins {
+export interface OneDirectionalConcatLayout extends BoundsMixins {
   /**
    * Boolean flag indicating if subviews should be centered relative to their respective rows or columns.
    *
@@ -25,13 +24,30 @@ export interface ConcatLayout extends BoundsMixins {
 }
 
 /**
+ * Base interface for a generalized concatenation specification.
+ */
+export interface GenericConcatSpec<U extends GenericUnitSpec<any, any>, L extends GenericLayerSpec<any>>
+  extends BaseSpec,
+    GenericCompositionLayoutWithColumns {
+  /**
+   * A list of views to be concatenated.
+   */
+  concat: (GenericSpec<U, L>)[];
+
+  /**
+   * Scale, axis, and legend resolutions for vertically concatenated charts.
+   */
+  resolve?: Resolve;
+}
+
+/**
  * Base interface for a vertical concatenation specification.
  */
 export interface GenericVConcatSpec<U extends GenericUnitSpec<any, any>, L extends GenericLayerSpec<any>>
   extends BaseSpec,
-    ConcatLayout {
+    OneDirectionalConcatLayout {
   /**
-   * A list of views that should be concatenated and put into a column.
+   * A list of views to be concatenated and put into a column.
    */
   vconcat: (GenericSpec<U, L>)[];
 
@@ -46,9 +62,9 @@ export interface GenericVConcatSpec<U extends GenericUnitSpec<any, any>, L exten
  */
 export interface GenericHConcatSpec<U extends GenericUnitSpec<any, any>, L extends GenericLayerSpec<any>>
   extends BaseSpec,
-    ConcatLayout {
+    OneDirectionalConcatLayout {
   /**
-   * A list of views that should be concatenated and put into a row.
+   * A list of views to be concatenated and put into a row.
    */
   hconcat: (GenericSpec<U, L>)[];
 
@@ -57,13 +73,19 @@ export interface GenericHConcatSpec<U extends GenericUnitSpec<any, any>, L exten
    */
   resolve?: Resolve;
 }
+
 /** A concat spec without any shortcut/expansion syntax */
 export type NormalizedConcatSpec =
+  | GenericConcatSpec<NormalizedUnitSpec, NormalizedLayerSpec>
   | GenericVConcatSpec<NormalizedUnitSpec, NormalizedLayerSpec>
   | GenericHConcatSpec<NormalizedUnitSpec, NormalizedLayerSpec>;
 
-export function isConcatSpec(spec: BaseSpec): spec is GenericVConcatSpec<any, any> | GenericHConcatSpec<any, any> {
-  return isVConcatSpec(spec) || isHConcatSpec(spec);
+export function isAnyConcatSpec(spec: BaseSpec): spec is GenericVConcatSpec<any, any> | GenericHConcatSpec<any, any> {
+  return isVConcatSpec(spec) || isHConcatSpec(spec) || isConcatSpec(spec);
+}
+
+export function isConcatSpec(spec: BaseSpec): spec is GenericConcatSpec<any, any> {
+  return spec['concat'] !== undefined;
 }
 
 export function isVConcatSpec(spec: BaseSpec): spec is GenericVConcatSpec<any, any> {
