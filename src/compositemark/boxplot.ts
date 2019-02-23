d3004aaaa0d4acb46a451bae41359549e085e29e
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-02-23 14:38:00 -0800

    Update docs

--- src/compositemark/boxplot.ts
+++ src/compositemark/boxplot.ts
@@ -44,7 +44,7 @@ export interface BoxPlotConfig extends BoxPlotPartsMixins {
   /**
    * The extent of the whiskers. Available options include:
    * - `"min-max"`: min and max are the lower and upper whiskers respectively.
-   * - A number representing multiple of the interquartile range (Q3-Q1).  This number will be multiplied by the IQR. The product will be added to the third quartile to get the upper whisker and subtracted from the first quartile to get the lower whisker.
+   * - A number representing multiple of the interquartile range.  This number will be multiplied by the IQR to determine whisker boundary, which spans from the smallest data to the largest data within the range _[Q1 - k * IQR, Q3 + k * IQR]_ where _Q1_ and _Q3_ are the first and third quartiles while _IQR_ is the interquartile range (_Q3-Q1_).
    *
    * __Default value:__ `1.5`.
    */
