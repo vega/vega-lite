bcba36205cccc4b1d0105231f2de8accb33d83c2
Dominik Moritz <domoritz@gmail.com>
2019-02-22 18:07:39 -0800

    Remove eval

--- src/compile/signal.ts
+++ src/compile/signal.ts
@@ -1,5 +1,4 @@
 import {SignalRef} from 'vega';
-import {codegen, parse} from 'vega-expression';
 
 export type Rename = (oldSignalName: string) => string;
 
@@ -21,13 +20,4 @@ export class SignalRefWrapper implements SignalRef {
   public static fromName(rename: Rename, signalName: string) {
     return new SignalRefWrapper(() => rename(signalName));
   }
-}
-
-const generate = codegen({globalvar: 'global'});
-
-export function evalExpression(expr: string) {
-  const ast = parse(expr);
-  const {code} = generate(ast);
-  const f = new Function('global', `return ${code};`); // tslint:disable-line:function-constructor
-  return f();
 }
