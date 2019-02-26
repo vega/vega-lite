66c3e73517fb6871096f27824505a11244c5e6a3
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-02-26 15:39:30 -0800

    Rename Repeat to RepeatMapping as it's more accurate (#4599)

--- src/spec/repeat.ts
+++ src/spec/repeat.ts
@@ -1,10 +1,10 @@
 import {BaseSpec, GenericCompositionLayoutWithColumns, ResolveMixins} from './base';
 import {GenericSpec} from './index';
 import {GenericLayerSpec, NormalizedLayerSpec} from './layer';
-import {Repeat} from './repeat';
+import {RepeatMapping} from './repeat';
 import {GenericUnitSpec, NormalizedUnitSpec} from './unit';
 
-export interface Repeat {
+export interface RepeatMapping {
   /**
    * An array of fields to be repeated vertically.
    */
@@ -28,7 +28,7 @@ export interface GenericRepeatSpec<U extends GenericUnitSpec<any, any>, L extend
    * 1) An array of fields to be repeated.  If `"repeat"` is an array, the field can be referred using `{"repeat": "repeat"}`
    * 2) An object that mapped `"row"` and/or `"column"` to the listed of fields to be repeated along the particular orientations. The objects `{"repeat": "row"}` and `{"repeat": "column"}` can be used to refer to the repeated field respectively.
    */
-  repeat: string[] | Repeat;
+  repeat: string[] | RepeatMapping;
 
   /**
    * A specification of the view that gets repeated.
