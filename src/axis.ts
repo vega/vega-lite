2319eafe5e4ef1cfc239708ca5fb7af811c09f44
Dominik Moritz <domoritz@gmail.com>
2019-03-10 15:34:46 -0700

    Update src/axis.ts
    Co-Authored-By: kanitw <kanitw@gmail.com>

--- src/axis.ts
+++ src/axis.ts
@@ -61,7 +61,7 @@ interface AxisMixins {
 
 export interface AxisOrientMixins {
   /**
-   * The orientation of the axis. One of `"top"`, `"bottom"`, `"left"` or `"right"`. The orientation can be used to further specialize the axis type (e.g., a y-axis oriented for the right edge of the chart).
+   * The orientation of the axis. One of `"top"`, `"bottom"`, `"left"` or `"right"`. The orientation can be used to further specialize the axis type (e.g., a y-axis oriented towards the right edge of the chart).
    *
    * __Default value:__ `"bottom"` for x-axes and `"left"` for y-axes.
    */
