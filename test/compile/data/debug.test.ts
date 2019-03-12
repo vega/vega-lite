0a70800ed1d46f2439a958a8a4a908bafe9f2d62
Saba Noorassa <saba1998@gmail.com>
2019-03-12 03:59:25 -0700

    More debug tests

--- test/compile/data/debug.test.ts
+++ test/compile/data/debug.test.ts
@@ -1,4 +1,6 @@
+import {BinNode} from '../../../src/compile/data/bin';
 import {checkLinks, debug, draw} from '../../../src/compile/data/debug';
+import {SourceNode} from '../../../src/compile/data/source';
 import {resetIdCounter} from '../../../src/util';
 import {DataFlowNode} from './../../../src/compile/data/dataflow';
 
@@ -31,6 +33,30 @@ const dot2 = `digraph DataFlow {
   "43" -> "45"
 }`;
 
+const dot3 =
+  `digraph DataFlow {
+  rankdir = TB;
+  node [shape=record]
+    "43" [
+    label = <Source<br/><i>foo.bar</i>>;
+    tooltip = "[43]&#010;foo.bar"
+  ]` + '\n\n  \n}';
+
+const dot4 = `digraph DataFlow {
+  rankdir = TB;
+  node [shape=record]
+    "43" [
+    label = <DataFlow>;
+    tooltip = "[43]&#010;44"
+  ]
+  "45" [
+    label = <Bin<br/><font color="grey" point-size="10">IN:</font> foo<br/><font color="grey" point-size="10">OUT:</font> bar>;
+    tooltip = "[45]&#010;Bin {foo:{as:[bar],bin:{},field:foo}}"
+  ]
+
+  "43" -> "45"
+}`;
+
 describe('compile/data/debug', () => {
   describe('draw', () => {
     it('should draw simple dataflow graph', () => {
@@ -43,10 +69,25 @@ describe('compile/data/debug', () => {
     });
     it('should print node debugName when defined', () => {
       resetIdCounter();
+
       const root = new DataFlowNode(null, 'foo');
       // @ts-ignore
       const node = new DataFlowNode(root, 'bar');
       expect(draw([root])).toBe(dot2);
+    });
+    it('should print node.data.url when defined', () => {
+      resetIdCounter();
+
+      const root = new SourceNode({url: 'foo.bar'});
+      expect(draw([root])).toBe(dot3);
+    });
+    it('should print dependent and produced field', () => {
+      resetIdCounter();
+
+      const root = new DataFlowNode(null);
+      // @ts-ignore
+      const node = new BinNode(root, {foo: {field: 'foo', as: ['bar'], bin: {}}});
+      expect(draw([root])).toBe(dot4);
     });
   });
   describe('checkLinks', () => {
@@ -55,6 +96,14 @@ describe('compile/data/debug', () => {
       const root = new DataFlowNode(null);
       const node = new DataFlowNode(root);
       node.parent = null;
+      expect(checkLinks([root])).toBe(false);
+    });
+    it('should return false with more complicated inconsistent data flow', () => {
+      resetIdCounter();
+      const root = new DataFlowNode(null);
+      const node = new DataFlowNode(root);
+      const node2 = new DataFlowNode(node);
+      node2.parent = null;
       expect(checkLinks([root])).toBe(false);
     });
   });
