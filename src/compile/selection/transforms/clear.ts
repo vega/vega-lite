958360c8d4232b9ce6e8c06714f37abb6d573fff
Allen Lee <allenlee@mit.edu>
2019-03-13 02:30:41 -0400

    added test suite and modified clear to not occur on default - must flag as true at least
    modified default behavior and updated tests appropriately
    
    clear tx and modified dependent files
    
    testing editor linking
    
    clear tx working but slow
    
    removing unnecssary type
    
    updating tx type to modify existing signal
    
    added test suite and modified clear to not occur on default - must flag as true at least
    
    modified default behavior and updated tests appropriately
    
    readding schema file found in master branch

--- src/compile/selection/transforms/clear.ts
+++ src/compile/selection/transforms/clear.ts
@@ -1,15 +1,15 @@
-import {TransformCompiler} from './transforms';
 import {TUPLE} from '..';
+import {TransformCompiler} from './transforms';
 
 const clear: TransformCompiler = {
   has: selCmpt => {
-    return selCmpt.type === 'multi' && selCmpt.clear !== 'none';
+    return selCmpt.type === 'multi' && selCmpt.clear;
   },
 
   signals: (model, selCmpt, signals) => {
     const tupleIdx = signals.findIndex(i => i.name === selCmpt.name + TUPLE);
     signals[tupleIdx].on.push({
-      events: selCmpt.clear,
+      events: [{source: 'scope', type: selCmpt.clear}],
       update: 'null'
     });
     return signals;
