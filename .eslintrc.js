14d6360f6372ca1d4f71a06d01eef75f7ed2a9d4
Dominik Moritz <domoritz@gmail.com>
2019-04-21 16:34:46 -0700

    Better eslintrc format

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,14 +20,14 @@ module.exports = {
   rules: {
     "prettier/prettier": "warn",
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
-    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/prefer-for-of": 1,
-    // "@typescript-eslint/no-for-in-array": 1, // requires type information
-    "@typescript-eslint/no-require-imports": 1,
-    "@typescript-eslint/no-parameter-properties": 0,
-    "@typescript-eslint/explicit-member-accessibility": 0,
+    "@typescript-eslint/prefer-for-of": "error",
+    // "@typescript-eslint/no-for-in-array": "error", // requires type information
+    "@typescript-eslint/no-require-imports": "error",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
@@ -37,26 +37,20 @@ module.exports = {
         ignoreRestSiblings: true
       }
     ],
-    "@typescript-eslint/no-object-literal-type-assertion": [
-      "error",
-      {
-        allowAsParameter: true
-      }
-    ],
-    "@typescript-eslint/no-object-literal-type-assertion": 0,
-    "@typescript-eslint/no-namespace": 0, // we can do this later
+    "@typescript-eslint/no-object-literal-type-assertion": "off", // we can do this later
+    "@typescript-eslint/no-namespace": "off", // we can do this later
     // https://eslint.org/docs/rules/
     "linebreak-style": ["error", "unix"],
     "no-irregular-whitespace": ["error", { skipComments: true }],
-    "no-alert": 1,
-    "prefer-const": 1,
-    "no-return-assign": 1,
-    "no-useless-call": 1,
-    "no-shadow": 1,
-    "no-useless-concat": 1,
-    // "prefer-template": 1,  // we can do this later
-    "no-console": 0, // we use console
-    "no-undef": 0, // typescript takes care of this for us
-    "no-unreachable": 0 // typescript takes care of this for us
+    "no-alert": "error",
+    "prefer-const": "error",
+    "no-return-assign": "error",
+    "no-useless-call": "error",
+    "no-shadow": "error",
+    "no-useless-concat": "error",
+    // "prefer-template": "error",  // we can do this later
+    "no-console": "off", // we use console
+    "no-undef": "off", // typescript takes care of this for us
+    "no-unreachable": "off" // typescript takes care of this for us
   }
 };
