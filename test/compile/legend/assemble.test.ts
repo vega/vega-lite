926f0e4aa3ce0b83839d493847c98563001f893b
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-08-05 10:50:00 -0700

    Update test/compile/legend/assemble.test.ts
    Co-Authored-By: Dominik Moritz <domoritz@gmail.com>

--- test/compile/legend/assemble.test.ts
+++ test/compile/legend/assemble.test.ts
@@ -3,7 +3,6 @@ import {parseUnitModelWithScale} from '../../util';
 describe('legend/assemble', () => {
   it('correctly applies labelExpr.', () => {
     const model = parseUnitModelWithScale({
-      $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
       description: 'A scatterplot showing horsepower and miles per gallons.',
       data: {url: 'data/cars.json'},
       mark: 'point',
