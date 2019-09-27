9fcc6849f4afeaa4cbf79ea2931088c3c9e67315
Dominik Moritz <domoritz@gmail.com>
2019-09-27 10:40:14 -0700

    chore: simplify versions script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -4,7 +4,7 @@ set -e
 
 FILE='site/_data/versions.yml'
 
-echo "vega: `./scripts/version.sh vega`" > $FILE
-echo "vega-lite: `./scripts/version.sh vega-lite`" >> $FILE
-echo "vega-embed: `./scripts/version.sh vega-embed`" >> $FILE
-echo "vega-tooltip: `./scripts/version.sh vega-tooltip`" >> $FILE
+echo "vega: `scripts/version.sh vega`" > $FILE
+echo "vega-lite: `scripts/version.sh vega-lite`" >> $FILE
+echo "vega-embed: `scripts/version.sh vega-embed`" >> $FILE
+echo "vega-tooltip: `scripts/version.sh vega-tooltip`" >> $FILE
