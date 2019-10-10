0fea784588b4bca0b968e51a5eee0bb44a6d1dc7
Dominik Moritz <domoritz@gmail.com>
2019-10-10 10:54:01 -0700

    Updates

---
+++ _site/rollup.config.js
@@ -0,0 +1,19 @@
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'build/site/static/index.js',
+  output: {
+    file: 'site/static/bundle.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: true
+  },
+  plugins: [nodeResolve({browser: true}), commonjs(), json(), production && terser()]
+};
