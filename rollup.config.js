89c35965b683a907d00f80f1d41140b296bbfb5d
Dominik Moritz <domoritz@gmail.com>
2020-01-07 22:53:47 -0800

    chore: upgrade deps and replace rollup deps (#5727)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export function disallowedImports() {
