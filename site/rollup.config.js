9712ac9694279918251b56ef26a21de1cfae2e89
Dominik Moritz <domoritz@gmail.com>
2020-01-07 18:34:21 -0800

    chore: upgrade deps and replace rollup deps

--- site/rollup.config.js
+++ site/rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 
 // `npm run build` -> `production` is true
