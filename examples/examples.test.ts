1670204d60745f43d3ba3583c73a4e5bdf395dcc
Dominik Moritz <domoritz@gmail.com>
2020-01-19 15:34:58 -0800

    fix: actually enable tests for examples

--- examples/examples.test.ts
+++ examples/examples.test.ts
@@ -48,59 +48,52 @@ function validateVega(vegaSpec: VgSpec) {
   expect(valid).toBe(true);
 }
 
-const futureSuffixLength = '_future.vl.json'.length;
-const brokenSuffixLength = '_broken.vl.json'.length;
+const BROKEN_SUFFIX = '_broken.vl.json';
+const FUTURE_SUFFIX = '_future.vl.json';
 
-describe('Examples', () => {
-  const examples = fs.readdirSync('examples/specs').map(file => 'examples/specs/' + file);
-  const normalizedExamples = fs
-    .readdirSync('examples/specs/normalized')
-    .map(file => 'examples/specs/normalized/' + file);
+const examples = fs.readdirSync('examples/specs').map(file => 'examples/specs/' + file);
+const normalizedExamples = fs.readdirSync('examples/specs/normalized').map(file => 'examples/specs/normalized/' + file);
 
-  for (const example of examples.concat(normalizedExamples)) {
-    if (path.extname(example) !== '.json') {
-      return;
-    }
-    const jsonSpec = JSON.parse(fs.readFileSync(example).toString());
-    const originalSpec = duplicate(jsonSpec);
+expect(examples).toContain('examples/specs/scatter_image.vl.json');
 
-    describe(
-      example,
-      log.wrap(localLogger => {
-        const vegaSpec: VgSpec = compile(jsonSpec).spec;
-
-        it('should not cause any side effects', () => {
-          expect(jsonSpec).toEqual(originalSpec);
-        });
-
-        it('should be valid Vega-Lite with proper $schema', () => {
-          if (
-            // Ignore all examples with "_future" suffix
-            example.lastIndexOf('_future.vl.json', example.length - futureSuffixLength) >= 0
-          ) {
-            return;
-          }
-          validateVL(jsonSpec);
-        });
-
-        it('should not include any warning', () => {
-          if (example.lastIndexOf('_broken.vl.json', example.length - brokenSuffixLength) >= 0) {
-            // Ignore all examples with "_broken" suffix
-            return;
-          }
-
-          expect(localLogger.warns).toEqual([]);
-        });
-
-        it('should produce valid vega', () => {
-          if (example.lastIndexOf('_broken.vl.json', example.length - brokenSuffixLength) >= 0) {
-            // Ignore all examples with "_broken" suffix
-            return;
-          }
-
-          validateVega(vegaSpec);
-        });
-      })
-    );
+for (const example of [...examples, ...normalizedExamples]) {
+  if (path.extname(example) !== '.json') {
+    continue;
   }
-});
+  const jsonSpec = JSON.parse(fs.readFileSync(example).toString());
+  const originalSpec = duplicate(jsonSpec);
+
+  describe(
+    example,
+    log.wrap(localLogger => {
+      const vegaSpec: VgSpec = compile(jsonSpec).spec;
+
+      it('should not cause any side effects', () => {
+        expect(jsonSpec).toEqual(originalSpec);
+      });
+
+      it('should be valid Vega-Lite with proper $schema', () => {
+        if (example.endsWith(FUTURE_SUFFIX)) {
+          return;
+        }
+        validateVL(jsonSpec);
+      });
+
+      it('should not produce any warning', () => {
+        if (example.endsWith(BROKEN_SUFFIX)) {
+          return;
+        }
+
+        expect(localLogger.warns).toEqual([]);
+      });
+
+      it('should produce valid vega', () => {
+        if (example.endsWith(BROKEN_SUFFIX)) {
+          return;
+        }
+
+        validateVega(vegaSpec);
+      });
+    })
+  );
+}
