3ba455ceed3883c805a69b2da096fd5da083dfc5
Kanit Wongsuphasawat <kanitw@gmail.com>
2020-02-07 17:51:53 -0800

    fix: rename to hide valueOrSignal

--- scripts/rename-schema.sh
+++ scripts/rename-schema.sh
@@ -10,11 +10,13 @@ perl -pi -e s,'CompositeEncoding','Encoding',g build/vega-lite-schema.json
 perl -pi -e s,'GenericLayerSpec<UnitSpec>','LayerSpec',g build/vega-lite-schema.json # This is required as LayerSpec's own definition would depend on generic and have GenericLayerSpec<UnitSpec>, not LayerSpec
 perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec>','\1',g build/vega-lite-schema.json
 
+
 perl -pi -e s,'ValueDef(.*)\<\(number\|\\\"width\\\"\)\>','XValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'ValueDef(.*)\<\(number\|\\\"height\\\"\)\>','YValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'ValueDef(.*)<\(string\|null\)>','StringValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'ValueDef(.*)<Value>','ValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'ValueDef(.*)<number>','NumberValueDef\1',g build/vega-lite-schema.json
+perl -pi -e s,'ValueOrSignal','Value',g build/vega-lite-schema.json
 
 perl -pi -e s,'Conditional<(.*)>','Conditional\1',g build/vega-lite-schema.json
 
