1453134594cd2e2010005ba111c8018836817263
Dominik Moritz <domoritz@gmail.com>
2020-02-12 13:44:45 -0800

    chore: cleanup jest config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,7 @@ module.exports = {
     dumpio: true
   },
   server: {
-    command: 'node ./node_modules/.bin/http-server -s -p 8000'
+    command: 'node ./node_modules/.bin/http-server -s -p 8000',
+    port: 8000
   }
 };
