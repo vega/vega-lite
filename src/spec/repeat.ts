57fbac8015c0d7b92da96ed04b48512cbfe03a87
Dominik Moritz <domoritz@gmail.com>
2020-03-16 09:42:49 -0700

    docs: clarify how repeat wrap works

--- src/spec/repeat.ts
+++ src/spec/repeat.ts
@@ -24,8 +24,8 @@ export interface GenericRepeatSpec<U extends GenericUnitSpec<any, any>, L extend
     ResolveMixins {
   /**
    * Definition for fields to be repeated. One of:
-   * 1) An array of fields to be repeated. If `"repeat"` is an array, the field can be referred using `{"repeat": "repeat"}`
-   * 2) An object that mapped `"row"` and/or `"column"` to the listed of fields to be repeated along the particular orientations. The objects `{"repeat": "row"}` and `{"repeat": "column"}` can be used to refer to the repeated field respectively.
+   * 1) An array of fields to be repeated. If `"repeat"` is an array, the field can be referred to as `{"repeat": "repeat"}`. The repeated views are laid out in a wrapped row. You can set the number of columns to control the wrapping.
+   * 2) An object that maps `"row"` and/or `"column"` to the listed fields to be repeated along the particular orientations. The objects `{"repeat": "row"}` and `{"repeat": "column"}` can be used to refer to the repeated field respectively.
    */
   repeat: string[] | RepeatMapping;
 
