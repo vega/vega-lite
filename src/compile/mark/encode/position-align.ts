f526748ad4a3b6e038cb8e30f92f5128820adba5
Kanit Wongsuphasawat <kanitw@gmail.com>
2020-04-04 16:56:20 -0700

    fix: consolidate positionAndSize() + support theta size (with theta2 = theta + size)
    fix band position for arc (fix #6238)

--- src/compile/mark/encode/position-align.ts
+++ src/compile/mark/encode/position-align.ts
@@ -1,4 +1,5 @@
 import {Align} from 'vega';
+import {getVgPositionChannel} from '../../../channel';
 import {Config} from '../../../config';
 import {MarkDef} from '../../../mark';
 import {VgEncodeChannel} from '../../../vega.schema';
@@ -16,15 +17,20 @@ const BASELINED_Y_CHANNEL = {
   bottom: 'y2'
 };
 
-export function alignedPositionChannel(channel: 'x' | 'y' | 'radius' | 'theta', markDef: MarkDef, config: Config) {
+export function vgAlignedPositionChannel(
+  channel: 'x' | 'y' | 'radius' | 'theta',
+  markDef: MarkDef,
+  config: Config,
+  defaultAlign: 'top' | 'middle' = 'middle'
+) {
   if (channel === 'radius' || channel === 'theta') {
-    return channel;
+    return getVgPositionChannel(channel);
   }
   const alignChannel = channel === 'x' ? 'align' : 'baseline';
   const align = getMarkPropOrConfig(alignChannel, markDef, config);
   if (channel === 'x') {
-    return ALIGNED_X_CHANNEL[align ?? 'center'];
+    return ALIGNED_X_CHANNEL[align || (defaultAlign === 'top' ? 'left' : 'center')];
   } else {
-    return BASELINED_Y_CHANNEL[align ?? 'middle'];
+    return BASELINED_Y_CHANNEL[align || defaultAlign];
   }
 }
