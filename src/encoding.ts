52bffdabbcc945ae620c107fc6bfca4b1536ceb0
Dominik Moritz <domoritz@gmail.com>
2020-04-16 11:28:15 -0700

    chore: use correct type

--- src/encoding.ts
+++ src/encoding.ts
@@ -325,7 +325,7 @@ export interface Encoding<F extends Field> {
   /**
    * A string that labels the axis group for accessibility.
    */
-  ariaLabel?: StringFieldDefWithCondition<F> | StringValueOrSignalWithCondition<F>;
+  ariaLabel?: StringFieldDefWithCondition<F> | StringValueOrSignalDefWithCondition<F>;
 
   /**
    * Order of the marks.
