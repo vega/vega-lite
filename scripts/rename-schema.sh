6410e042a4365bfd62d90bc8bffa2cb21c636dea
Kanit Wongsuphasawat <kanitw@gmail.com>
2020-05-02 12:15:28 -0700

    fix: revise `encoding.color = {signal:...}` to be `{value: {signal:...}}` (#6438)

--- scripts/rename-schema.sh
+++ scripts/rename-schema.sh
@@ -10,13 +10,7 @@ perl -pi -e s,'CompositeEncoding','Encoding',g build/vega-lite-schema.json
 perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,FieldName>','\1',g build/vega-lite-schema.json
 perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,Field>','Normalized\1',g build/vega-lite-schema.json
 
-perl -pi -e s,'ValueDef(.*)\<\(number\|\\\"width\\\"\)\>','XValueDef\1',g build/vega-lite-schema.json
-perl -pi -e s,'ValueDef(.*)\<\(number\|\\\"height\\\"\)\>','YValueDef\1',g build/vega-lite-schema.json
-perl -pi -e s,'ValueDef(.*)<\(string\|null\)>','StringValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'ValueDef(.*)<Value>','ValueDef\1',g build/vega-lite-schema.json
-perl -pi -e s,'ValueDef(.*)<number>','NumberValueDef\1',g build/vega-lite-schema.json
-perl -pi -e s,'ValueOrSignalDefWithCondition','ValueDefWithConditionAlias',g build/vega-lite-schema.json
-
 perl -pi -e s,'Conditional<(.*)>','Conditional\1',g build/vega-lite-schema.json
 
 perl -pi -e s,'FieldDefWithCondition<FieldDefWithoutScale>','FieldDefWithConditionWithoutScale',g build/vega-lite-schema.json
