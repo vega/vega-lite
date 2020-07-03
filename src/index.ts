625f79471654365820337dda63ec31f033eda5e5
Dominik Moritz <domoritz@gmail.com>
2020-07-03 11:34:18 -0700

    chore: unformat

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {version} from '../package.json';
 import {normalize} from './normalize';
 
+
 export {compile} from './compile/compile';
 export {Config} from './config';
 export {TopLevelSpec} from './spec';
