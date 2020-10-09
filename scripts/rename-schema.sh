d708bf363ade23fa39060e9c5ff0b405637efa70
Dominik Moritz <domoritz@gmail.com>
2020-10-09 15:43:32 -0700

    fix: correct which specs are normalized
    Repeat specs are normalized by filling in the field name.

--- scripts/rename-schema.sh
+++ scripts/rename-schema.sh
@@ -9,8 +9,8 @@ perl -pi -e s,'<ExprRef>','',g build/vega-lite-schema.json
 perl -pi -e s,'\,ExprRef>','>',g build/vega-lite-schema.json
 
 perl -pi -e s,'CompositeEncoding','Encoding',g build/vega-lite-schema.json
-perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,FieldName>','\1',g build/vega-lite-schema.json
-perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,Field>','Normalized\1',g build/vega-lite-schema.json
+perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,FieldName>','Normalized\1',g build/vega-lite-schema.json
+perl -pi -e s,'Generic(.*)<FacetedUnitSpec\,LayerSpec\,?.*\,Field>','\1',g build/vega-lite-schema.json
 
 perl -pi -e s,'ValueDef(.*)<Value>','ValueDef\1',g build/vega-lite-schema.json
 perl -pi -e s,'Conditional<(.*)>','Conditional\1',g build/vega-lite-schema.json
