2f39853c64d53b985bbb3c837095aae7aaa1cfcf
Dominik Moritz <domoritz@gmail.com>
2020-11-04 16:01:43 -0800

    refactor: export directly instead of importing first

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {version} from '../package.json';
-import {normalize} from './normalize';
+import pkg from '../package.json';
+export const version = pkg.version;
 
 export {compile} from './compile/compile';
 export type {Config} from './config';
+export {normalize} from './normalize';
 export type {TopLevelSpec} from './spec';
 export * from './util';
-export {normalize, version};
