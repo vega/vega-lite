d6c11572cc8b6cd66b688b5193ba243950634e8c
Dominik Moritz <domoritz@gmail.com>
2020-11-05 12:13:33 -0800

    docs: correct documentation of domain union with (#7027)

--- src/scale.ts
+++ src/scale.ts
@@ -429,17 +429,8 @@ export function isSelectionDomain(domain: Domain): domain is SelectionExtent {
 
 export interface DomainUnionWith {
   /**
-   * Customized domain values to be union with the field's values.
-   *
-   * 1) `domain` for _quantitative_ fields can take one of the following forms:
-   *
-   * - a two-element array with minimum and maximum values.
-   * - an array with more than two entries, for [Piecewise  quantitative scales](https://vega.github.io/vega-lite/docs/scale.html#piecewise). (Alternatively, the `domainMid` property can be set for a diverging scale.)
-   * - a string value `"unaggregated"`, if the input field is aggregated, to indicate that the domain should include the raw data values prior to the aggregation.
-   *
-   * 2) `domain` for _temporal_ fields can be a two-element array minimum and maximum values, in the form of either timestamps or the [DateTime definition objects](https://vega.github.io/vega-lite/docs/types.html#datetime).
-   *
-   * 3) `domain` for _ordinal_ and _nominal_ fields can be an array that lists valid input values.
+   * Customized domain values to be union with the field's values or explicitly defined domain.
+   * Should be an array of valid scale domain values.
    */
   unionWith: number[] | string[] | boolean[] | DateTime[];
 }
