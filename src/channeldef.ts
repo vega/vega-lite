a47cc60a76947af7192035a7579b9d040ba1e65f
Dominik Moritz <domoritz@gmail.com>
2020-11-13 09:39:17 -0800

    refactor: move format mixins out of guide sinc eit's also used for string field defs

--- src/channeldef.ts
+++ src/channeldef.ts
@@ -51,7 +51,7 @@ import {Config} from './config';
 import {DateTime, dateTimeToExpr, isDateTime} from './datetime';
 import {Encoding} from './encoding';
 import {ExprRef, isExprRef} from './expr';
-import {FormatMixins, Guide, GuideEncodingConditionalValueDef, TitleMixins} from './guide';
+import {Guide, GuideEncodingConditionalValueDef, TitleMixins} from './guide';
 import {ImputeParams} from './impute';
 import {Legend} from './legend';
 import * as log from './log';
@@ -73,6 +73,7 @@ import {
 import {AggregatedFieldDef, WindowFieldDef} from './transform';
 import {getFullName, QUANTITATIVE, StandardType, Type} from './type';
 import {
+  Dict,
   flatAccessWithDatum,
   getFirstDefined,
   internalField,
@@ -386,6 +387,31 @@ export interface DatumDef<
   // only apply Repeatref if field (F) can be RepeatRef
   // FIXME(https://github.com/microsoft/TypeScript/issues/37586):
   // `F extends RepeatRef` probably should be `RepeatRef extends F` but there is likely a bug in TS.
+}
+
+export interface FormatMixins {
+  /**
+   * When used with the default `"number"` and `"time"` format type, the text formatting pattern for labels of guides (axes, legends, headers) and text marks.
+   *
+   * - If the format type is `"number"` (e.g., for quantitative fields), this is D3's [number format pattern](https://github.com/d3/d3-format#locale_format).
+   * - If the format type is `"time"` (e.g., for temporal fields), this is D3's [time format pattern](https://github.com/d3/d3-time-format#locale_format).
+   *
+   * See the [format documentation](https://vega.github.io/vega-lite/docs/format.html) for more examples.
+   *
+   * When used with a [custom `formatType`](https://vega.github.io/vega-lite/docs/config.html#custom-format-type), this value will be passed as `format` alongside `datum.value` to the registered function.
+   *
+   * __Default value:__  Derived from [numberFormat](https://vega.github.io/vega-lite/docs/config.html#format) config for number format and from [timeFormat](https://vega.github.io/vega-lite/docs/config.html#format) config for time format.
+   */
+  format?: string | Dict<unknown>;
+
+  /**
+   * The format type for labels. One of `"number"`, `"time"`, or a [registered custom format type](https://vega.github.io/vega-lite/docs/config.html#custom-format-type).
+   *
+   * __Default value:__
+   * - `"time"` for temporal fields and ordinal and nominal fields with `timeUnit`.
+   * - `"number"` for quantitative fields as well as ordinal and nominal fields without `timeUnit`.
+   */
+  formatType?: 'number' | 'time' | string;
 }
 
 export type StringDatumDef<F extends Field = string> = DatumDef<F> & FormatMixins;
