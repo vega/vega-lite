346750640ac1d8a83bf1eef8b4b9aa1851656ccd
Dominik Moritz <domoritz@gmail.com>
2020-11-17 16:10:19 -0800

    fix: correct dataflow debug vis (#7050)

--- src/compile/data/debug.ts
+++ src/compile/data/debug.ts
@@ -23,9 +23,10 @@ export function printDebugDataflow(node: DataFlowNode) {
 export function drawDataflow(roots: readonly DataFlowNode[], size = 500) {
   const dot = dotString(roots);
   const text = new TextEncoder().encode(dot);
-  const compressed = pako.deflate(text, {level: 9, to: 'string'});
-  const result = btoa(compressed).replace(/\+/g, '-').replace(/\//g, '_');
-  const imageURL = `https://kroki.io/plantuml/png/${result}`;
+  const compressed = pako.deflate(text, {level: 9});
+  const result = Buffer.from(compressed).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+  const imageURL = `https://kroki.io/graphviz/png/${result}`;
+  console.log('Dataflow visualization: ', imageURL);
   console.log('%c ', `font-size:${size}px; background:url(${imageURL}) no-repeat; background-size:contain`);
 }
 
