976fa9e9897a1113716ce03e56be32278265f7eb
Dominik Moritz <domoritz@gmail.com>
2021-04-08 09:54:03 -0700

    no base [SVG]

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -39,7 +39,7 @@ do
   then
     rm -f examples/compiled/$name.svg
     rm -f examples/compiled/$name.png
-    npx vg2svg --base ./site --seed 123456789 examples/compiled/$name.vg.json > examples/compiled/$name.svg
-    npx vg2png --base ./site --seed 123456789 examples/compiled/$name.vg.json > examples/compiled/$name.png
+    npx vg2svg --seed 123456789 examples/compiled/$name.vg.json > examples/compiled/$name.svg
+    npx vg2png --seed 123456789 examples/compiled/$name.vg.json > examples/compiled/$name.png
   fi
 done
