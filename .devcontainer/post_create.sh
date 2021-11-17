b9648d1ea971effe693bedf5b273d6cb4100f56d
Carsten Behring <carsten.behring@gmail.com>
2021-11-17 21:11:59 +0100

    fixed file permissins and remove link

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,4 +1,4 @@
 pushd site && bundle install && popd
-yarn link
-cd ~ && git clone https://github.com/vega/editor.git
-cd editor && yarn link "vega-lite"
+#yarn link
+#cd ~ && git clone https://github.com/vega/editor.git
+#cd editor && yarn link "vega-lite"
