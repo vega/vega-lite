53f0127f892a08dee9dd4b7952d6ca7696524f7c
Jonathan Zong <jzong@mit.edu>
2023-05-24 21:36:57 -0400

    time rescale optional

--- src/channeldef.ts
+++ src/channeldef.ts
@@ -521,7 +521,7 @@ export type PolarDef<F extends Field> = PositionFieldDefBase<F> | PositionDatumD
 
 export type TimeDef<F extends Field> = TimeFieldDef<F>;
 export interface TimeMixins {
-  rescale: boolean;
+  rescale?: boolean;
 }
 export type TimeFieldDef<F extends Field> = ScaleFieldDef<F, StandardType> & TimeMixins;
 
