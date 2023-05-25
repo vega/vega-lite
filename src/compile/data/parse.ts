a27984b93ece469e93b523737305b7748598f2cc
Jonathan Zong <jzong@mit.edu>
2023-05-24 20:03:45 -0400

    cleanup

--- src/compile/data/parse.ts
+++ src/compile/data/parse.ts
@@ -160,8 +160,6 @@ function parseRoot(model: Model, sources: SourceNode[]): DataFlowNode {
 export function parseTransformArray(head: DataFlowNode, model: Model, ancestorParse: AncestorParse): DataFlowNode {
   let lookupCounter = 0;
 
-  console.log('model', model);
-
   for (const t of model.transforms) {
     let derivedType: ParseValue = undefined;
     let transformNode: DataFlowNode;
