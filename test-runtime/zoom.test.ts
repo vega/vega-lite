7874ea9396c2256bfd31307365da6fb5b6e66912
Dominik Moritz <domoritz@gmail.com>
2023-08-31 10:07:58 -0600

    feat: switch from clone to built in structured clone (#9068)

--- test-runtime/zoom.test.ts
+++ test-runtime/zoom.test.ts
@@ -33,7 +33,7 @@ describe('Zoom interval selections at runtime', () => {
   let testRender: (filename: string) => Promise<void>;
 
   beforeAll(async () => {
-    page = await (global as any).__BROWSER__.newPage();
+    page = await (global as any).__BROWSER_GLOBAL__.newPage();
     embed = embedFn(page);
     await page.goto('http://0.0.0.0:8000/test-runtime/');
   });
