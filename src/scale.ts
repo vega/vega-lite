d50d73fab7946243bd24bd3ca4b73312f8a8d003
Josh Pollock <jopo@mit.edu>
2023-09-19 14:44:15 -0700

    expose default values for time ranges

--- src/scale.ts
+++ src/scale.ts
@@ -424,6 +424,22 @@ export interface ScaleConfig<ES extends ExprRef | SignalRef> {
    *
    */
   zero?: boolean;
+
+  /**
+   * Default framerate (frames per second) for time [`band`](https://vega.github.io/vega-lite/docs/scale.html#band) scales.
+   *
+   * __Default value:__ `2`
+   *
+   */
+  framesPerSecond?: number;
+
+  /**
+   * Default animation duration (in seconds) for time encodings, except for [`band`](https://vega.github.io/vega-lite/docs/scale.html#band) scales.
+   *
+   * __Default value:__ `5`
+   *
+   */
+  animationDuration?: number;
 }
 
 export const defaultScaleConfig: ScaleConfig<SignalRef> = {
@@ -450,7 +466,10 @@ export const defaultScaleConfig: ScaleConfig<SignalRef> = {
   quantileCount: 4,
   quantizeCount: 4,
 
-  zero: true
+  zero: true,
+
+  framesPerSecond: 2,
+  animationDuration: 5
 };
 
 export interface SchemeParams {
