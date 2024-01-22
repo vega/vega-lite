ff95d7b29a5c46e1e9246584b815bbb431c5e038
Jonathan Zong <jzong@mit.edu>
2024-01-22 17:01:34 -0500

    renamed variable

--- test-runtime/animation.test.ts
+++ test-runtime/animation.test.ts
@@ -49,7 +49,7 @@ describe('time encoding animations', () => {
   let testRender: (filename: string) => Promise<void>;
 
   beforeAll(async () => {
-    page = await (global as any).__BROWSER__.newPage();
+    page = await (global as any).__BROWSER_GLOBAL__.newPage();
     embed = embedFn(page);
     testRender = testRenderFn(page, 'animation');
     await page.goto('http://0.0.0.0:8000/test-runtime/');
