8b3b30b352ce0e50fe5422cd4d24323a5a5ec9b4
Lukas Hermann <lukashermann28@gmail.com>
2024-04-15 19:59:50 -0700

    fix: extent name collision in transforms (#9312)

--- src/transform.ts
+++ src/transform.ts
@@ -678,7 +678,7 @@ export function isFold(t: Transform): t is FoldTransform {
 }
 
 export function isExtent(t: Transform): t is ExtentTransform {
-  return 'extent' in t && !('density' in t);
+  return 'extent' in t && !('density' in t) && !('regression' in t);
 }
 export type Transform =
   | AggregateTransform
