41f3c37dda5941695585425a778215ea06d77a52
Kanit Wongsuphasawat <kanit.w@databricks.com>
2024-05-18 13:50:43 -0700

    fix: make text formatting preserve invalid values by coercing them to string by default

--- src/invalid.ts
+++ src/invalid.ts
@@ -8,7 +8,7 @@ import {isObject} from 'vega-util';
  */
 export interface MarkInvalidMixins {
   /**
-   * Invalid data mode, which defines how the marks and corresponding scales should represent invalid values (`null` and `NaN` in continuous scales *without* defined output for invalid values).
+   * Invalid data mode, which defines how the marks and corresponding scales should represent invalid values (`null` and `NaN` in continuous scales *without* defined output for invalid values in `config.scale.invalid`).
    *
    * - `"filter"` — *Exclude* all invalid values from the visualization's *marks* and *scales*.
    * For path marks (for line, area, trail), this option will create paths that connect valid points, as if the data rows with invalid values do not exist.
