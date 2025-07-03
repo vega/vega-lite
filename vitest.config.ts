b2a3171814c363ea0cd60899e511f17b28776eeb
Dominik Moritz <domoritz@gmail.com>
2025-07-03 14:37:01 +0200

    chore: update vitest config to not deprecated version

---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          include: ['examples/**/*.test.ts'],
+          name: 'examples',
+          environment: 'node',
+          globals: true,
+        },
+      },
+      {
+        test: {
+          include: ['test/**/*.test.ts'],
+          name: 'unit',
+          environment: 'node',
+          globals: true,
+        },
+      },
+      {
+        test: {
+          include: ['test-runtime/**/*.test.ts'],
+          globalSetup: './vitest.global-setup.ts',
+          name: 'runtime',
+          browser: {
+            provider: 'playwright',
+            enabled: true,
+            headless: false,
+            instances: [{browser: 'chromium'}],
+          },
+          globals: true,
+        },
+      },
+    ],
+  },
+});
