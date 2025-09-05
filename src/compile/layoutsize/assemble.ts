c6b1ea48906db76d91bf4c21b74b9ea3dc349f4b
Yuvvan Talreja <31566676+yuvvantalreja@users.noreply.github.com>
2025-09-04 21:14:28 -0400

    Update src/compile/layoutsize/assemble.ts
    Co-authored-by: Dominik Moritz <domoritz@gmail.com>

--- src/compile/layoutsize/assemble.ts
+++ src/compile/layoutsize/assemble.ts
@@ -20,7 +20,7 @@ export function assembleLayoutSignals(model: Model): NewSignal[] {
 export function sizeSignals(model: Model, sizeType: LayoutSizeType): (NewSignal | InitSignal)[] {
   const channel = sizeType === 'width' ? 'x' : 'y';
   const size = model.component.layoutSize.get(sizeType);
-  if (size === undefined || size === null || size === 'merged') {
+  if (size == null || size === 'merged') {
     return [];
   }
 
